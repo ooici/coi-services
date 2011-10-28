19bc392bf1dbf337cccd351d6a6b2f55cdd845cb
Michael Meisinger <mmeisinger@ucsd.edu>
2011-10-28 13:41:19 -0700

    Added ion-definitions submodule and some build files

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+import os
+import sys
+
+# Add /usr/local/include to the path for macs, fixes easy_install for several packages (like gevent and pyyaml)
+if sys.platform == 'darwin':
+    os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
+
+version = '0.1'
+
+setup(  name = 'coi-services',
+        version = version,
+        description = 'OOI ION COI Services' 
+        url = 'https://github.com/ooici/coi-services',
+        download_url = 'http://ooici.net/releases',
+        license = 'Apache 2.0',
+        author = 'Michael Meisinger',
+        author_email = 'mmeisinger@ucsd.edu',
+        keywords = ['ooici','ioncore', 'pyon', 'coi'],
+        packages = find_packages(),
+        entry_points = {
+            'console_scripts' : [
+                'pycc=scripts.pycc:entry',
+                'control_cc=scripts.control_cc:main',
+                'generate_interfaces=scripts.generate_interfaces:main'
+                ]
+            },
+        dependency_links = [
+            'http://ooici.net/releases'
+        ],
+        test_suite = 'pyon',
+        install_requires = [
+            'pyon',
+        ],
+     )
