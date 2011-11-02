429cd485647176be47297f7f764db38ae32bee82
Michael Meisinger <mmeisinger@ucsd.edu>
2011-11-01 18:43:36 -0700

    Added an initial COI service

---
+++ ion/services/coi/resource_registry_service.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__author__ = 'Michael Meisinger'
+
+from pyon.util.log import log
+
+from interface.services.iresource_registry_service import BaseResourceRegistryService
+
+class ResourceRegistryService(BaseResourceRegistryService):
+    pass
