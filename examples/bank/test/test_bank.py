72560bb22d81c56f69529ff5bc606e23b1e7a121
Thomas Lennan <thomaslennan@gmail.com>
2011-11-15 13:14:33 -0800

    Added example unittest that utilizes full blown container and messaging

---
+++ examples/bank/test/test_bank.py
@@ -0,0 +1,50 @@
+from pyon.container.cc import IContainerAgent
+from pyon.net.endpoint import ProcessRPCClient
+from pyon.public import Container
+from pyon.test.pyontest import PyonTestCase
+from interface.services.examples.bank.ibank_service import IBankService
+
+class FakeProcess(object):
+    name = ''
+
+class Test_Bank(PyonTestCase):
+
+    def test_bank(self):
+        # Start container
+        container = Container()
+        container.start()
+
+        # Establish endpoint with container
+        container_client = ProcessRPCClient(node=container.node, name=container.name, iface=IContainerAgent, process=FakeProcess())
+        container_client.start_rel_from_url('res/deploy/examples/bank_complete.yml')
+
+        # Now create client to bank service
+        client = ProcessRPCClient(node=container.node, name="bank", iface=IBankService, process=FakeProcess())
+
+        # Send some requests
+        print 'Creating savings account'
+        savingsAcctNum = client.new_account('kurt', 'Savings')
+        print "New savings account number: " + str(savingsAcctNum)
+        print "Starting savings balance %s" % str(client.get_balances(savingsAcctNum))
+        client.deposit(savingsAcctNum, 99999999)
+        print "Savings balance after deposit %s" % str(client.get_balances(savingsAcctNum))
+        client.withdraw(savingsAcctNum, 1000)
+        print "Savings balance after withdrawl %s" % str(client.get_balances(savingsAcctNum))
+
+        print "Buying 1000 savings bonds"
+        client.buy_bonds(savingsAcctNum, 1000)
+        print "Savings balance after bond purchase %s" % str(client.get_balances(savingsAcctNum))
+
+        checkingAcctNum = client.new_account('kurt', 'Checking')
+        print "New checking account number: " + str(checkingAcctNum)
+        print "Starting checking balance %s" % str(client.get_balances(checkingAcctNum))
+        client.deposit(checkingAcctNum, 99999999)
+        print "Confirming checking balance after deposit %s" % str(client.get_balances(checkingAcctNum))
+        client.withdraw(checkingAcctNum, 1000)
+        print "Confirming checking balance after withdrawl %s" % str(client.get_balances(checkingAcctNum))
+
+        acctList = client.list_accounts('kurt')
+        self.assertTrue(len(acctList) == 2)
+
+        # Shut down container
+        container.stop()
