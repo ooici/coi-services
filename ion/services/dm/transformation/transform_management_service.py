6ca110488c95181e292b6f92d0e955a6ae3b486d
Maurice Manning <mmmanning@ucsd.edu>
2011-11-15 22:58:10 -0800

    update to new format conventions

---
+++ ion/services/dm/transformation/transform_management_service.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+__author__ = 'Maurice Manning'
+__license__ = 'Apache 2.0'
+
+
+from interface.services.dm.itransform_management_service import BaseTransformManagementService
+
+class TransformationService(BaseTransformManagementService):
+
+    def create_transform(self, transform={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {transform_id: ''}
+        #
+        pass
+
+    def update_transform(self, transform={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_transform(self, transform_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # transform: {}
+        #
+        pass
+
+    def delete_transform(self, transform_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def bind_transform(self, transform_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def schedule_transform(self, transform_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
