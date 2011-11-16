393c8f4657cc48fe348e9078008b46ee70f9214d
Maurice Manning <mmmanning@ucsd.edu>
2011-11-15 22:59:22 -0800

    update to new format conventions

--- ion/services/sa/instrument_management/instrument_management_service.py
+++ ion/services/sa/instrument_management/instrument_management_service.py
@@ -9,7 +9,10 @@ from interface.services.sa.iinstrument_management_service import BaseInstrumentM
 class InstrumentManagementService(BaseInstrumentManagementService):
 
 
-    def define_instrument(self, instrument={}):
+    def create_instrument(self, instrument={}):
+        """
+        method docstring
+        """
         # Validate the input filter and augment context as required
 
         # Define YAML/params from
@@ -22,35 +25,233 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         # Return a resource ref
         pass
 
-    def get_instrument(self, resourceId=''):
+    def update_instrument(self, instrument={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_instrument(self, instrument_id=''):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
         # instrument: {}
         #
         pass
 
-    def activate_instrument(self, resourceId='', agentId=''):
+    def delete_instrument(self, instrument_id=''):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # {status: true}
+        # {success: true}
         #
         pass
 
-    def update_instrument(self, instrument={}):
+    def find_instruments(self, filters={}):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # {status: true}
+        # instrument_list: []
         #
         pass
 
-    def find_instrument(self, filters={}):
+    def request_direct_access(self, instrument_id=''):
+        """
+        method docstring
+        """
+        # Validate request; current instrument state, policy, and other
+
+        # Retrieve and save current instrument settings
+
+        # Request DA channel, save reference
+
+        # Return direct access channel
+        pass
+
+    def stop_direct_access(self, instrument_id=''):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # results: []
+        # {success: true}
         #
         pass
 
-    def define_platform(self, platform={}):
+    def request_command(self, instrument_id='', command={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def create_instrument_device(self, instrument_device={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {instrument_device_id: ''}
+        #
+        pass
+
+    def update_instrument_device(self, instrument_device={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_instrument_device(self, instrument_device_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_device: {}
+        #
+        pass
+
+    def delete_instrument_device(self, instrument_device_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_instrument_devices(self, filters={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_device_list: []
+        #
+        pass
+
+    def assign_instrument_device(self, instrument_id='', instrument_device_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def unassign_instrument_device(self, instrument_id='', instrument_device_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def activate_instrument_device(self, instrument_device_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def create_instrument_agent(self, instrument_agent={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {instrument_agent_id: ''}
+        #
+        pass
+
+    def update_instrument_agent(self, instrument_agent={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_instrument_agent(self, instrument_agent_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_agent: {}
+        #
+        pass
+
+    def delete_instrument_agent(self, instrument_agent_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_instrument_agents(self, filters={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_agent_list: []
+        #
+        pass
+
+    def assign_instrument_agent(self, instrument_agent_id='', instrument_id='', instrument_agent_instance={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def unassign_instrument_agent(self, instrument_agent_id='', instrument_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def create_platform(self, platform={}):
+        """
+        method docstring
+        """
         # Validate the input filter and augment context as required
 
         # Define YAML/params from https://confluence.oceanobservatories.org/display/CIDev/R2+Resource+Page+for+Platform
@@ -61,44 +262,114 @@ class InstrumentManagementService(BaseInstrumentManagementService):
 
         # Create associations
 
-        # Return a resource ref
         pass
 
-    def get_platform(self, resourceId=''):
+    def update_platform(self, platform={}):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # result: {}
+        # {success: true}
         #
         pass
 
-    def find_platform(self, filters={}):
+    def read_platform(self, platform_id=''):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # results: []
+        # platform: {}
         #
         pass
 
-    def retire_platform(self, resourceId=''):
+    def delete_platform(self, platform_id=''):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # {status: true}
+        # {success: true}
         #
         pass
 
-    def request_direct_access(self, resourceId=''):
-        # Validate request; current instrument state, policy, and other
-
-        # Retrieve and save current instrument settings
-
-        # Request DA channel, save reference
-
-        # Return direct access channel
-        pass
-
-    def request_command(self, resourceId=''):
+    def find_platforms(self, filters={}):
+        """
+        method docstring
+        """
         # Return Value
         # ------------
-        # {status: true}
+        # platform_list: []
         #
         pass
-  
+
+    def create_platform_agent(self, platform_agent={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {platform_agent_id: ''}
+        #
+        pass
+
+    def update_platform_agent(self, platform_agent={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_platform_agent(self, platform_agent_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # platform_agent: {}
+        #
+        pass
+
+    def delete_platform_agent(self, platform_agent_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_platform_agents(self, filters={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # platform_agent_list: []
+        #
+        pass
+
+    def assign_platform_agent(self, platform_agent_id='', platform_id='', platform_agent_instance={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def unassign_agent(self, platform_agent_id='', platform_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
