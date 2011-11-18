b7fa4e9196c1477c66e0d6ad78994989dd20dd8d
Ian Katz <ijk5@mit.edu>
2011-11-18 14:32:10 -0500

    F-CRUD added for all necessary entities, still need resource lifecycle state transitions

--- ion/services/sa/instrument_management/instrument_management_service.py
+++ ion/services/sa/instrument_management/instrument_management_service.py
@@ -4,12 +4,109 @@ __author__ = 'Maurice Manning'
 __license__ = 'Apache 2.0'
 
 
+#from pyon.public import Container
+from pyon.core.bootstrap import IonObject
+from pyon.core.exception import BadRequest, NotFound
+from pyon.datastore.datastore import DataStore
+#from pyon.net.endpoint import RPCClient
+from pyon.util.log import log
+
+
+
 from interface.services.sa.iinstrument_management_service import BaseInstrumentManagementService
 
 class InstrumentManagementService(BaseInstrumentManagementService):
 
 
-    def create_instrument(self, instrument={}):
+    
+    ##########################################################################
+    #
+    # INSTRUMENT DRIVER
+    #
+    ##########################################################################
+
+    def create_instrument_driver(self, instrument_driver_info={}):
+        """
+        method docstring
+        """
+
+        # Validate the input filter and augment context as required
+        try:
+            find_res = self.clients.resource_registry.find([
+                    ("type_", DataStore.EQUAL, "InstrumentDriver"), 
+                    DataStore.AND, 
+                    ("name", DataStore.EQUAL, name)
+                    ])
+            driver_info = find_res[0]
+        except NotFound:
+            # New after all.  PROCEED.
+            pass
+        else:
+            raise BadRequest("An instrument driver named '%s' already exists" % instrument_driver_info[name])
+
+        #FIXME: more validation?
+
+
+        #persist
+        instrument_driver_obj = IonObject("InstrumentDriver", instrument_driver_info)
+        create_tuple = self.clients.resource_registry.create(account_obj)
+        instrument_driver_id = create_tuple[0]
+
+        # Return a resource ref
+        return create_tuple
+
+
+    def update_instrument_driver(self, instrument_driver_id='', instrument_driver_info={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_instrument_driver(self, instrument_driver_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_driver_info: {}
+        #
+        pass
+
+    def delete_instrument_driver(self, instrument_driver_id='', reason=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_instrument_driver(self, filters={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_driver_info_list: []
+        #
+        pass
+
+
+
+
+    
+    ##########################################################################
+    #
+    # INSTRUMENT MODEL
+    #
+    ##########################################################################
+
+    def create_instrument(self, instrument_info={}):
         """
         method docstring
         """
@@ -23,9 +120,11 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         # Create associations
 
         # Return a resource ref
+
+
         pass
 
-    def update_instrument(self, instrument={}):
+    def update_instrument(self, instrument_id='', instrument_info={}):
         """
         method docstring
         """
@@ -41,11 +140,11 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         """
         # Return Value
         # ------------
-        # instrument: {}
+        # instrument_info: {}
         #
         pass
 
-    def delete_instrument(self, instrument_id=''):
+    def delete_instrument(self, instrument_id='', reason=''):
         """
         method docstring
         """
@@ -55,50 +154,31 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def find_instruments(self, filters={}):
+    def find_instrument(self, filters={}):
         """
         method docstring
         """
         # Return Value
         # ------------
-        # instrument_list: []
+        # instrument_info_list: []
         #
         pass
 
-    def request_direct_access(self, instrument_id=''):
-        """
-        method docstring
-        """
-        # Validate request; current instrument state, policy, and other
 
-        # Retrieve and save current instrument settings
 
-        # Request DA channel, save reference
 
-        # Return direct access channel
-        pass
 
-    def stop_direct_access(self, instrument_id=''):
-        """
-        method docstring
-        """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
-        pass
 
-    def request_command(self, instrument_id='', command={}):
-        """
-        method docstring
-        """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
-        pass
 
-    def create_instrument_device(self, instrument_device={}):
+    ##########################################################################
+    #
+    # PHYSICAL INSTRUMENT
+    #
+    ##########################################################################
+
+
+
+    def create_instrument_device(self, instrument_device_info={}):
         """
         method docstring
         """
@@ -108,7 +188,7 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def update_instrument_device(self, instrument_device={}):
+    def update_instrument_device(self, instrument_device_id='', instrument_device_info={}):
         """
         method docstring
         """
@@ -124,11 +204,11 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         """
         # Return Value
         # ------------
-        # instrument_device: {}
+        # instrument_device_info: {}
         #
         pass
 
-    def delete_instrument_device(self, instrument_device_id=''):
+    def delete_instrument_device(self, instrument_device_id='', reason=''):
         """
         method docstring
         """
@@ -138,13 +218,13 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def find_instrument_devices(self, filters={}):
+    def find_instrument_device(self, filters={}):
         """
         method docstring
         """
         # Return Value
         # ------------
-        # instrument_device_list: []
+        # instrument_device_info_list: []
         #
         pass
 
@@ -178,6 +258,132 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
+
+    def request_direct_access(self, instrument_id=''):
+        """
+        method docstring
+        """
+        # Validate request; current instrument state, policy, and other
+
+        # Retrieve and save current instrument settings
+
+        # Request DA channel, save reference
+
+        # Return direct access channel
+        pass
+
+    def stop_direct_access(self, instrument_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+
+
+    
+    ##########################################################################
+    #
+    # LOGICAL INSTRUMENT
+    #
+    ##########################################################################
+
+    def create_instrument(self, instrument_info={}):
+        """
+        method docstring
+        """
+        # Validate the input filter and augment context as required
+
+        # Define YAML/params from
+        # Instrument metadata draft: https://confluence.oceanobservatories.org/display/CIDev/R2+Resource+Page+for+Instrument+Instance
+
+        # Create instrument resource, set initial state, persist
+
+        # Create associations
+
+        # Return a resource ref
+        pass
+
+    def update_instrument(self, instrument_id='', instrument_info={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_instrument(self, instrument_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_info: {}
+        #
+        pass
+
+    def delete_instrument(self, instrument_id='', reason=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_instrument(self, filters={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # instrument_info_list: []
+        #
+        pass
+
+    def request_direct_access(self, instrument_id=''):
+        """
+        method docstring
+        """
+        # Validate request; current instrument state, policy, and other
+
+        # Retrieve and save current instrument settings
+
+        # Request DA channel, save reference
+
+        # Return direct access channel
+        pass
+
+    def stop_direct_access(self, instrument_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+
+
+
+
+
+
+
+    ##########################################################################
+    #
+    # INSTRUMENT AGENT
+    #
+    ##########################################################################
+
+
     def create_instrument_agent(self, instrument_agent={}):
         """
         method docstring
@@ -188,7 +394,7 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def update_instrument_agent(self, instrument_agent={}):
+    def update_instrument_agent(self, instrument_agent_id='', instrument_agent={}):
         """
         method docstring
         """
@@ -208,7 +414,7 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def delete_instrument_agent(self, instrument_agent_id=''):
+    def delete_instrument_agent(self, instrument_agent_id='', reason=''):
         """
         method docstring
         """
@@ -218,13 +424,13 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def find_instrument_agents(self, filters={}):
+    def find_instrument_agent(self, filters={}):
         """
         method docstring
         """
         # Return Value
         # ------------
-        # instrument_agent_list: []
+        # instrument_agent_info_list: []
         #
         pass
 
@@ -248,7 +454,78 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def create_platform(self, platform={}):
+
+
+    ##########################################################################
+    #
+    # PHYSICAL PLATFORM
+    #
+    ##########################################################################
+
+
+
+    def create_platform_device(self, platform_device_info={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {platform_device_id: ''}
+        #
+        pass
+
+    def update_platform_device(self, platform_device_id='', platform_device_info={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_platform_device(self, platform_device_id=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # platform_device_info: {}
+        #
+        pass
+
+    def delete_platform_device(self, platform_device_id='', reason=''):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_platform_device(self, filters={}):
+        """
+        method docstring
+        """
+        # Return Value
+        # ------------
+        # platform_device_info_list: []
+        #
+        pass
+
+
+
+
+
+
+    ##########################################################################
+    #
+    # LOGICAL PLATFORM 
+    #
+    ##########################################################################
+
+    def create_platform(self, platform_info={}):
         """
         method docstring
         """
@@ -264,7 +541,7 @@ class InstrumentManagementService(BaseInstrumentManagementService):
 
         pass
 
-    def update_platform(self, platform={}):
+    def update_platform(self, platform_id='', platform_info={}):
         """
         method docstring
         """
@@ -294,17 +571,26 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def find_platforms(self, filters={}):
+    def find_platform(self, filters={}):
         """
         method docstring
         """
         # Return Value
         # ------------
-        # platform_list: []
+        # platform_info_list: []
         #
         pass
 
-    def create_platform_agent(self, platform_agent={}):
+
+
+
+    ##########################################################################
+    #
+    # PLATFORM AGENT
+    #
+    ##########################################################################
+
+    def create_platform_agent(self, platform_agent_info={}):
         """
         method docstring
         """
@@ -314,7 +600,7 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def update_platform_agent(self, platform_agent={}):
+    def update_platform_agent(self, platform_agent_id='', platform_agent_info={}):
         """
         method docstring
         """
@@ -334,7 +620,7 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def delete_platform_agent(self, platform_agent_id=''):
+    def delete_platform_agent(self, platform_agent_id='', reason=''):
         """
         method docstring
         """
@@ -344,13 +630,13 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         #
         pass
 
-    def find_platform_agents(self, filters={}):
+    def find_platform_agent(self, filters={}):
         """
         method docstring
         """
         # Return Value
         # ------------
-        # platform_agent_list: []
+        # platform_agent_info_list: []
         #
         pass
 
