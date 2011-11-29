56c51285f73114d7b28ceea92293893665d894f9
Maurice Manning <mmmanning@ucsd.edu>
2011-11-29 13:25:02 -0800

    add preservation mgmt svc and update marine facility mgmt service

---
+++ ion/services/dm/preservation/preservation_management_service.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+__author__ = 'Maurice Manning'
+__license__ = 'Apache 2.0'
+
+
+from interface.services.dm.ipreservation_management_service import BasePreservationManagementService
+
+
+class PreservationManagementService(BasePreservationManagementService):
+
+    def create_datastore(self, datastore={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {datastore_id: ''}
+        #
+        pass
+
+    def update_datastore(self, datastore={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_datastore(self, datastore_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # datastore: {}
+        #
+        pass
+
+    def delete_datastore(self, datastore_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def create_persistent_archive(self, persistent_archive={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {persistent_archive_id: ''}
+        #
+        pass
+
+    def update_persistent_archive(self, persistent_archive={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_persistent_archive(self, persistent_archive_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # persistent_archive: {}
+        #
+        pass
+
+    def delete_persistent_archive(self, persistent_archive_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
--- ion/services/sa/marine_facility_management_service/marine_facility_management_service.py
+++ ion/services/sa/marine_facility_management_service/marine_facility_management_service.py
@@ -11,8 +11,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
 
 
     def create_marine_facility(self, marine_facility={}):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -21,8 +20,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def update_marine_facility(self, marine_facility={}):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -31,8 +29,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def read_marine_facility(self, marine_facility_id=''):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -41,8 +38,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def delete_marine_facility(self, marine_facility_id=''):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -50,9 +46,8 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         #
         pass
 
-    def find_marine_facility(self, filters={}):
-        """
-        method docstring
+    def find_marine_facilities(self, filters={}):
+        """method docstring
         """
         # Return Value
         # ------------
@@ -61,8 +56,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def create_site(self, site={}):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -71,8 +65,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def update_site(self, site={}):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -81,8 +74,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def read_site(self, site_id=''):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -90,9 +82,8 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         #
         pass
 
-    def delete_site(self, logical_platform_id=''):
-        """
-        method docstring
+    def delete_site(self, site_id=''):
+        """method docstring
         """
         # Return Value
         # ------------
@@ -100,9 +91,8 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         #
         pass
 
-    def find_site(self, filters={}):
-        """
-        method docstring
+    def find_sites(self, filters={}):
+        """method docstring
         """
         # Return Value
         # ------------
@@ -110,19 +100,35 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         #
         pass
 
-    def create_logical_platform(self, logical_platform={}):
-        """
-        method docstring
+    def assign_site(self, child_site_id='', parent_site_id=''):
+        """method docstring
         """
         # Return Value
         # ------------
-        # {site_id: ''}
+        # {success: true}
+        #
+        pass
+
+    def unassign_site(self, parent_site_id='', child_site_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def create_logical_platform(self, logical_platform={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {logical_platform_id: ''}
         #
         pass
 
     def update_logical_platform(self, logical_platform={}):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -131,8 +137,7 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         pass
 
     def read_logical_platform(self, logical_platform_id=''):
-        """
-        method docstring
+        """method docstring
         """
         # Return Value
         # ------------
@@ -140,13 +145,112 @@ class MarineFacilityManagementService(BaseMarineFacilityManagementService):
         #
         pass
 
-    def find_logical_platform(self, filters={}):
+    def delete_logical_platform(self, logical_platform_id=''):
+        """method docstring
         """
-        method docstring
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_logical_platforms(self, filters={}):
+        """method docstring
         """
         # Return Value
         # ------------
         # logical_platform_list: []
+        #
+        pass
+
+    def assign_platform(self, logical_platform_id='', parent_site_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def unassign_platform(self, logical_platform_id='', parent_site_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def create_logical_instrument(self, logical_instrument={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {logical_instrument_id: ''}
+        #
+        pass
+
+    def update_logical_instrument(self, logical_instrument={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def read_logical_instrument(self, logical_instrument_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # logical_instrument: {}
+        #
+        pass
+
+    def delete_logical_instrument(self, logical_instrument_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def find_logical_instruments(self, filters={}):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # logical_instrument_list: []
+        #
+        pass
+
+    def assign_instrument(self, logical_instrument_id='', parent_site_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def unassign_instrument(self, logical_instrument_id='', parent_site_id=''):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # {success: true}
+        #
+        pass
+
+    def define_observatory_policy(self):
+        """method docstring
+        """
+        # Return Value
+        # ------------
+        # null
+        # ...
         #
         pass
 
