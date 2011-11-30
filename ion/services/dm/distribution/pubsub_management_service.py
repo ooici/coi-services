18296340dd695b78b469201c09f5494259d6cc1f
Tim Giguere <tgiguere@asascience.com>
2011-11-30 16:05:16 -0500

    First run at implementing PubSub service

--- ion/services/dm/distribution/pubsub_management_service.py
+++ ion/services/dm/distribution/pubsub_management_service.py
@@ -3,18 +3,32 @@
 __author__ = 'Maurice Manning'
 __license__ = 'Apache 2.0'
 
-from pyon.public import log
+'''
+@package ion.services.dm.distribution.pubsub_management_service Implementation of IPubsubManagementService interface
+@file ion/services/dm/distribution/pubsub_management_service.py
+@author Tim Giguere
+@brief PubSub Management service to keep track of Streams, Publishers, Subscriptions,
+and the relationships between them
+'''
 
-from interface.services.dm.ipubsub_management_service import BasePubsubManagementService
-from pyon.core.bootstrap import IonObject
+from interface.services.dm.ipubsub_management_service import \
+    BasePubsubManagementService
+from pyon.core.exception import NotFound
+from pyon.core.bootstrap import IonObject, AT
+from pyon.datastore.datastore import DataStore
+
 
 class PubsubManagementService(BasePubsubManagementService):
+    '''Implementation of IPubsubManagementService. This class uses resource registry client
+        to create streams and subscriptions.
+    '''
 
+    def create_stream(self, stream=None):
+        '''Create a new stream.
 
-    def create_stream(self, stream={}):
-        """
-        method docstring
-        """
+        @param stream New stream properties
+        @retval id New stream id
+        '''
 #        log.debug("create_stream" + stream.name)
 #        assert not hasattr(stream, "_id"), "ID already set"
 #        # Register the stream; create and store the resource and associations
@@ -25,16 +39,18 @@ class PubsubManagementService(BasePubsubManagementService):
 #
 #        # Return the stream id
 #        return stream_id
-        
         stream_obj = IonObject("Stream", stream)
-        id, rev = self.clients.resource_registry.create(stream_obj)
+        stream_id, rev = self.clients.resource_registry.create(stream_obj)
 
-        return id
+        return stream_id
 
     def update_stream(self, stream={}):
-        """
-        method docstring
-        """
+        '''
+        Update an existing stream.
+
+        @param stream The stream object with updated properties
+        @retval success Boolean to indicate successful update
+        '''
         # Return Value
         # ------------
         # {success: true}
@@ -42,33 +58,39 @@ class PubsubManagementService(BasePubsubManagementService):
         return self.clients.resource_registry.update(stream)
 
     def read_stream(self, stream_id=''):
-        """
-        method docstring
-        """
+        '''
+        Get an existing stream object
+
+        @param stream_id The id of the stream
+        @retval stream The stream object
+        @throws NotFound when stream doesn't exist
+        '''
         # Return Value
         # ------------
         # stream: {}
         #
-        stream_obj = self.clients.resource_registry.find(stream_id)
+        stream_obj = self.clients.resource_registry.read(stream_id)
         if stream_obj is None:
             raise NotFound("Stream %d does not exist" % stream_id)
         return stream_obj
 
     def delete_stream(self, stream_id=''):
-        """
-        method docstring
-        """
+        '''
+        Delete an existing stream
+
+        @param stream_id The id of the stream
+        @retval success Boolean to indicate successful deletion
+        @throws NotFound when stream doesn't exist
+        '''
         # Return Value
         # ------------
         # {success: true}
         #
         stream_obj = self.read_stream(stream_id)
-        if stream_obj is not None:
-            self.clients.resource_registry.delete(stream_obj)
-            return True
-        else:
-            return False
+        if stream_obj is None:
+            raise NotFound("Stream %d does not exist" % stream_id)
 
+        return self.clients.resource_registry.delete(stream_obj)
 
     def find_streams(self, filter={}):
         """
@@ -81,15 +103,18 @@ class PubsubManagementService(BasePubsubManagementService):
         pass
 
     def find_streams_by_producer(self, producer_id=''):
-        """
-        method docstring
-        """
+        '''
+        Find all streams that contain a particular producer
+
+        @param producer_id The id of the producer
+        @retval stream_list The list of streams that contain the producer
+        '''
         # Return Value
         # ------------
         # stream_list: []
         #
         pass
-
+    
     def find_streams_by_consumer(self, consumer_id=''):
         """
         method docstring
@@ -101,64 +126,100 @@ class PubsubManagementService(BasePubsubManagementService):
         pass
 
     def create_subscription(self, subscription={}):
-        """
-        method docstring
-        """
+        '''
+        Create a new subscription
+
+        @param subscription New subscription properties
+        @retval id The id of the the new subscription
+        '''
         # Return Value
         # ------------
         # {subscription_id: ''}
         #
-        pass
+        subscription_obj = IonObject("Subscription", subscription)
+        id, rev = self.clients.resource_registry.create(subscription_obj)
+
+        return id
 
     def update_subscription(self, subscription={}):
-        """
-        method docstring
-        """
+        '''
+        Update an existing subscription
+
+        @param subscription The subscription object with updated properties
+        @retval success Boolean to indicate successful update
+        '''
         # Return Value
         # ------------
         # {success: true}
         #
-        pass
+        return self.clients.resource_registry.update(subscription)
 
     def read_subscription(self, subscription_id=''):
-        """
-        method docstring
-        """
+        '''
+        Get an existing subscription object
+
+        @param subscription_id The id of the subscription
+        @retval subscription The subscription object
+        @throws NotFound when subscription doesn't exist
+        '''
         # Return Value
         # ------------
         # subscription: {}
         #
-        pass
+        subscription_obj = self.clients.resource_registry.read(subscription_id)
+        if subscription_obj is None:
+            raise NotFound("Subscription %d does not exist" % subscription_id)
+        return subscription_obj
 
     def delete_subscription(self, subscription_id=''):
-        """
-        method docstring
-        """
+        '''
+        Delete an existing subscription
+
+        @param subscription_id The id of the subscription
+        @retval success Boolean to indicate successful deletion
+        @throws NotFound when subscription doesn't exist
+        '''
         # Return Value
         # ------------
         # {success: true}
         #
-        pass
+        subscription_obj = self.read_subscription(subscription_id)
+        if subscription_obj is None:
+            raise NotFound("Subscription %d does not exist" % subscription_id)
+
+        return self.clients.resource_registry.delete(subscription_obj)
 
     def activate_subscription(self, subscription_id=''):
-        """
-        method docstring
-        """
+        '''
+        Activate a subscription
+
+        @param subscription_id The id of the subscription
+        @retval success Boolean to indicate successful activation
+        @throws NotFound when subscription doesn't exist
+        '''
         # Return Value
         # ------------
         # {success: true}
         #
-        pass
+        subscription_obj = self.read_subscription(subscription_id)
+        if subscription_obj is None:
+            raise NotFound("Subscription %d does not exist" % subscription_id)
 
     def deactivate_subscription(self, subscription_id=''):
-        """
-        method docstring
-        """
+        '''
+        Deactivate a subscription
+
+        @param subscription_id The id of the subscription
+        @retval success Boolean to indicate successful deactivation
+        @throws NotFound when subscription doesn't exist
+        '''
         # Return Value
         # ------------
         # {success: true}
         #
-        pass
+        subscription_obj = self.read_subscription(subscription_id)
+        if subscription_obj is None:
+            raise NotFound("Subscription %d does not exist" % subscription_id)
 
     def register_consumer(self, exchange_name=''):
         """
@@ -191,30 +252,59 @@ class PubsubManagementService(BasePubsubManagementService):
         pass
 
     def register_producer(self, exchange_name='', stream_id=''):
-        """
-        method docstring
-        """
-        # logic to create credentials for a publisher to use to place data onto stream.
+        '''
+        Register a producer with a stream
+
+        @param exchange_name The producer exchange name to register
+        @param stream_id The id of the stream
+        @retval credentials Credentials for a publisher to use
+        @throws NotFound when stream doesn't exist
+        '''
+        # logic to create credentials for a publisher to use
+        # to place data onto stream.
         # return mock credentials
+        stream_obj = self.read_stream(stream_id)
+        if stream_obj is None:
+            raise NotFound("Stream %d does not exist" % stream_id)
+
+        stream_obj.producers.append(exchange_name)
         return "credentials"
 
     def unregister_producer(self, exchange_name='', stream_id=''):
-        """
-        method docstring
-        """
+        '''
+        Unregister a producer with a stream
+
+        @param exchange_name The producer exchange name to unregister
+        @param stream_id The id of the stream
+        @retval success Boolean to indicate successful unregistration
+        @throws NotFound when stream doesn't exist
+        '''
         # Return Value
         # ------------
         # {success: true}
         #
-        pass
+        stream_obj = self.read_stream(stream_id)
+        if stream_obj is None:
+            raise NotFound("Stream %d does not exist" % stream_id)
+
+        stream_obj.producers.remove(exchange_name)
+        return True
 
     def find_producers_by_stream(self, stream_id=''):
-        """
-        method docstring
-        """
+        '''
+        Return the list of producers for a stream
+
+        @param stream_id The id of the stream
+        @retval producer_list List of producers for the stream
+        @throws NotFound when stream doesn't exist
+        '''
         # Return Value
         # ------------
         # producer_list: []
         #
-        pass
-  
+        stream_obj = self.read_stream(stream_id)
+        if stream_obj is None:
+            raise NotFound("Stream %d does not exist" % stream_id)
+
+        return stream_obj.producers
+        
