74ac274b4df0654935c266853bf0b7c198b7d1c1
Tim Giguere <tgiguere@asascience.com>
2011-12-02 17:14:58 -0500

    PubSub implementation and test

--- ion/services/dm/distribution/test/test_pubsub.py
+++ ion/services/dm/distribution/test/test_pubsub.py
@@ -1,13 +1,10 @@
+from mock import Mock
 from pyon.container.cc import IContainerAgent
 from pyon.net.endpoint import ProcessRPCClient
 from pyon.public import Container
-from pyon.test.pyontest import PyonTestCase
-from interface.services.dm.ipubsub_management_service \
-    import IPubsubManagementService
-from interface.services.coi.iresource_registry_service \
-    import IResourceRegistryService
+from pyon.util.unit_test import PyonTestCase
 from pyon.public import log
-
+from ion.services.dm.distribution.pubsub_management_service import PubsubManagementService
 
 class FakeProcess(object):
     name = ''
@@ -15,7 +12,14 @@ class FakeProcess(object):
     def get_context(self):
         pass
 
+'''
+To run test_pubsub, run the following commands in pycc
 
+cc.start_rel_from_url('res/deploy/pubsub.yml')
+from ion.services.dm.distribution.test.test_pubsub import PubSubTest
+instance = PubSubTest()
+instance.test_runStream(cc)
+'''
 class PubSubTest(PyonTestCase):
 
     def setUp(self):
@@ -39,65 +43,76 @@ class PubSubTest(PyonTestCase):
     def runTest(self):
         pass
 
-    def runStream(self, container):
+    def test_runStream(self, container):
         # Now create client to pubsub service
-        client = ProcessRPCClient(node=container.node,
-                                  name="pubsub",
-                                  iface=IPubsubManagementService,
-                                  process=FakeProcess())
-        #resourceRegistryClient = ProcessRPCClient(node=container.node,
-        #                                          name="resourceregistry",
-        #                                          iface=IResourceRegistryService,
-        #                                          process=FakeProcess())
+        #client = ProcessRPCClient(node=container.node,
+        #                          name="pubsub",
+        #                          iface=IPubsubManagementService,
+        #                          process=FakeProcess())
+        self.mock_ionobj = self._create_IonObject_mock('ion.services.dm.distribution.pubsub_management_service.IonObject')
+        self._create_service_mock('pubsub_management')
 
-        self.createPubSubStream(client)
-        self.readPubSubStream(client)
-        self.updatePubSubStream(client)
-        #self.deletePubSubStream(client)
-        self.createSubscription(client)
-        self.readSubscription(client)
-        self.updateSubscription(client)
-        #self.deleteSubscription(client)
-        self.registerProducer(client)
-        self.unregisterProducer(client)
+        self.pubsub_service = PubsubManagementService()
+        self.pubsub_service.clients = self.clients
 
-    def createPubSubStream(self, client):
-        #stream_resource_dict = {"mimetype": "", "encoding": "", "archive" : archive, "url" : "", "name" : "SampleStream", "description" : "Sample Stream In PubSub"}
-        producers = ['a', 'b', 'c']
+        # Rename to save some typing
+        self.mock_create = self.resource_registry.create
+        self.mock_create_association = self.resource_registry.create_association
+        self.mock_read = self.resource_registry.read
+        self.mock_update = self.resource_registry.update
+        self.mock_find_objects = self.resource_registry.find_objects
+        self.mock_find_subjects = self.resource_registry.find_subjects
+        self.mock_find_resources = self.resource_registry.find_resources
+
+        self.createPubSubStream()
+        self.readPubSubStream()
+        self.updatePubSubStream()
+        self.deletePubSubStream()
+        self.createSubscription()
+        self.readSubscription()
+        self.updateSubscription()
+        self.deleteSubscription()
+        self.registerProducer()
+        self.unregisterProducer()
+
+    def createPubSubStream(self):
+        self.mock_create.return_value = ('id_2', 'I do not care')
+        producers = ['producer1', 'producer2', 'producer3']
         stream_resource_dict = {"mimetype": "", "name": "SampleStream", "description": "Sample Stream In PubSub", "producers": producers}
-        self.streamID = client.create_stream(stream_resource_dict)
+        self.streamID = self.pubsub_service.create_stream(stream_resource_dict)
+        self.mock_find_resources.assert_called_once_with('Stream', None, self.streamID, True)
         
-    def readPubSubStream(self, client):
-        stream_obj = client.read_stream(self.streamID)
+    def readPubSubStream(self):
+        stream_obj = self.pubsub_service.read_stream(self.streamID)
 
-    def updatePubSubStream(self, client):
-        stream_obj = client.read_stream(self.streamID)
+    def updatePubSubStream(self):
+        stream_obj = self.pubsub_service.read_stream(self.streamID)
         stream_obj.name = "SampleStream2"
         stream_obj.description = "Updated Sample Stream"
-        client.update_stream(stream_obj)
+        self.pubsub_service.update_stream(stream_obj)
 
-    def deletePubSubStream(self, client):
-        client.delete_stream(self.streamID)
+    def deletePubSubStream(self):
+        self.pubsub_service.delete_stream(self.streamID)
 
-    def createSubscription(self, client):
+    def createSubscription(self):
         subscription_resource_dict = {"name": "SampleSubscription", "description": "Sample Subscription In PubSub"}
-        self.subscriptionID = client.create_subscription(subscription_resource_dict)
+        self.subscriptionID = self.pubsub_service.create_subscription(subscription_resource_dict)
 
-    def readSubscription(self, client):
-        subscription_obj = client.read_subscription(self.subscriptionID)
+    def readSubscription(self):
+        subscription_obj = self.pubsub_service.read_subscription(self.subscriptionID)
 
-    def updateSubscription(self, client):
-        subscription_obj = client.read_subscription(self.subscriptionID)
+    def updateSubscription(self):
+        subscription_obj = self.pubsub_service.read_subscription(self.subscriptionID)
         subscription_obj.name = "SampleSubscription2"
         subscription_obj.description = "Updated Sample Subscription"
-        client.update_subscription(subscription_obj)
-        subscription_obj = client.read_subscription(self.subscriptionID)
+        self.pubsub_service.update_subscription(subscription_obj)
+        subscription_obj = self.pubsub_service.read_subscription(self.subscriptionID)
 
-    def deleteSubscription(self, client):
-        client.delete_subscription(self.subscriptionID)
+    def deleteSubscription(self):
+        self.pubsub_service.delete_subscription(self.subscriptionID)
 
-    def registerProducer(self, client):
-        client.register_producer("Test Producer", self.streamID)
+    def registerProducer(self):
+        self.pubsub_service.register_producer("Test Producer", self.streamID)
 
-    def unregisterProducer(self, client):
-        client.unregister_producer("Test Producer", self.streamID)
+    def unregisterProducer(self):
+        self.pubsub_service.unregister_producer("Test Producer", self.streamID)
