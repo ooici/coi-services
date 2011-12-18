84b973f7eafda0cd60c7500e4cda06d51dc8426d
Alon Yaari <ayaari@ucsd.edu>
2011-12-18 12:53:40 -0800

    create and read for data process mgmt service

--- ion/services/sa/data_process_management/data_process_management_service.py
+++ ion/services/sa/data_process_management/data_process_management_service.py
@@ -3,6 +3,8 @@
 __author__ = 'Maurice Manning'
 __license__ = 'Apache 2.0'
 
+import time
+
 from pyon.util.log import log
 from interface.services.sa.idata_process_management_service  import BaseDataProcessManagementService
 from pyon.datastore.datastore import DataStore
@@ -17,57 +19,109 @@ class DataProcessManagementService(BaseDataProcessManagementService):
     """
 
     def create_data_process(self, data_process={}, in_subscription_id='', out_data_product_id=''):
-        """method docstring
         """
-        @param      data_process dict defining the general resource and the specific transform
-        @param      transform    dict specifying which existing transform definition to apply
-        @param      data_product_in     ordered list of data product IDs matching inputs
-        @param      data_product_out    ID of data product to publish process output to
-        @retval     data_process_id     ID of the created data process object
+        @param      data_process          dict defining the general resource and specific transform
+        @param      in_subscription_id    ordered list of data product IDs matching inputs
+        @param      out_data_product_id   ID of data product to publish process output to
+        @retval     data_process_id       ID of the created data process object
         """
-        # Before calling create, the following must exist:
-        #       - output data product
-        #       - input data product(s)
-        #       - definition of the data transform being applied
+        log.debug("DataProcessManagementService:create_data_process: " + str(data_process))
 
-        result, asso = self.clients.resource_registry.find_by_name(data_product["name"], "DataProcess", True)
-        if len(result) != 0:
-            raise BadRequest("Data process '%s' already exists" % data_product["name"])
+        # Validation
+        if not "name"
+        if not data_process[atbd_reference]:
+            raise BadRequest("Invalid ATBD reference " + str(data_process[atbd_reference]))
+            # TODO: Check that ATBD reference is valid
+        # TODO: Replace dict definition with a transform object definition
 
-        pass
+        # Assemble transform input data
+        transform_name = str (data_process.atbd_reference) + \
+                         " - " + \
+                         str (out_data_product_id) +\
+                         time.ctime()
+        transform_object = IonObject (RT.Transform, name = transform_name)
+        transform_object.atbd_reference = data_process.atbd_reference
+        transform_object.sourcecode = data_process.sourcecode
+        transform_object.in_subscription_id = in_subscription_id
+        transform_object.out_data_product_id = out_data_product_id
+
+        # Register the transform with the transform mgmt service
+        transform_id = self.clients.transform_management_service.create_transform(transform_object)
+
+        # Create and store a new DataProcess with the resource registry
+        data_process_id, version = self.clients.resource_registry.create(data_process)
+
+        # TODO: Flesh out more details of transform mgmt service's schedule and bind methods
+        self.clients.transform_management_service.schedule_transform(transform_id)
+        self.clients.transform_management_service.bind_transform(transform_id)
+
+        # Register data process as a producer
+        self.clients.data_acquisition_management.register_process(data_process_id)
+
+        # Associations
+        self.clients.resource_registry.create_association(data_process_id,
+                                                          AT.hasInputProduct,
+                                                          in_subscription_id)
+        self.clients.resource_registry.create_association(data_process_id,
+                                                          AT.hasOutputProduct,
+                                                          out_data_product_id)
+        self.clients.resource_registry.create_association(data_process_id,
+                                                          AT.hasTransform,
+                                                          transform_id)
+
+        return data_process_id
 
     def update_data_process(self, data_process={}):
-        """method docstring
         """
         @param      data_process dict which defines the general resource and the specific transform
         @retval     {success: True or False}
         """
-        pass
+        log.debug("DataProcessManagementService:update_data_process: " + str(data_process))
+        data_process_id = self.clients.resource_registry.update(data_process)
+        transform_ids, _ = self.clients.resource_registry.find_associations(data_process_id, AT.hasTransform)
+        if not transform_ids:
+            raise NotFound("No transform associated with data process ID " + str (data_process_id))
+        goodUpdate = True
+        for x in transform_ids:
+            transform_obj = self.clients.transform_management_service.read_transform(x)
+            transform_obj.atbd_reference = data_process.atbd_reference
+            transform_obj.sourcecode = data_process.sourcecode
+            transform_obj.in_subscription_id = in_subscription_id
+            transform_obj.out_data_product_id = out_data_product_id
+            goodUpdate = goodUpdate & \
+                         self.clients.transform_management_service.update_transform(transform_obj)
+        return goodUpdate
 
     def read_data_process(self, data_process_id=''):
         """
         @param      data_process_id: ID of the data process resource of interest
         @retval     data_process dict populated with values known about the requested process
         """
-        pass
+        log.debug("DataProcessManagementService:read_data_process: " + str(data_process_id))
+        data_process_obj = self.clients.resource_registry.read(data_process_id)
+        if not data_process_obj:
+            raise NotFound("Data Process does not exist " + str(data_process_id))
+        return data_process_obj
 
     def delete_data_process(self, data_process_id=''):
         """
         @param      data_process_id: ID of the data process resource to be stopped
         @retval     {success: True or False}
         """
-        pass
+        log.debug("DataProcessManagementService:delete_data_process: " + str(data_process_id))
+
 
     def find_data_process(self, filters={}):
         """
         @param      filters: dict of parameters used to filter down the list of possible data proc.
         @retval
         """
+        log.debug("DataProcessManagementService:find_data_process")
         # Return Value
         # ------------
         # data_process_list: []
         #
-        pass
+
 
     def attach_process(self, process=''):
         """
@@ -76,3 +130,9 @@ class DataProcessManagementService(BaseDataProcessManagementService):
         """
         # TODO: Determine the proper input param
         pass
+
+    def prepare_transform(self, in_transform):
+        """
+        @param      in_transform    transform object as passed in
+        #retval     transform
+        """
