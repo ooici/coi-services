e4ff051717546f246e5e3eb6f8901f16530f05b6
Stephen Henrie <shenrie@ucsd.edu>
2011-12-21 17:27:04 -0800

    Implemented the CRUD operations for Policy and UserRole

--- ion/services/coi/policy_management_service.py
+++ ion/services/coi/policy_management_service.py
@@ -5,134 +5,179 @@ __author__ = 'Stephen P. Henrie'
 __license__ = 'Apache 2.0'
 
 from interface.services.coi.ipolicy_management_service import BasePolicyManagementService
+from pyon.core.exception import Conflict, Inconsistent, NotFound
+from pyon.public import AT, RT
+from pyon.util.log import log
 
 class PolicyManagementService(BasePolicyManagementService):
 
-
     def create_policy(self, policy={}, org_id=''):
-        """ Should receive a Policy object
+        """Persists the provided Policy object for the specified Org id. The id string returned
+        is the internal id by which Policy will be indentified in the data store.
+
+        @param policy    Policy
+        @param org_id    str
+        @retval policy_id    str
+        @throws BadRequest    if object passed has _id or _rev attribute
         """
-        # Return Value
-        # ------------
-        # {policy_id: ''}
-        #
-        pass
+        policy_id, version = self.clients.resource_registry.create(policy)
+        return policy_id
 
     def update_policy(self, policy={}):
-        """ Should receive a Policy object
+        """Updates the provided Policy object.  Throws NotFound exception if
+        an existing version of Policy is not found.  Throws Conflict if
+        the provided Policy object is not based on the latest persisted
+        version of the object.
+
+        @param policy    Policy
+        @throws NotFound    object with specified id does not exist
+        @throws BadRequest    if object does not have _id or _rev attribute
+        @throws Conflict    object not based on latest persisted object version
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
-        pass
+        self.clients.resource_registry.update(policy)
 
     def read_policy(self, policy_id=''):
-        """ Should return a Policy object
+        """Returns the Policy object for the specified policy id.
+        Throws exception if id does not match any persisted Policy
+        objects.
+
+        @param policy_id    str
+        @retval policy    Policy
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # policy: {}
-        #
-        pass
+        policy = self.clients.resource_registry.read(policy_id)
+        if not policy:
+            raise NotFound("Policy %s does not exist" % policy_id)
+        return policy
 
     def delete_policy(self, policy_id=''):
-        """method docstring
+        """For now, permanently deletes Policy object with the specified
+        id. Throws exception if id does not match any persisted Policy.
+
+        @param policy_id    str
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
-        pass
+        policy = self.clients.resource_registry.read(policy_id)
+        if not policy:
+            raise NotFound("Policy %s does not exist" % policy_id)
+        self.clients.resource_registry.delete(policy)
 
     def enable_policy(self, policy_id=''):
-        """method docstring
+        """Advances the lifecycle state of the specified Policy object to be enabled. Only
+        enabled policies should be considered by the policy engine.
+
+        @param policy_id    str
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
         pass
 
+
     def disable_policy(self, policy_id=''):
-        """method docstring
+        """Advances the lifecycle state of the specified Policy object to be disabled. Only
+        enabled policies should be considered by the policy engine.
+
+        @param policy_id    str
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
         pass
 
     def create_role(self, role={}):
-        """ Should receive a UserRole object
+        """Persists the provided UserRole object. The id string returned
+        is the internal id by which a UserRole will be indentified in the data store.
+
+        @param role    UserRole
+        @retval role_id    str
+        @throws BadRequest    if object passed has _id or _rev attribute
         """
-        # Return Value
-        # ------------
-        # {role_id: ''}
-        #
-        pass
+        role_id, version = self.clients.resource_registry.create(role)
+        return role_id
 
     def update_role(self, role={}):
-        """ Should receive a UserRole object
+        """Updates the provided UserRole object.  Throws NotFound exception if
+        an existing version of UserRole is not found.  Throws Conflict if
+        the provided UserRole object is not based on the latest persisted
+        version of the object.
+
+        @param role    UserRole
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        @throws BadRequest    if object does not have _id or _rev attribute
+        @throws Conflict    object not based on latest persisted object version
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
-        pass
+        self.clients.resource_registry.update(role)
 
     def read_role(self, role_id=''):
-        """ Should return a UserRole object
+        """Returns the UserRole object for the specified role id.
+        Throws exception if id does not match any persisted UserRole
+        objects.
+
+        @param role_id    str
+        @retval role    UserRole
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # role: {}
-        #
-        pass
+        role = self.clients.resource_registry.read(role_id)
+        if not role:
+            raise NotFound("Role %s does not exist" % role_id)
+        return role
 
     def delete_role(self, role_id=''):
-        """method docstring
-        """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
-        pass
+        """For now, permanently deletes UserRole object with the specified
+        id. Throws exception if id does not match any persisted UserRole.
 
-    def grant_role(self, org_id='', user_id='', role_id='', scope={}):
-        """  Should receive a RoleScope object
+        @param role_id    str
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
+        role = self.clients.resource_registry.read(role_id)
+        if not role:
+            raise NotFound("Role %s does not exist" % role_id)
+        self.clients.resource_registry.delete(role)
+
+
+    def grant_role(self, org_id='', role_id='', user_id='', scope={}):
+        """Grants a defined role within an organization to a specific user. Will throw a not NotFound exception
+        if none of the specified ids do not exist.
+
+        @param org_id    str
+        @param role_id    str
+        @param user_id    str
+        @param scope    RoleScope
+        @throws NotFound    object with specified id does not exist
+        """
         pass
 
     def revoke_role(self, org_id='', user_id='', role_id=''):
-        """method docstring
+        """Revokes a defined role within an organization to a specific user. Will throw a not NotFound exception
+        if none of the specified ids do not exist.
+
+        @param org_id    str
+        @param user_id    str
+        @param role_id    str
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # {success: true}
-        #
         pass
 
     def find_roles_by_user(self, org_id='', user_id=''):
-        """method docstring
+        """Returns a list of orgaization roles for a specific user. Will throw a not NotFound exception
+        if none of the specified ids do not exist.
+
+        @param org_id    str
+        @param user_id    str
+        @retval role_list    []
+        @throws NotFound    object with specified id does not exist
         """
-        # Return Value
-        # ------------
-        # role_list: []
-        #
         pass
 
-    def has_permission(self, org_id='', user_id='', action_id='', resource_id=''):
-        """method docstring
-        """
-        # Return Value
-        # ------------
-        # {has_permission: false}
-        #
-        pass
 
+    def has_permission(self, org_id='', action_id='', user_id='', resource_id=''):
+        """Returns a boolean of the specified user has permission for the specified action on a specified resource. Will
+        throw a NotFound exception if none of the specified ids do not exist.
+
+        @param org_id    str
+        @param action_id    str
+        @param user_id    str
+        @param resource_id    str
+        @retval has_permission    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        pass
 
