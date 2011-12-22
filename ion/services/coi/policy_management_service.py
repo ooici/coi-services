3eddf2db521add35c0ca5d5a4525a0034168626f
Stephen Henrie <shenrie@ucsd.edu>
2011-12-22 09:41:27 -0800

    Cleaning up Policy Management Service interfaces

--- ion/services/coi/policy_management_service.py
+++ ion/services/coi/policy_management_service.py
@@ -81,100 +81,100 @@ class PolicyManagementService(BasePolicyManagementService):
         """
         pass
 
-    def create_role(self, role={}):
+    def create_role(self, user_role={}):
         """Persists the provided UserRole object. The id string returned
         is the internal id by which a UserRole will be indentified in the data store.
 
-        @param role    UserRole
-        @retval role_id    str
+        @param user_role    UserRole
+        @retval user_role_id    str
         @throws BadRequest    if object passed has _id or _rev attribute
         """
-        role_id, version = self.clients.resource_registry.create(role)
-        return role_id
+        user_role_id, version = self.clients.resource_registry.create(user_role)
+        return user_role_id
 
-    def update_role(self, role={}):
+    def update_role(self, user_role={}):
         """Updates the provided UserRole object.  Throws NotFound exception if
         an existing version of UserRole is not found.  Throws Conflict if
         the provided UserRole object is not based on the latest persisted
         version of the object.
 
-        @param role    UserRole
+        @param user_role    UserRole
         @retval success    bool
         @throws NotFound    object with specified id does not exist
         @throws BadRequest    if object does not have _id or _rev attribute
         @throws Conflict    object not based on latest persisted object version
         """
-        self.clients.resource_registry.update(role)
+        self.clients.resource_registry.update(user_role)
 
-    def read_role(self, role_id=''):
+    def read_role(self, user_role_id=''):
         """Returns the UserRole object for the specified role id.
         Throws exception if id does not match any persisted UserRole
         objects.
 
-        @param role_id    str
-        @retval role    UserRole
+        @param user_role_id    str
+        @retval user_role    UserRole
         @throws NotFound    object with specified id does not exist
         """
-        role = self.clients.resource_registry.read(role_id)
+        user_role = self.clients.resource_registry.read(user_role_id)
         if not role:
-            raise NotFound("Role %s does not exist" % role_id)
-        return role
+            raise NotFound("Role %s does not exist" % user_role_id)
+        return user_role
 
-    def delete_role(self, role_id=''):
+    def delete_role(self, user_role_id=''):
         """For now, permanently deletes UserRole object with the specified
         id. Throws exception if id does not match any persisted UserRole.
 
-        @param role_id    str
+        @param user_role_id    str
         @throws NotFound    object with specified id does not exist
         """
-        role = self.clients.resource_registry.read(role_id)
-        if not role:
-            raise NotFound("Role %s does not exist" % role_id)
-        self.clients.resource_registry.delete(role)
+        user_role = self.clients.resource_registry.read(user_role_id)
+        if not user_role:
+            raise NotFound("Role %s does not exist" % user_role_id)
+        self.clients.resource_registry.delete(user_role)
 
 
-    def grant_role(self, org_id='', role_id='', user_id='', scope={}):
+    def grant_role(self, org_id='', user_id='', user_role_id='', scope={}):
         """Grants a defined role within an organization to a specific user. Will throw a not NotFound exception
         if none of the specified ids do not exist.
 
         @param org_id    str
-        @param role_id    str
         @param user_id    str
+        @param user_role_id    str
         @param scope    RoleScope
         @throws NotFound    object with specified id does not exist
         """
         pass
 
-    def revoke_role(self, org_id='', user_id='', role_id=''):
+    def revoke_role(self, org_id='', user_id='', user_role_id=''):
         """Revokes a defined role within an organization to a specific user. Will throw a not NotFound exception
         if none of the specified ids do not exist.
 
         @param org_id    str
         @param user_id    str
-        @param role_id    str
+        @param user_role_id    str
         @throws NotFound    object with specified id does not exist
         """
         pass
 
     def find_roles_by_user(self, org_id='', user_id=''):
-        """Returns a list of orgaization roles for a specific user. Will throw a not NotFound exception
+        """Returns a list of organization roles for a specific user. Will throw a not NotFound exception
         if none of the specified ids do not exist.
 
         @param org_id    str
         @param user_id    str
-        @retval role_list    []
+        @retval user_role_list    []
         @throws NotFound    object with specified id does not exist
         """
         pass
 
 
-    def has_permission(self, org_id='', action_id='', user_id='', resource_id=''):
+    def has_permission(self, org_id='', user_id='', action_id='', resource_id=''):
         """Returns a boolean of the specified user has permission for the specified action on a specified resource. Will
         throw a NotFound exception if none of the specified ids do not exist.
 
         @param org_id    str
-        @param action_id    str
         @param user_id    str
+        @param action_id    str
         @param resource_id    str
         @retval has_permission    bool
         @throws NotFound    object with specified id does not exist
