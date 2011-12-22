1392da782c229c334714bea975899da1e3c3ca38
Stephen Henrie <shenrie@ucsd.edu>
2011-12-22 12:28:27 -0800

    Implementing initial Org Service CRUD operations

--- ion/services/coi/org_management_service.py
+++ ion/services/coi/org_management_service.py
@@ -5,51 +5,238 @@ __author__ = 'Michael Meisinger'
 from pyon.public import CFG, IonObject, log, RT, AT
 
 from interface.services.coi.iorg_management_service import BaseOrgManagementService
+from pyon.core.exception import Conflict, Inconsistent, NotFound
+from pyon.public import AT, RT
+from pyon.util.log import log
 
 class OrgManagementService(BaseOrgManagementService):
 
     def create_org(self, org={}):
-        log.debug("create_org" + org.name)
-        rid,rev = self.clients.resource_registry.create(org)
-        return rid
+        """Persists the provided Org object. The id string returned
+        is the internal id by which Org will be identified in the data store.
+
+        @param org    Org
+        @retval org_id    str
+        @throws BadRequest    if object passed has _id or _rev attribute
+        """
+        org_id, version = self.clients.resource_registry.create(org)
+        return org_id
+
+    def update_org(self, org={}):
+        """Updates the provided Org object.  Throws NotFound exception if
+        an existing version of Org is not found.  Throws Conflict if
+        the provided Policy object is not based on the latest persisted
+        version of the object.
+
+        @param org    Org
+        @throws BadRequest    if object does not have _id or _rev attribute
+        @throws NotFound    object with specified id does not exist
+        @throws Conflict    object not based on latest persisted object version
+        """
+        self.clients.resource_registry.update(org)
+
+    def read_org(self, org_id=''):
+        """Returns the Org object for the specified policy id.
+        Throws exception if id does not match any persisted Policy
+        objects.
+
+        @param org_id    str
+        @retval org    Org
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
+        return org
+
+    def delete_org(self, org_id=''):
+        """Permanently deletes Org object with the specified
+        id. Throws exception if id does not match any persisted Policy.
+
+        @param org_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
+        self.clients.resource_registry.delete(org)
 
     def affiliate_org(self, org_id='', affiliate_org_id=''):
-        # Return Value
-        # ------------
-        # null
-        # ...
-        # 
-        pass
+        """Creates an association between multiple Orgs as an affiliation
+        so that they may coordinate activities between them.
+        Throws a NotFound exception if neither id is found.
+
+        @param org_id    str
+        @param affiliate_org_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org1 = self.clients.resource_registry.read(org_id)
+        if not org1:
+            raise NotFound("Org %s does not exist" % org_id)
+
+        org2 = self.clients.resource_registry.read(affiliate_org_id)
+        if not org2:
+            raise NotFound("Org %s does not exist" % affiliate_org_id)
+
+        aid = self.clients.resource_registry.create_association(org_id, AT.hasOrgAffiliation, affiliate_org_id)
+        if not aid:
+            return False
+
+        return True
+
+
+    def unaffiliate_org(self, org_id='', affiliate_org_id=''):
+        """Removes an association between multiple Orgs as an affiliation.
+        Throws a NotFound exception if neither id is found.
+
+        @param org_id    str
+        @param affiliate_org_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org1 = self.clients.resource_registry.read(org_id)
+        if not org1:
+            raise NotFound("Org %s does not exist" % org_id)
+
+        org2 = self.clients.resource_registry.read(affiliate_org_id)
+        if not org2:
+            raise NotFound("Org %s does not exist" % affiliate_org_id)
+
+        aid = self.clients.resource_registry.get_association(org_id, AT.hasOrgAffiliation, affiliate_org_id)
+        if not aid:
+            raise NotFound("The affiliation association between the specified Orgs is not found")
+
+        aid2 = self.clients.resource_registry.delete_association(aid)
+        if not aid2:
+            return False
+
+        return True
 
     def enroll_member(self, org_id='', user_id=''):
-        # Return Value
-        # ------------
-        # null
-        # ...
-        #
-        pass
+        """Enrolls a specified user into the specified Org so that they may find and negotiate to use resources
+        of the Org. Throws a NotFound exception if neither id is found.
+
+        @param org_id    str
+        @param user_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
+
+        user = self.clients.resource_registry.read(user_id)
+        if not user:
+            raise NotFound("User %s does not exist" % user_id)
+
+        aid = self.clients.resource_registry.create_association(org_id, AT.hasMembership, user_id)
+        if not aid:
+            return False
+
+        return True
+
+    def cancel_member_enrollment(self, org_id='', user_id=''):
+        """Cancels the membership of a specifid user within the specified Org. Once canceled, the user will no longer
+        have access to the resource of that Org. Throws a NotFound exception if neither id is found.
+
+        @param org_id    str
+        @param user_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
+
+        user = self.clients.resource_registry.read(user_id)
+        if not user:
+            raise NotFound("User %s does not exist" % user_id)
+
+        aid = self.clients.resource_registry.get_association(org_id, AT.hasMembership, user_id)
+        if not aid:
+            raise NotFound("The membership association between the specified user and Org is not found")
+
+        aid2 = self.clients.resource_registry.delete_association(aid)
+        if not aid2:
+            return False
+
+        return True
+
+    def is_enrolled(self, org_id='', user_id=''):
+        """Returns True if the specified user_id is enrolled in the Org and False if not.
+        Throws a NotFound exception if neither id is found.
+
+        @param org_id    str
+        @param user_id    str
+        @retval is_enrolled    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
+
+        user = self.clients.resource_registry.read(user_id)
+        if not user:
+            raise NotFound("User %s does not exist" % user_id)
+
+        aid = self.clients.resource_registry.get_association(org_id, AT.hasMembership, user_id)
+        if not aid:
+            return False
+
+        return True
+    
 
     def share_resource(self, org_id='', resource_id=''):
-        # Return Value
-        # ------------
-        # null
-        # ...
-        #
-        pass
+        """Share a specified resource with the specified Org. Once shared, the resource will be added to a directory
+        of available resources within the Org. Throws a NotFound exception if neither id is found.
 
-    def leave(self, org_id='', user_id=''):
-        # Return Value
-        # ------------
-        # null
-        # ...
-        #
-        pass
+        @param org_id    str
+        @param resource_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
 
-    def has_permission(self, org_id='', user_id='', action_id='', resource_id=''):
-        # Return Value
-        # ------------
-        # {has_permission: false}
-        # 
-        pass
+        resource = self.clients.resource_registry.read(resource_id)
+        if not resource:
+            raise NotFound("Resource %s does not exist" % resource_id)
+
+        aid = self.clients.resource_registry.create_association(org_id, AT.hasResource, resource_id)
+        if not aid:
+            return False
+
+        return True
+
+    def unshare_resource(self, org_id='', resource_id=''):
+        """Unshare a specified resource with the specified Org. Once unshared, the resource will be removed from a directory
+        of available resources within the Org. Throws a NotFound exception if neither id is found.
+
+        @param org_id    str
+        @param resource_id    str
+        @retval success    bool
+        @throws NotFound    object with specified id does not exist
+        """
+        org = self.clients.resource_registry.read(org_id)
+        if not org:
+            raise NotFound("Org %s does not exist" % org_id)
+
+        resource = self.clients.resource_registry.read(resource_id)
+        if not resource:
+            raise NotFound("Resource %s does not exist" % resource_id)
+
+        aid = self.clients.resource_registry.get_association(org_id, AT.hasMembership, resource_id)
+        if not aid:
+            raise NotFound("The shared association between the specified resource and Org is not found")
+
+        aid2 = self.clients.resource_registry.delete_association(aid)
+        if not aid2:
+            return False
+
+        return True
+
 
 
---
+++ ion/services/coi/test/test_org_management_service.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+__author__ = 'Stephen P. Henrie'
+__license__ = 'Apache 2.0'
+
+import unittest
+from mock import Mock, patch
+from pyon.util.unit_test import PyonTestCase
+from nose.plugins.attrib import attr
+
+from pyon.core.exception import BadRequest, Conflict, Inconsistent, NotFound
+from pyon.public import AT, RT
+from ion.services.coi.org_management_service import OrgManagementService
+
+
+@attr('UNIT', group='coi')
+class TestOrgManagementService(PyonTestCase):
+
+    def setUp(self):
+        mock_clients = self._create_service_mock('org_management')
+
+        self.org_management_service = OrgManagementService()
+        self.org_management_service.clients = mock_clients
+
+        # Rename to save some typing
+        self.mock_create = mock_clients.resource_registry.create
+        self.mock_read = mock_clients.resource_registry.read
+        self.mock_update = mock_clients.resource_registry.update
+        self.mock_delete = mock_clients.resource_registry.delete
+        self.mock_create_association = mock_clients.resource_registry.create_association
+        self.mock_delete_association = mock_clients.resource_registry.delete_association
+        self.mock_find_objects = mock_clients.resource_registry.find_objects
+        self.mock_find_resources = mock_clients.resource_registry.find_resources
+        self.mock_find_subjects = mock_clients.resource_registry.find_subjects
+
+        # Exchange Space
+        self.org = Mock()
+        self.org.name = "Foo"
+
+
+    def test_create_exchange_space(self):
+        self.mock_create.return_value = ['111', 1]
+
+        org_id = self.org_management_service.create_org(self.org)
+
+        assert org_id == '111'
+        self.mock_create.assert_called_once_with(self.org)
+
+    def test_read_and_update_exchange_space(self):
+        self.mock_read.return_value = self.org
+
+        org = self.org_management_service.read_org('111')
+
+        assert org is self.mock_read.return_value
+        self.mock_read.assert_called_once_with('111', '')
+
+        org.name = 'Bar'
+
+        self.mock_update.return_value = ['111', 2]
+
+        self.org_management_service.update_org(org)
+
+        self.mock_update.assert_called_once_with(org)
+
+    def test_delete_exchange_space(self):
+        self.mock_read.return_value = self.org
+
+        self.org_management_service.delete_org('111')
+
+        self.mock_read.assert_called_once_with('111', '')
+        self.mock_delete.assert_called_once_with(self.org)
+
+    def test_read_exchange_space_not_found(self):
+        self.mock_read.return_value = None
+
+        # TEST: Execute the service operation call
+        with self.assertRaises(NotFound) as cm:
+            self.org_management_service.read_org('bad')
+
+        ex = cm.exception
+        self.assertEqual(ex.message, 'Org bad does not exist')
+        self.mock_read.assert_called_once_with('bad', '')
+
+    def test_delete_exchange_space_not_found(self):
+        self.mock_read.return_value = None
+
+        # TEST: Execute the service operation call
+        with self.assertRaises(NotFound) as cm:
+            self.org_management_service.delete_org('bad')
+
+        ex = cm.exception
+        self.assertEqual(ex.message, 'Org bad does not exist')
+        self.mock_read.assert_called_once_with('bad', '')
+
+
