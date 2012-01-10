874365a7ba5c817e01f92f71f1cf47aa5831bf38
Luke Campbell <luke.s.campbell@gmail.com>
2012-01-10 11:54:38 -0500

    Squashed all TMS work Rebase with a7ea26c106...
    Further implementation and interfacing
    
    Added interface calls to some CRUD methods
    Mocked interfaces for pubsub, proc dispatch
    starting building some core functionality into it.
    mock bind_transform
    
    More concrete implementation and mock interfaces.
    
    rough implementation for bind_transform
    
    Implemented rough version of schedule_transform
    
    For now the interface is mocked, and the ProcessSchedule is made up.
    
    More implementation to TMS to match expectations
    
    - Molded TMS to match the calls in sa.dpms a little better
    - Better fit with pubsub
    
    Fixed bind_transform
    
    - bind now uses the supplied out_stream_id in the Transform Object
    
    Further refinement of interface interactions
    
    TMS is now integrated with pubsub
    
    refactoring with new design in mind-
    
    fixed the schedule resource type issue
    
    Reworked several TMS methods
    
    - create_transform.
    - delete_transform.
    - implemented association mapping.
    
    fixed bind_transform, added unit test

--- ion/services/dm/transformation/test/test_transform_service.py
+++ ion/services/dm/transformation/test/test_transform_service.py
@@ -3,11 +3,16 @@
 @file ion/services/dm/transformation/test_transform_service.py
 @description Unit Test for Transform Management Service
 '''
-from pyon.core.exception import NotFound
-
+from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
+from interface.services.dm.itransform_management_service import TransformManagementServiceClient
+from interface.services.icontainer_agent import ContainerAgentClient
+from pyon.core.exception import NotFound, BadRequest
+from pyon.util.containers import DotDict
+from pyon.public import IonObject, RT, log, AT
 from pyon.util.unit_test import PyonTestCase
 from mock import Mock
 from nose.plugins.attrib import attr
+from pyon.util.int_test import IonIntegrationTestCase
 from ion.services.dm.transformation.transform_management_service import TransformManagementService
 import unittest
 
@@ -20,60 +25,150 @@ class TransformManagementServiceTest(PyonTestCase):
         mock_clients = self._create_service_mock('transform_management_service')
         self.transform_service = TransformManagementService()
         self.transform_service.clients = mock_clients
+        self.transform_service.clients.pubsub_management = DotDict()
+        self.transform_service.clients.pubsub_management['XP'] = 'science.data'
+        self.transform_service.clients.pubsub_management['create_stream'] = Mock()
+        self.transform_service.clients.pubsub_management['create_subscription'] = Mock()
+        self.transform_service.clients.pubsub_management['register_producer'] = Mock()
+        self.transform_service.clients.pubsub_management['activate_subscription'] = Mock()
         # CRUD Shortcuts
-        self.mock_create = self.transform_service.clients.resource_registry.create
-        self.mock_read = self.transform_service.clients.resource_registry.read
-        self.mock_update = self.transform_service.clients.resource_registry.update
-        self.mock_delete = self.transform_service.clients.resource_registry.delete
+        self.mock_rr_create = self.transform_service.clients.resource_registry.create
+        self.mock_rr_read = self.transform_service.clients.resource_registry.read
+        self.mock_rr_update = self.transform_service.clients.resource_registry.update
+        self.mock_rr_delete = self.transform_service.clients.resource_registry.delete
+        self.mock_rr_find = self.transform_service.clients.resource_registry.find_objects
+        self.mock_rr_assoc = self.transform_service.clients.resource_registry.find_associations
+        self.mock_rr_create_assoc = self.transform_service.clients.resource_registry.create_association
+        self.mock_rr_del_assoc = self.transform_service.clients.resource_registry.delete_association
+
+        self.mock_pd_create = self.transform_service.clients.process_dispatcher_service.create_process_definition
+        self.mock_pd_read = self.transform_service.clients.process_dispatcher_service.read_process_definition
+        self.mock_pd_update = self.transform_service.clients.process_dispatcher_service.update_process_definition
+        self.mock_pd_delete = self.transform_service.clients.process_dispatcher_service.delete_process_definition
+        self.mock_pd_schedule = self.transform_service.clients.process_dispatcher_service.schedule_process
+        self.mock_pd_cancel = self.transform_service.clients.process_dispatcher_service.cancel_process
+
+        self.mock_ps_create_stream = self.transform_service.clients.pubsub_management.create_stream
+        self.mock_ps_create_sub = self.transform_service.clients.pubsub_management.create_subscription
+        self.mock_ps_register = self.transform_service.clients.pubsub_management.register_producer
+        self.mock_ps_activate = self.transform_service.clients.pubsub_management.activate_subscription
 
     def test_create_transform(self):
-        self.mock_create.return_value = ['transform_id',1]
+        # mocks
+        self.mock_pd_schedule.return_value = 'mock_pid'
+        self.mock_rr_create.return_value = ('transform_id','rev')
+        predicates = [AT.hasProcessDefinition, AT.hasSubscription, AT.hasOutStream]
+        def create_assoc(*args, **kwargs):
+            self.assertEquals(args[1],predicates.pop(0))
+        self.mock_rr_create_assoc.side_effect = create_assoc
 
-        transform_id = self.transform_service.create_transform(transform={})
-        self.mock_create.assert_called_with({})
-        self.assertEquals(transform_id,'transform_id','create_transform does not pass the id correctly to return.')
+        # execution
+        res = self.transform_service.create_transform('subscription_id','out_stream','process_definition_id',
+                                                        {'name':'test_transform'})
+
+        # assertions
+        # it should create the association between the ids and the transform_id
+        self.assertEquals(self.mock_rr_create_assoc.call_count,3)
+        # it should launch the process
+        self.assertTrue(self.mock_pd_schedule.called)
 
 
     def test_update_transform(self):
-        self.mock_update.return_value = ['transform_id',2]
+        with self.assertRaises(NotImplementedError):
+            self.transform_service.update_transform()
 
-        transform_id = self.transform_service.update_transform(transform={})
-        self.mock_update.assert_called_with({})
-        self.assertEquals(transform_id,'transform_id','update_transform does not correctly update the object.')
 
     def test_read_transform(self):
-        self.mock_read.return_value = 'TransformObject'
+        # mocks
+        self.mock_rr_read.return_value = 'mock_object'
 
-        ret = self.transform_service.read_transform(transform_id='transform_id')
-        self.mock_read.assert_called_with('transform_id','')
-        self.assertEquals('TransformObject',ret)
+        # execution
+        ret = self.transform_service.read_transform('transform_id')
 
-        def side_effect(*args, **kwargs):
-           raise NotFound
-
-        self.mock_read.side_effect = side_effect
-
-        # Ensure that read_transform throws NotFound when the transform doesn't exist
-
-        with self.assertRaises(NotFound):
-           self.transform_service.read_transform(transform_id='not_here')
+        # assertions
+        self.mock_rr_read.assert_called_with('transform_id','')
+        self.assertEquals(ret,'mock_object')
 
 
     def test_delete_transform(self):
-        self.mock_read.return_value = 'transform_object'
-        self.transform_service.delete_transform(transform_id="transform_id")
-        self.mock_delete.assert_called_with('transform_object')
-        def side_effect(*args,**kwargs):
-           raise NotFound
-        self.mock_read.side_effect = side_effect
-        with self.assertRaises(NotFound):
-           self.transform_service.delete_transform(transform_id='blank')
-            
-    @unittest.skip('bind_transform not implemented yet')
-    def test_bind_transform(self):
-        pass
+        # mocks
+        mock_id_list = [(['proc_def_id'],'rev'),
+                        (['sub_id'],'rev'),
+                        (['stream_id'],'rev')]
+        def mock_ids(*args,**kwargs):
+            return mock_id_list.pop()
+        self.mock_rr_find.side_effect = mock_ids
 
-    @unittest.skip('schedule_transform not implemented yet')
-    def test_schedule_transform(self):
-        pass
+        self.transform_service.read_transform = Mock()
+        self.transform_service.read_transform.return_value = DotDict({'process_id':'123'})
+
+        associations=[['first'],['second'],['third']]
+        def mock_association(*args,**kwargs):
+            return associations.pop()
+        self.mock_rr_assoc.side_effect = mock_association
+
+        # execution
+        self.transform_service.delete_transform('mock_transform_id')
+
+        # assertions
+        self.transform_service.read_transform.assert_called_with(transform_id='mock_transform_id')
+        self.assertEquals(self.mock_rr_find.call_count,3)
+        self.mock_pd_cancel.assert_called_with('123')
+        self.assertEquals(self.mock_rr_del_assoc.call_count,3)
+        self.assertEquals(self.mock_rr_delete.call_count,4)
+
+        # ends case checking
+        #@todo: fill this out
+
             
+
+    def test_bind_transform(self):
+        # mocks
+        self.mock_rr_find.return_value = [['id'],'garbage']
+
+        # execution
+        ret = self.transform_service.bind_transform('transform_id')
+
+        # assertions
+        self.mock_rr_find.assert_called_with('transform_id',AT.hasSubscription,RT.Subscription,True)
+        self.mock_ps_activate.assert_called_with('id')
+
+
+
+    def test_schedule_transform(self):
+        # not implemented
+        with self.assertRaises(NotImplementedError):
+            self.transform_service.schedule_transform()
+
+@attr('INT', group='dm')
+class TransformManagementServiceIntTest(IonIntegrationTestCase):
+    pass
+#    def setUp(self):
+#        # start the container
+#        self._start_container()
+#
+#        # Establish endpoint with container
+#        self.container_client = ContainerAgentClient(node=self.container.node,name=self.container.name)
+#        self.container_client.start_rel_from_url('res/deploy/r2deploy.yml')
+#
+#        self.tms_client = TransformManagementServiceClient(node=self.container.node)
+#        self.pubsub_client = PubsubManagementServiceClient(node=self.container.node)
+#
+#    def test_createTransform(self):
+#        # Make an output stream with pubsub
+#        output_stream = IonObject(RT.Stream,name='test_product_stream_out')
+#        output_stream.original = True
+#        output_stream.producers = ['science.data']
+#
+#        output_stream_id = self.pubsub_client.create_stream(output_stream)
+#
+#        # Create the transform object
+#        transform = IonObject(RT.Transform,name='test_transform')
+#        transform.out_stream_id = output_stream_id
+#        transform_id = self.tms_client.create_transform(transform)
+#
+#        # Bind the transform object
+#        self.tms_client.bind_transform(transform_id)
+#
+#
+#
