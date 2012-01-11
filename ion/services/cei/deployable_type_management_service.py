a7ea26c106275c107862da7149f2d5d6d9587560
Stephen Henrie <shenrie@ucsd.edu>
2012-01-11 09:55:33 -0800

    Adding skelton implementation for Deployable Type Management Service

---
+++ ion/services/cei/deployable_type_management_service.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+__author__ = 'Stephen P. Henrie'
+__license__ = 'Apache 2.0'
+
+from interface.services.cei.ideployable_type_management_service import BaseDeployableTypeManagementService
+
+class DeployableTypeManagementService(BaseDeployableTypeManagementService):
+    pass
