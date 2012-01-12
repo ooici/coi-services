cd8f635673bdf0c71dbec90f86ec2c00108722d6
Bill Bollenbacher <wbollenbacher@ucsd.edu>
2012-01-11 17:39:12 -0800

    initial integration tests

--- ion/services/sa/process/test/test_int_data_process_management_service.py
+++ ion/services/sa/process/test/test_int_data_process_management_service.py
@@ -9,40 +9,39 @@
 from nose.plugins.attrib import attr
 from pyon.util.int_test import IonIntegrationTestCase
 import unittest
+from interface.services.icontainer_agent import ContainerAgentClient
+from interface.services.sa.idata_process_management_service import DataProcessManagementServiceClient
+from pyon.public import Container, log, IonObject
+from pyon.public import RT, AT, LCS
+from pyon.core.exception import BadRequest, NotFound, Conflict
 
 @attr('INT', group='sa')
 class TestIntDataProcessManagementService(IonIntegrationTestCase):
 
     def setUp(self):
-        pass
+        # Start container
+        self._start_container()
+        #print 'started container'
 
-    @unittest.skip('Not done yet.')
-    def test_create_update_delete_data_process(self):
-        # Test the creation of a calibration data process- it brings level0 data to level1
-        #   Prerequisites
-        #       - A level0 data product (raw parsed data coming out of an instrument) must
-        #               be streaming into an existing data product
-        #   create() Test:
-        #       - Test will create a new data product
-        #       - Call create_data_process(), passing in the level0 data product, the new
-        #               data product, and a data transform definition
-        #   create() Identification of Success:
-        #       - Data process stored in the resource registry
-        #       - Transform stored in the resource registry
-        #       - Data stream published by the new data product is a match for data being
-        #           published into the level0 data product (plus the calibration transform)
-        #   update() Test:
-        #       - Test creates yet another data product
-        #       - Call update_data_process(), passing in the new data product to publish to
-        #   update() Identification of Success:
-        #       - Data process association changed from original data product to new one
-        #       - Original data product has no active data stream
-        #       - The new data product has active data stream matching the level0 product
-        #   delete() Test:
-        #       - Test deletes the data process
-        #   delete() Identification of Success:
-        #       - Data product continues to exist but no longer publishes
-        #       - Child transform is no longer in the resource registry
-        #       - Data process is no longer in the resource registry
-        #       - Association to the data process no longer exist
-        pass
+        # Establish endpoint with container
+        container_client = ContainerAgentClient(node=self.container.node, name=self.container.name)
+        #print 'got CC client'
+        container_client.start_rel_from_url('res/deploy/r2sa.yml')
+        
+        print 'started services'
+
+        # Now create client to DataProcessManagementService
+        self.client = DataProcessManagementServiceClient(node=self.container.node)
+
+    def test_createDataProcess(self):
+        # test creating a new data process 
+        print 'Creating new data process definition'
+        dpd_obj = IonObject(RT.DataProcessDefinition, 
+                           name='DPD1', 
+                           description='some new dpd')
+        try:
+            dpd_id = self.client.create_data_process_definition(dpd_obj)
+        except BadRequest as ex:
+            self.fail("failed to create new data process definition: %s" %ex)
+        print 'new dpd_id = ', dpd_id
+
