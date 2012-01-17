8fcfeef6c54eb1d5fc1a79c623ebac138cdf7094
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-01-17 15:42:16 -0500

    Reset hard for extern/iondefinitions... some changes in ingestion_management_service.py

--- ion/services/dm/ingestion/ingestion_management_service.py
+++ ion/services/dm/ingestion/ingestion_management_service.py
@@ -16,14 +16,20 @@ class IngestionManagementService(BaseIngestionManagementService):
     class docstring
     """
 
-    def create_ingestion_configuration(self, exchange_point_id, couch_storage, filesystem, root_path, hdf_storage,\
-                                    server, database, number_of_workers = 0, default_policy = StreamIngestionPolicy):
-        """
-        Setup ingestion workers to ingest all the data from a single exchange point.
+    def create_ingestion_configuration(self, exchange_point_id='', couch_storage={}, hfd_storage={}, \
+                                       number_of_workers=0, default_policy={}):
+        """Setup ingestion workers to ingest all the data from a single exchange point.
+
+        @param exchange_point_id    str
+        @param couch_storage    Unknown
+        @param hfd_storage    Unknown
+        @param number_of_workers    int
+        @param default_policy    Unknown
+        @retval ingestion_configuration_id    str
         """
         # create an ingestion_configuration instance and update the registry
-        ingestion_configuration = IngestionConfiguration(exchange_point_id, couch_storage, filesystem, root_path, \
-            hdf_storage, server, database, number_of_workers, default_policy = StreamIngestionPolicy)
+        ingestion_configuration = IonObject(RT.IngestionConfiguration,exchange_point_id = exchange_point_id, couch_storage, hfd_storage, \
+            number_of_workers, default_policy)
 
         id, rev = self.clients.resource_registry.create(ingestion_configuration)
 
@@ -33,22 +39,20 @@ class IngestionManagementService(BaseIngestionManagementService):
 
         return ingestion_configuration._id
 
-    def update_ingestion_configuration(self, ingestion_configuration):
+    def update_ingestion_configuration(self, ingestion_configuration={}):
+        """Change the number of workers or the default policy for ingesting data on each stream
+
+        @param ingestion_configuration    IngestionConfiguration
         """
-        Change the number of workers or the default policy for ingesting data on each stream
-        """
-        log.debug("Updating ingestion configuration: %s" % ingestion_configuration.id)
+        log.debug("Updating ingestion configuration at exchange point: %s" % ingestion_configuration['exchange_point_id'])
         id, rev = self.clients.resource_registry.update(ingestion_configuration)
 
-        # update the ingestion_configuration with its new id
-        ingestion_configuration._id = id
-        ingestion_configuration._rev = rev
+    def read_ingestion_configuration(self, ingestion_configuration_id=''):
+        """Get an existing ingestion configuration object.
 
-        return ingestion_configuration._id
-
-    def read_ingestion_configuration(self, ingestion_configuration_id):
-        """
-        Get an existing ingestion configuration object.
+        @param ingestion_configuration_id    str
+        @retval ingestion_configuration    IngestionConfiguration
+        @throws NotFound    if ingestion configuration did not exist
         """
         log.debug("Reading ingestion configuration object id: %s", ingestion_configuration_id)
         ingestion_configuration = self.clients.resource_registry.read(ingestion_configuration_id)
@@ -56,9 +60,11 @@ class IngestionManagementService(BaseIngestionManagementService):
             raise NotFound("Ingestion configuration %s does not exist" % ingestion_configuration_id)
         return ingestion_configuration
 
-    def delete_ingestion_configuration(self, ingestion_configuration_id):
-        """
-        Delete an existing ingestion configuration object.
+    def delete_ingestion_configuration(self, ingestion_configuration_id=''):
+        """Delete an existing ingestion configuration object.
+
+        @param ingestion_configuration_id    str
+        @throws NotFound    if ingestion configuration did not exist
         """
         log.debug("Deleting ingestion configuration: %s", ingestion_configuration_id)
         ingestion_configuration = self.read_ingestion_configuration(ingestion_configuration_id)
@@ -66,31 +72,41 @@ class IngestionManagementService(BaseIngestionManagementService):
             raise NotFound("Ingestion configuration %d does not exist" % ingestion_configuration_id)
 
         self.clients.resource_registry.delete(ingestion_configuration)
-        return True
 
-    def create_stream_policy(self, stream_id, archive_data, archive_metadata):
-        """
-        Create a policy for a particular stream and associate it to the ingestion configuration for the
-        exchange point the stream is on. (After LCA)
+
+    def create_stream_policy(self, stream_id='', archive_data='', archive_metadata=''):
+        """Create a policy for a particular stream and associate it to the ingestion configuration for the exchange point the stream is on. (After LCA)
+
+        @param stream_id    str
+        @param archive_data    str
+        @param archive_metadata    str
+        @retval ingestion_policy_id    str
         """
 
 #        return ingestion_policy_id
 
-    def update_stream_policy(self, stream_policy):
-        """
-        Change the number of workers or the default policy for ingesting data on each stream (After LCA)
+    def update_stream_policy(self, stream_policy={}):
+        """Change the number of workers or the default policy for ingesting data on each stream (After LCA)
+
+        @param stream_policy    Unknown
+        @throws NotFound    if ingestion configuration did not exist
         """
 
 
-    def read_stream_policy(self, stream_policy_id):
-        """
-        Get an existing stream policy object. (After LCA)
+    def read_stream_policy(self, stream_policy_id=''):
+        """Get an existing stream policy object. (After LCA)
+
+        @param stream_policy_id    str
+        @retval ingestion_configuration    IngestionConfiguration
+        @throws NotFound    if ingestion configuration did not exist
         """
 
 #        return ingestion_configuration
 
-    def delete_stream_policy(self, ingestion_configuration_id):
-        """
-        Delete an existing stream policy object. (After LCA)
+    def delete_stream_policy(self, ingestion_configuration_id=''):
+        """Delete an existing stream policy object. (After LCA)
+
+        @param ingestion_configuration_id    str
+        @throws NotFound    if ingestion configuration did not exist
         """
 
