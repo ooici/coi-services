137690873fcd8a4788af8612b947fd055bd1899f
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-01-20 12:52:41 -0500

    Wrote the skeleton for ingestion_worker.py. Needs fleshing out in several places. Also, the script, ingestion_script.py needs modification

--- ion/services/dm/ingestion/ingestion_worker.py
+++ ion/services/dm/ingestion/ingestion_worker.py
@@ -14,6 +14,8 @@ send data far and wide.
 # import ScienceObject, ScienceObjectTransport, HDFEncoder, HDFDecoder, and the exceptions... i.e.
 # everything in science_object_codec.py
 
+from ion.services.dm.ingestion.ingestion_management_service import IngestionManagementService
+
 class IngestionWorker(object):
     """
     This Class creates an instance of a science object and science object transport
@@ -28,45 +30,71 @@ class IngestionWorker(object):
     Alternatively the instance can be killed and a new worker created.
     """
 
-    def __init__(self):
-        # store a list of configuration id's
-        self.ingestion_config_ids = []
-
-    def store_config_id(self, ingestion_configuration_id):
-        self.ingestion_config_ids.append(ingestion_configuration_id)
-
-    def refresh_config_id_store(self):
-        self.ingestion_config_ids = []
-
-    def take_science_object(self, ingestion_configuration_id, science_obj):
+    def find_ingestion_configuration_from_id(self, ingestion_configuration_id = ''):
         """
-        Accept an science object and an ingestion configuration id and do something
+        Returns an ingestion_configuration when provided an ingestion_configuration_id
         """
+        ingestion_configuration = self.clients.resource_registry.read(ingestion_configuration_id)
 
-        # Read the transformation params and define a transformation object
+        return ingestion_configuration
 
+    def process_stream(self, ingestion_configuration_id = '', incoming_stream = '', instruction = '', output_stream = ''):
+        """
+        Accepts a stream. Also accepts instruction as a string, and according to what is contained in the instruction,
+        it does things with the stream such as store in hfd_storage, couch_storage or process the data and return the
+        output.
+        @param: ingestion_configuration_id The id for the configuration of type string.
+        @param: incoming_stream The incoming data stream of type stream.
+        @param: instruction The instruction telling this method what to do with the incoming data stream.
+
+        @return output_stream: The output stream. This may or may not be returned depending on what the instruction says.
+        """
 
         # Do something for the preservation of the object
 
+        configuration = self.find_ingestion_configuration_from_id(ingestion_configuration_id)
 
-    def define_datastream_raw(self, raw_samples_stream):
+        # do things with the data stream
+
+        # store stuff in couch datastore or in hdf datastore according to the instruction and the configuration
+        # specifications
+
+
+        # return the output stream if that is what the instruction says
+        send_output_stream = True # change this line
+
+        if send_output_stream:
+            return output_stream
+        else:
+            return None
+
+    def work_on_datastream(self, ingestion_configuration_id, incoming_stream, output_stream):
         """
-        Defines a data stream object to send to the distribution service
-        and returns the datastream id obtained from distribution service
+        This method will be called by Transformation.
+
+        The method will be used to work on an incoming stream, and extract the instruction regarding what to do with
+        the datastream. According to the instruction, it will process the datastream and/or send output to whatever is
+        specified by the ingestion_configuration
         """
 
-    def read_params_from_metadata(self):
-        """
-        Reads the params from the metadata attribute in the science object
-        """
+        # extract the instruction from the stream
+        instruction = self.extract_instruction_from_stream(incoming_stream)
 
-    def define_datastream_product(self, raw_product_stream_id):
-        """
-        Defines a raw product stream and returns the raw_product_stream_id obtained from the distribution service
-        """
+        # now work on the data stream according to the extracted instruction
+        output_stream = self.process_stream(ingestion_configuration_id, incoming_stream, instruction, output_stream)
 
-    def define_preservation(self, params):
-        """
-        Defines a preservation object and returns a success/fail value obtained from the preservation service
-        """
+        # now return an output_stream if
+        if output_stream:
+            return output_stream
+        else:
+            return True
 
+    def extract_instruction_from_stream(self, incoming_stream):
+        """
+        Extracts and returns the instruction from the data stream
+        """
+        instruction = ''
+        # do some extraction stuff
+
+        # return the extracted instruction
+        return instruction
