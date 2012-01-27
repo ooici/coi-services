e1e2e47e514bedea3bb8db39009b51bd92a156a5
Roger Unwin <raunwin@ucsd.edu>
2012-01-27 10:56:25 -0800

    Now coping with _optional_ config file settings...

--- ion/services/coi/test/test_service_gateway.py
+++ ion/services/coi/test/test_service_gateway.py
@@ -9,6 +9,7 @@ import simplejson, urllib
 from pyon.util.int_test import IonIntegrationTestCase
 from nose.plugins.attrib import attr
 from interface.services.icontainer_agent import ContainerAgentClient
+from pyon.core.bootstrap import CFG
 
 
 @attr('INT', group='coi')
@@ -26,7 +27,16 @@ class test_service_gateway(IonIntegrationTestCase):
         self._stop_container()
 
     def test_list_resource_types(self):
-        SEARCH_BASE = 'http://localhost:5000/ion-service/list_resource_types'
+        port = 5000
+        hostname = 'localhost'
+        if 'web_server' in CFG:
+            web_server_cfg = CFG['web_server']
+            if web_server_cfg is not None:
+                if 'hostname' in web_server_cfg:
+                    hostname = web_server_cfg['hostname']
+                if 'port' in web_server_cfg:
+                    port = web_server_cfg['port']
+        SEARCH_BASE = 'http://' + hostname + ':' + str(port) + '/ion-service/list_resource_types'
         args = {}
         args.update({
            #'format': "unix",
