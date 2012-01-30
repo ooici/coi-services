e702ffbf73bf27b1b2a21a9f03a721bb90845fe8
Carlos Rueda <carueda@mbari.org>
2012-01-30 13:16:24 -0800

    reversed change in InstrumentProtocol.connect to keep the os.wait call, so as to facilitate merging with master

--- ion/services/mi/instrument_protocol.py
+++ ion/services/mi/instrument_protocol.py
@@ -106,8 +106,7 @@ class InstrumentProtocol(object):
         mi_logger.info('Found logger pid: %s.', str(logger_pid))
         if not logger_pid:
             self._logger_popen = self._logger.launch_process()
-            #-retval = os.wait()
-            retval = self._logger_popen.wait()
+            retval = os.wait()
             mi_logger.debug('os.wait returned %s', str(retval))
             mi_logger.debug('popen wait returned %s', str(self._logger_popen.wait()))
         time.sleep(1)         
