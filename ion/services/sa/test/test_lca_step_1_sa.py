e91124bac1107823dc8e7729b336ac31c53f279d
ijk5 <ijk5@mit.edu>
2012-01-31 10:52:46 -0800

    basic init of test

--- ion/services/sa/test/test_lca_step_1_sa.py
+++ ion/services/sa/test/test_lca_step_1_sa.py
@@ -1,18 +1,18 @@
-from interface.services.icontainer_agent import ContainerAgentClient
+#from interface.services.icontainer_agent import ContainerAgentClient
 #from pyon.net.endpoint import ProcessRPCClient
-from pyon.public import Container, log, IonObject
+from pyon.public import Container, IonObject
+from pyon.util.containers import DotDict
 from pyon.util.int_test import IonIntegrationTestCase
 
-from ion.services.sa.instrument.instrument_management_service import InstrumentManagementService
-from interface.services.sa.iinstrument_management_service import IInstrumentManagementService, InstrumentManagementServiceClient
+from interface.services.sa.idata_product_management_service import DataProductManagementServiceClient
+from interface.services.sa.idata_acquisition_management_service import DataAcquisitionManagementServiceClient
+from interface.services.sa.iinstrument_management_service import InstrumentManagementServiceClient
+from interface.services.sa.imarine_facility_management_service import MarineFacilityManagementServiceClient
 
 from pyon.util.context import LocalContextMixin
 from pyon.core.exception import BadRequest, NotFound, Conflict
 from pyon.public import RT, PRED, LCS
-from mock import Mock, patch
-from pyon.util.unit_test import PyonTestCase
 from nose.plugins.attrib import attr
-import unittest
 from pyon.util.log import log
 
 
@@ -27,6 +27,13 @@ class TestLCAStep1SA(IonIntegrationTestCase):
         # Start container
         self._start_container()
         self.container.start_rel_from_url('res/deploy/r2sa.yml')
+
+        # Now create client to DataProductManagementService
+        self.client = DotDict()
+        self.client.DAMS = DataAcquisitionManagementServiceClient(node=self.container.node)
+        self.client.DPMS = DataProductManagementServiceClient(node=self.container.node)
+        self.client.IMS  = InstrumentManagementServiceClient(node=self.container.node)
+        self.client.MFMS = MarineFacilityManagementServiceClient(node=self.container.node)
         
     def test_just_the_setup(self):
         return
