18efec0f4fb1bce2ec110642b5664ad39409eb72
Luke Campbell <luke.s.campbell@gmail.com>
2012-02-01 09:28:56 -0500

    Luke: commented out example scripts
    - bin/generate will RUN the scripts if they're exposed
    - need to either comment or wrap them in a function

--- ion/services/dm/ingestion/example/couch_datastore_functionality.py
+++ ion/services/dm/ingestion/example/couch_datastore_functionality.py
@@ -1,16 +1,16 @@
-__author__ = 'SChatterjee'
-
-from pyon.datastore.couchdb.couchdb_datastore import CouchDB_DataStore
-
-db = CouchDB_DataStore()
-
-# Creates a datastore
-
-datastore_name = 'dm_datastore'
-db.create_datastore(datastore_name)
-
-# Create a document
-document1 = {'BlogAuthor': {'name' : 'roger', 'email': 'rabbit.com'}}
-
-_id, _rev = db.create_doc(document1, None, datastore_name)
-
+#__author__ = 'SChatterjee'
+#
+#from pyon.datastore.couchdb.couchdb_datastore import CouchDB_DataStore
+#
+#db = CouchDB_DataStore()
+#
+## Creates a datastore
+#
+#datastore_name = 'dm_datastore'
+#db.create_datastore(datastore_name)
+#
+## Create a document
+#document1 = {'BlogAuthor': {'name' : 'roger', 'email': 'rabbit.com'}}
+#
+#_id, _rev = db.create_doc(document1, None, datastore_name)
+#
--- ion/services/dm/ingestion/example/ingestion_script_example.py
+++ ion/services/dm/ingestion/example/ingestion_script_example.py
@@ -1,27 +1,27 @@
-from interface.services.dm.iingestion_management_service import IngestionManagementServiceClient
-from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
-from interface.services.coi.iresource_registry_service import ResourceRegistryServiceClient
-
-
-from pyon.public import IonObject, RT, log, PRED
-from pyon.ion.endpoint import ProcessPublisher
-from pyon.public import log, StreamProcess
-
-#########################################################################################
-# Run this part first in the pycc container
-#########################################################################################
-ingestion_client = IngestionManagementServiceClient(node = cc.node)
-pubsub_client = PubsubManagementServiceClient(node=cc.node)
-
-ctd_output_stream_id = pubsub_client.create_stream(name='ctd_output_stream', original=True)
-
-# create transforms... queues will be created in this step
-ingestion_configuration_id = ingestion_client.create_ingestion_configuration(exchange_point_id='science_data', couch_storage={},\
-    hdf_storage={},  number_of_workers=2, default_policy={})
-# activates the transforms... so bindings will be created in this step
-ingestion_client.activate_ingestion_configuration(ingestion_configuration_id)
-
-# messages produced and published
-id_p = cc.spawn_process('ingestion_queue', 'ion.services.dm.ingestion.ingestion_example', 'IngestionExampleProducer',\
-        {'process': {'type':'stream_process', 'listen_name':'do_not_publish', 'publish_streams':{'out_stream':ctd_output_stream_id}},'stream_producer':{'interval':4000}})
-cc.proc_manager.procs['%s.%s' %(cc.id,id_p)].start()
+#from interface.services.dm.iingestion_management_service import IngestionManagementServiceClient
+#from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
+#from interface.services.coi.iresource_registry_service import ResourceRegistryServiceClient
+#
+#
+#from pyon.public import IonObject, RT, log, PRED
+#from pyon.ion.endpoint import ProcessPublisher
+#from pyon.public import log, StreamProcess
+#
+##########################################################################################
+## Run this part first in the pycc container
+##########################################################################################
+#ingestion_client = IngestionManagementServiceClient(node = cc.node)
+#pubsub_client = PubsubManagementServiceClient(node=cc.node)
+#
+#ctd_output_stream_id = pubsub_client.create_stream(name='ctd_output_stream', original=True)
+#
+## create transforms... queues will be created in this step
+#ingestion_configuration_id = ingestion_client.create_ingestion_configuration(exchange_point_id='science_data', couch_storage={},\
+#    hdf_storage={},  number_of_workers=2, default_policy={})
+## activates the transforms... so bindings will be created in this step
+#ingestion_client.activate_ingestion_configuration(ingestion_configuration_id)
+#
+## messages produced and published
+#id_p = cc.spawn_process('ingestion_queue', 'ion.services.dm.ingestion.ingestion_example', 'IngestionExampleProducer',\
+#        {'process': {'type':'stream_process', 'listen_name':'do_not_publish', 'publish_streams':{'out_stream':ctd_output_stream_id}},'stream_producer':{'interval':4000}})
+#cc.proc_manager.procs['%s.%s' %(cc.id,id_p)].start()
