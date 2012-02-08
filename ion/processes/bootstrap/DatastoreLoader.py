404b4c67c48531f170420b37d42ea975bca7d8e1
Michael Meisinger <mmeisinger@ucsd.edu>
2012-02-08 13:54:25 -0800

    DatastoreLoader refined

--- ion/processes/bootstrap/DatastoreLoader.py
+++ ion/processes/bootstrap/DatastoreLoader.py
@@ -15,6 +15,7 @@ Features
 """
 
 import yaml
+import datetime
 import os
 import os.path
 
@@ -32,25 +33,60 @@ class DatastoreLoader(StreamProcess):
     def on_quit(self, *args, **kwargs):
         pass
 
-def dump_datastore(ds_name, path=None, clear_dir=True):
-    if CFG.system.mockdb:
-        log.warn("Cannot dump from MockDB")
-        return
-    ds = DatastoreManager.get_datastore(ds_name)
-    if not ds:
-        return
-    outpath_base = "res/preload/local/dump"
-    outpath = "%s/%s" % (outpath_base, ds_name)
-    if not os.path.exists(outpath):
-        os.makedirs(outpath)
-    [os.remove(os.path.join(outpath, f)) for f in os.listdir(outpath)]
+    @classmethod
+    def load_datastore(cls, ds_name, path=None):
+        if CFG.system.mockdb:
+            log.warn("Cannot load into MockDB")
+            return
+        ds = DatastoreManager.get_datastore(ds_name)
+        if not ds:
+            return
+        path = path or "res/preload/default"
 
-    objs = ds.find_by_view("_all_docs", None, id_only=False, convert_doc=False)
-    numwrites = 0
-    for obj_id, obj_key, obj in objs:
-        if obj_id.startswith("_design"):
-            continue
-        with open("%s/%s.yml" % (outpath, obj_id), 'w') as f:
-            yaml.dump(obj, f, default_flow_style=False)
-            numwrites += 1
-    log.info("Wrote %s objects to %s" % (numwrites, outpath))
+
+    @classmethod
+    def dump_datastore(cls, ds_name=None, path=None, clear_dir=True):
+        """
+        Dumps CouchDB datastores into a directory as YML files.
+        @param ds_name Logical name (such as "resources") of an ION datastore
+        @param path Directory to put dumped datastores into (defaults to
+                    "res/preload/local/dump_[timestamp]")
+        @param clear_dir if True, delete contents of datastore dump dirs
+        """
+        if CFG.system.mockdb:
+            log.warn("Cannot dump from MockDB")
+            return
+        if not path:
+            dtstr = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+            path = "res/preload/local/dump_%s" % dtstr
+        if ds_name:
+            # 1 Check ds_name exists
+            ds = DatastoreManager.get_datastore("objects")
+            if ds.datastore_exists(ds_name):
+
+            cls._dump_datastore(ds, path, clear_dir)
+        else:
+            ds_list = [ds_name] if ds_name else ['resources','objects','state','events',]
+            for ds in ds_list:
+                cls._dump_datastore(ds, path, clear_dir)
+
+    @classmethod
+    def _dump_datastore(cls, ds_name, outpath_base, clear_dir=True):
+        ds = DatastoreManager.get_datastore(ds_name)
+        if not ds:
+            return
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+        [os.remove(os.path.join(outpath, f)) for f in os.listdir(outpath)]
+
+        outpath = "%s/%s" % (outpath_base, ds_name)
+
+        objs = ds.find_by_view("_all_docs", None, id_only=False, convert_doc=False)
+        numwrites = 0
+        for obj_id, obj_key, obj in objs:
+            if obj_id.startswith("_design"):
+                continue
+            with open("%s/%s.yml" % (outpath, obj_id), 'w') as f:
+                yaml.dump(obj, f, default_flow_style=False)
+                numwrites += 1
+        log.info("Wrote %s objects to %s" % (numwrites, outpath))
