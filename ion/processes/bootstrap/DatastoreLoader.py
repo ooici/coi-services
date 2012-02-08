ab5265ddbe4c39d32aa6af41a10d4d83d74ab199
Michael Meisinger <mmeisinger@ucsd.edu>
2012-02-08 08:08:05 -0800

    Initial datastore dumper

--- ion/processes/bootstrap/DatastoreLoader.py
+++ ion/processes/bootstrap/DatastoreLoader.py
@@ -14,7 +14,13 @@ Features
 - load a subset of objects by type, etc
 """
 
+import yaml
+import os
+import os.path
+
+from pyon.datastore.datastore import DataStore, DatastoreManager
 from pyon.public import CFG, IonObject, log, sys_name, RT, LCS, PRED, StreamProcess
+
 
 class DatastoreLoader(StreamProcess):
     def on_init(self, *args, **kwargs):
@@ -25,3 +31,26 @@ class DatastoreLoader(StreamProcess):
 
     def on_quit(self, *args, **kwargs):
         pass
+
+def dump_datastore(ds_name, path=None, clear_dir=True):
+    if CFG.system.mockdb:
+        log.warn("Cannot dump from MockDB")
+        return
+    ds = DatastoreManager.get_datastore(ds_name)
+    if not ds:
+        return
+    outpath_base = "res/preload/local/dump"
+    outpath = "%s/%s" % (outpath_base, ds_name)
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+    [os.remove(os.path.join(outpath, f)) for f in os.listdir(outpath)]
+
+    objs = ds.find_by_view("_all_docs", None, id_only=False, convert_doc=False)
+    numwrites = 0
+    for obj_id, obj_key, obj in objs:
+        if obj_id.startswith("_design"):
+            continue
+        with open("%s/%s.yml" % (outpath, obj_id), 'w') as f:
+            yaml.dump(obj, f, default_flow_style=False)
+            numwrites += 1
+    log.info("Wrote %s objects to %s" % (numwrites, outpath))
