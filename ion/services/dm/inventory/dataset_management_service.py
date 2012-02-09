75e47e93c8c659d0d6a79709d7896eafe136171c
Luke Campbell <luke.s.campbell@gmail.com>
2012-02-09 18:18:39 -0500

    Luke: Saved work.

--- ion/services/dm/inventory/dataset_management_service.py
+++ ion/services/dm/inventory/dataset_management_service.py
@@ -7,12 +7,16 @@ __license__ = 'Apache 2.0'
 
 from interface.services.dm.idataset_management_service import BaseDatasetManagementService
 from interface.objects import DataSet
+from pyon.datastore.datastore import DataStore
 
 class DatasetManagementService(BaseDatasetManagementService):
     def __init__(self, *args, **kwargs):
         super(DatasetManagementService, self).__init__(*args,**kwargs)
         self.logging_name = '(DatasetManagementService %s)' % (self.name or self.id)
 
+    def on_start(self):
+        super(DatasetManagementService,self).on_start()
+        self.db = self.container.datastore_manager.get_datastore('scidata',DataStore.DS_PROFILE.SCIDATA)
 
     """
     class docstring
@@ -77,9 +81,27 @@ class DatasetManagementService(BaseDatasetManagementService):
         @param dataset_id    str
         @retval bounds    Unknown
         """
-        dataset = self.read_dataset(dataset_id=dataset_id)
-        #@todo: Perform Query
-        return ''
+        #dataset = self.read_dataset(dataset_id=dataset_id)
+        bounds = {
+            'geo':{
+                'latmin':0,
+                'lonmin':0,
+                'latmax':0,
+                'lonmax':0
+            },
+            'height':[0,0],
+            'time':[0,0]
+        }
+
+        bounds['geo']['latmin'] = self.db.query_view('datasets/lattitude_min')[0]['value']
+        bounds['geo']['latmax'] = self.db.query_view('datasets/lattitude_max')[0]['value']
+        bounds['geo']['lonmin'] = self.db.query_view('datasets/longitude_min')[0]['value']
+        bounds['geo']['lonmax'] = self.db.query_view('datasets/longitude_max')[0]['value']
+        bounds['height'][0] = self.db.query_view('datasets/depth_min')[0]['value']
+        bounds['height'][1] = self.db.query_view('datasets/depth_max')[0]['value']
+        bounds['time'][0] = self.db.query_view('datasets/time_min')[0]['value']
+        bounds['time'][1] = self.db.query_view('datasets/time_max')[0]['value']
+        return bounds
 
     def get_dataset_metadata(self, dataset_id=''):
         """@brief Get the metadata for the dataset using a couch map/reduce query
