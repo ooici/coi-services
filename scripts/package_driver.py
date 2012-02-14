15795fe04b4c0b6dd11a903a96624c65065a3ed4
Bill French <wfrench@ucsd.edu>
2012-02-14 08:47:11 -0800

    added startup scripts for the IDK

---
+++ scripts/package_driver.py
@@ -0,0 +1,11 @@
+__author__ = 'Bill French'
+
+from ion.processes.idk.package_driver import PackageDriver
+
+def run():
+    app = PackageDriver()
+    app.run()
+   
+
+if __name__ == '__main__':
+    run()
---
+++ scripts/start_driver.py
@@ -0,0 +1,11 @@
+__author__ = 'Bill French'
+
+from ion.processes.idk.start_driver import StartDriver
+
+def run():
+    app = StartDriver()
+    app.run()
+   
+
+if __name__ == '__main__':
+    run()
