2f5a35cf0021c737af278e376a7589c7db2fb999
Edward Hunter <edhunter@ucsd.edu>
2012-02-17 11:54:55 -0800

    exception constructor bug

--- ion/services/mi/exceptions.py
+++ ion/services/mi/exceptions.py
@@ -40,7 +40,7 @@ class InstrumentStateException(InstrumentException):
 class InstrumentTimeoutException(InstrumentException):
     """Exception related to a command, request, or communication timing out"""
     def __init__(self, error_code=InstErrorCode.TIMEOUT, msg=None):
-        InstrumentException.__init__(error_code, msg)
+        InstrumentException.__init__(self, error_code, msg)
     
 class InstrumentDataException(InstrumentException):
     """Exception related to the data returned by an instrument or developed
