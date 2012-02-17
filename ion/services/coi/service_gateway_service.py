11ba059295681a777619d90073b8c1d369c5ed65
Stephen Henrie <shenrie@ucsd.edu>
2012-02-16 17:57:39 -0800

    Fixed Service Gateway finding correct client class and changed response format

--- ion/services/coi/service_gateway_service.py
+++ ion/services/coi/service_gateway_service.py
@@ -24,10 +24,11 @@ service_gateway_instance = None
 
 DEFAULT_WEB_SERVER_HOSTNAME = ""
 DEFAULT_WEB_SERVER_PORT = 5000
-DEFAULT_GATEWAY_REQUEST_ERROR = 'GatewayRequestError'
-DEFAULT_GATEWAY_REQUEST_ERROR = 'GatewayRequestError'
-DEFAULT_GATEWAY_REQUEST_ERROR_EXCEPTION = 'Exception'
-DEFAULT_GATEWAY_REQUEST_ERROR_MESSAGE = 'Message'
+
+DEFAULT_GATEWAY_RESPONSE = 'GatewayResponse'
+DEFAULT_GATEWAY_ERROR = 'GatewayError'
+DEFAULT_GATEWAY_ERROR_EXCEPTION = 'Exception'
+DEFAULT_GATEWAY_ERROR_MESSAGE = 'Message'
 
 #This class is used to manage the WSGI/Flask server as an ION process - and as a process endpoint for ION RPC calls
 class ServiceGatewayService(BaseServiceGatewayService):
@@ -108,7 +109,7 @@ def process_gateway_request(service_name, operation):
         #Retrieve service definition
         from pyon.core.bootstrap import service_registry
         # MM: Note: service_registry can do more now
-        target_service = service_registry.get_service_base(service_name)
+        target_service = service_registry.get_service_by_name(service_name)
 
         if not target_service:
             raise NotFound("Target service name not found in the URL")
@@ -118,14 +119,16 @@ def process_gateway_request(service_name, operation):
 
 
         #Find the concrete client class for making the RPC calls.
-        target_client = None
-        for name, cls in inspect.getmembers(inspect.getmodule(target_service),inspect.isclass):
-            if issubclass(cls, ProcessRPCClient) and not name.endswith('ProcessRPCClient'):
-                target_client = cls
-                break
+        #target_client = None
+        #for name, cls in inspect.getmembers(inspect.getmodule(target_service),inspect.isclass):
+        #    if issubclass(cls, ProcessRPCClient) and not name.endswith('ProcessRPCClient'):
+        #        target_client = cls
+        #        break
 
-        if not target_client:
-            raise NotFound("Service operation not correctly specified in the URL")
+        if not target_service.client:
+            raise NotFound("Cannot find a client class for the specified service: %s", service_name )
+
+        target_client = target_service.client
 
         #Retrieve json data from HTTP Post payload
         json_params = None
@@ -135,11 +138,20 @@ def process_gateway_request(service_name, operation):
             #payload = '{"serviceRequest": { "serviceName": "resource_registry", "serviceOp": "find_resources", "params": { "restype": "BankAccount", "lcstate": "", "name": "", "id_only": false } } }'
             json_params = json.loads(payload)
 
+            if not json_params.has_key('serviceRequest'):
+                raise Inconsistent("The JSON request is missing the 'serviceRequest' key in the request")
+
+            if not json_params['serviceRequest'].has_key('serviceName'):
+                raise Inconsistent("The JSON request is missing the 'serviceName' key in the request")
+
+            if not json_params['serviceRequest'].has_key('serviceOp'):
+                raise Inconsistent("The JSON request is missing the 'serviceOp' key in the request")
+
             if json_params['serviceRequest']['serviceName'] != target_service.name:
-                 raise Inconsistent("Target service name in the JSON request (%s) does not match service name in URL (%s)" % (str(json_params['serviceRequest']['serviceName']), target_service.name ) )
+                raise Inconsistent("Target service name in the JSON request (%s) does not match service name in URL (%s)" % (str(json_params['serviceRequest']['serviceName']), target_service.name ) )
 
             if json_params['serviceRequest']['serviceOp'] != operation:
-                 raise Inconsistent("Target service operation in the JSON request (%s) does not match service name in URL (%s)" % ( str(json_params['serviceRequest']['serviceOp']), operation ) )
+                raise Inconsistent("Target service operation in the JSON request (%s) does not match service name in URL (%s)" % ( str(json_params['serviceRequest']['serviceOp']), operation ) )
 
         param_list = create_parameter_list(service_name, target_client,operation, json_params)
 
@@ -147,15 +159,20 @@ def process_gateway_request(service_name, operation):
         methodToCall = getattr(client, operation)
         result = methodToCall(**param_list)
 
+        return json_response({DEFAULT_GATEWAY_RESPONSE: result})
+
+
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        result = { DEFAULT_GATEWAY_REQUEST_ERROR :
-                    { DEFAULT_GATEWAY_REQUEST_ERROR_EXCEPTION : exc_type.__name__,
-                      DEFAULT_GATEWAY_REQUEST_ERROR_MESSAGE : str(e.message)
-                    }
-                 }
+        result = {
+            DEFAULT_GATEWAY_ERROR_EXCEPTION : exc_type.__name__,
+            DEFAULT_GATEWAY_ERROR_MESSAGE : str(e.message)
+            }
 
-    return json_response(result)
+        return json_response({ DEFAULT_GATEWAY_ERROR :result } )
+
+
+
 
 #Private implementation of standard flask jsonify to specify the use of an encoder to walk ION objects
 def json_response(response_data):
@@ -259,16 +276,17 @@ def list_resource_types():
         for res in sorted(resultSet):
             ret_list.append(res)
 
-        return json_response(ret_list)
+
+        return json_response({ DEFAULT_GATEWAY_RESPONSE :ret_list } )
+
 
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        ret = { DEFAULT_GATEWAY_REQUEST_ERROR :
-                           { DEFAULT_GATEWAY_REQUEST_ERROR_EXCEPTION : exc_type.__name__,
-                             DEFAULT_GATEWAY_REQUEST_ERROR_MESSAGE : str(e.message)
-                       }
-        }
-        return json_response(ret)
+        ret = {
+                DEFAULT_GATEWAY_ERROR_EXCEPTION : exc_type.__name__,
+                DEFAULT_GATEWAY_ERROR_MESSAGE : str(e.message)
+               }
+        return json_response({ DEFAULT_GATEWAY_ERROR :  ret } )
 
 
 #Returns a json object for a specified resource type with all default values.
@@ -295,15 +313,16 @@ def get_resource_schema(resource_type):
                         value = None
                     setattr(ret_obj, field, value)
 
+
+        return json_response({ DEFAULT_GATEWAY_ERROR :ret_list } )
+
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        ret_obj = { DEFAULT_GATEWAY_REQUEST_ERROR :
-                        { DEFAULT_GATEWAY_REQUEST_ERROR_EXCEPTION : exc_type.__name__,
-                          DEFAULT_GATEWAY_REQUEST_ERROR_MESSAGE : str(e.message)
-                    }
+        ret = {
+            DEFAULT_GATEWAY_ERROR_EXCEPTION : exc_type.__name__,
+            DEFAULT_GATEWAY_ERROR_MESSAGE : str(e.message)
         }
-
-    return json_response(ret_obj)
+        return json_response({ DEFAULT_GATEWAY_ERROR :  ret } )
 
 
 #More RESTfull examples...should probably not use but here for example reference
@@ -322,15 +341,16 @@ def get_resource(resource_id):
             if not result:
                 raise NotFound("No resource found for id: %s " % resource_id)
 
+            return json_response({ DEFAULT_GATEWAY_RESPONSE :result } )
+
         except Exception, e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
-            result = { DEFAULT_GATEWAY_REQUEST_ERROR :
-                            { DEFAULT_GATEWAY_REQUEST_ERROR_EXCEPTION : exc_type.__name__,
-                              DEFAULT_GATEWAY_REQUEST_ERROR_MESSAGE : str(e.message)
-                        }
-        }
+            ret = {
+                DEFAULT_GATEWAY_ERROR_EXCEPTION : exc_type.__name__,
+                DEFAULT_GATEWAY_ERROR_MESSAGE : str(e.message)
+            }
+            return json_response({ DEFAULT_GATEWAY_ERROR :  ret } )
 
-    return json_response(result)
 
 
 #Example operation to return a list of resources of a specific type like
@@ -348,15 +368,16 @@ def list_resources_by_type(resource_type):
         for res in res_list:
             result.append(res)
 
+        return json_response({ DEFAULT_GATEWAY_RESPONSE :result } )
+
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        result = { DEFAULT_GATEWAY_REQUEST_ERROR :
-                        { DEFAULT_GATEWAY_REQUEST_ERROR_EXCEPTION : exc_type.__name__,
-                          DEFAULT_GATEWAY_REQUEST_ERROR_MESSAGE : str(e.message)
-                    }
-    }
+        ret = {
+            DEFAULT_GATEWAY_ERROR_EXCEPTION : exc_type.__name__,
+            DEFAULT_GATEWAY_ERROR_MESSAGE : str(e.message)
+        }
+        return json_response({ DEFAULT_GATEWAY_ERROR :  ret } )
 
-    return json_response(result)
 
 #Example restful call to a client function for another service like
 #http://hostname:port/ion-service/run_bank_client
