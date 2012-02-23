31008dee60d5a3bc9d0b3bb9007ecaeca81fdfc0
Bill French <wfrench@ucsd.edu>
2012-02-23 15:10:39 -0800

    added command line option

--- scripts/start_driver.py
+++ scripts/start_driver.py
@@ -1,11 +1,25 @@
 __author__ = 'Bill French'
 
+import argparse
+
 from ion.processes.idk.start_driver import StartDriver
 
 def run():
     app = StartDriver()
-    app.run()
+    opts = parseArgs()
+
+    if( opts.overwrite ):
+        app.overwrite()
+    else:
+        app.run()
    
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="IDK Start Driver")
+    parser.add_argument("--overwrite", dest='overwrite', action="store_true",
+                        help="rewrite generated files from current metadata" )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
     run()
