83b4b7c00dc197c3aa42481b1ab8bb4164d6be49
Edward Hunter <edhunter@ucsd.edu>
2012-03-07 16:02:17 -0800

    test

--- ion/services/mi/drivers/sbe37_driver.py
+++ ion/services/mi/drivers/sbe37_driver.py
@@ -864,7 +864,7 @@ class SBE37Protocol(CommandResponseInstrumentProtocol):
             return
         """
         CommandResponseInstrumentProtocol._got_data(self, data)
-        
+        mi_logger.debug('got data: linebuf: %s', self._linebuf)
         # Only keep the latest characters in the prompt buffer.
         if len(self._promptbuf)>7:
             self._promptbuf = self._promptbuf[-7:]
