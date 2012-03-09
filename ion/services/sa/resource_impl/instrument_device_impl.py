473b52f737b8634d5d595d44f1c2abb253c813ca
Maurice Manning <mmmanning@ucsd.edu>
2012-03-08 16:34:29 -0800

    fix definition

--- ion/services/sa/resource_impl/instrument_device_impl.py
+++ ion/services/sa/resource_impl/instrument_device_impl.py
@@ -92,12 +92,6 @@ class InstrumentDeviceImpl(ResourceImpl):
     def unlink_model(self, instrument_device_id='', instrument_model_id=''):
         return self._unlink_resources(instrument_device_id, PRED.hasModel, instrument_model_id)
 
-    def link_logicalmodel(self, logical_instrument_id='', instrument_model_id=''):
-        return self._link_resources_single_object(logical_instrument_id, PRED.hasModel, instrument_model_id)
-
-    def unlink_logicalmodel(self, logical_instrument_id='', instrument_model_id=''):
-        return self._unlink_resources(logical_instrument_id, PRED.hasModel, instrument_model_id)
-
     def link_sensor(self, instrument_device_id='', sensor_device_id=''):
         return self._link_resources(instrument_device_id, PRED.hasSensor, sensor_device_id)
 
