f7d5b7f4e484b0e9328bb9b0cb7566c6a6c8dded
Luke Campbell <luke.s.campbell@gmail.com>
2012-03-16 16:17:51 -0400

    Luke: impl visualize function

--- ion/processes/data/dispatcher/dispatcher_launcher.py
+++ ion/processes/data/dispatcher/dispatcher_launcher.py
@@ -3,10 +3,11 @@
 @file ion/processes/data/dispatcher/dispatcher_launcher.py
 @description Launcher for the dispatcher infrastructure
 '''
-from interface.objects import ExchangeQuery, ProcessDefinition
+from interface.objects import ExchangeQuery, ProcessDefinition, DataProduct, StreamQuery
 from interface.services.cei.iprocess_dispatcher_service import ProcessDispatcherServiceClient
 from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
 from interface.services.dm.itransform_management_service import TransformManagementServiceClient
+from interface.services.sa.idata_product_management_service import DataProductManagementServiceClient
 from pyon.service.service import BaseService
 from prototype.sci_data.stream_defs import SBE37_CDM_stream_definition
 from ion.processes.data.dispatcher.dispatcher_cache import DISPATCH_DATASTORE
@@ -56,12 +57,31 @@ class DispatcherLauncher(BaseService):
 
     @staticmethod
     def visualize(stream_id):
+        import re
+        dpms_cli = DataProductManagementServiceClient()
         pubsub_cli = PubsubManagementServiceClient()
-        pd_cli= ProcessDispatcherServiceClient()
-        stream_definition = SBE37_CDM_stream_definition()
-        stream_definition_id = pubsub_cli.create_stream_definition(container=stream_definition)
-        dispatch_stream_id = pubsub_cli.create_stream(stream_definition_id=stream_definition_id)
+        tms_cli = TransformManagementServiceClient()
+        pd_cli = ProcessDispatcherServiceClient()
+        stream_definition_id = pubsub_cli.find_stream_definition(stream_id=stream_id)
+        en = re.sub('.data','.viz',stream_id)
+        subscription_id = pubsub_cli.create_subscription(query=StreamQuery(stream_ids=[stream_id]), exchange_name=en)
+        visual_product = DataProduct(name='visual product')
+        visual_product_id = dpms_cli.create_data_product(data_product=visual_product, stream_definition_id=stream_definition_id)
 
-        stream_definition.stream_resource_id = dispatch_stream_idstream_id
+        config = {
+            'data_product_id':visual_product_id,
+            'stream_def_id':stream_definition_id
+        }
 
+        proc_def = ProcessDefinition()
+        proc_def.executable['module'] = 'ion.services.ans.visualization_service'
+        proc_def.executable['class'] = 'VizTransformProcForMatplotlibGraphs'
+        proc_def_id = pd_cli.create_process_definition(process_definition=proc_def)
 
+        transform_id = tms_cli.create_transform(
+            name='%s_vis' % stream_id,
+            in_subscription_id=subscription_id,
+            process_definition_id=proc_def_id,
+            configuration=config
+        )
+        tms_cli.activate_transform(transform_id)
