f974da18333c590745e4e4fd66bec9b2c77b9bd8
Luke Campbell <luke.s.campbell@gmail.com>
2012-03-23 12:28:27 -0400

    Luke: Added utility for presenting JSON structures
    - JSON structures for web queries.

---
+++ ion/services/dm/utility/resource_tree.py
@@ -0,0 +1,26 @@
+'''
+@author Luke Campbell <lcampbell@asascience.com>
+@file ion/services/dm/utility/resource_tree.py
+@description Builds a D3 JSON Hierarchy Tree based on a resource
+'''
+from interface.services.coi.iresource_registry_service import ResourceRegistryServiceClient
+from ion.services.dm.utility.jsonify import JSONtree as jt
+
+def build(resource_id):
+    rr_cli = ResourceRegistryServiceClient()
+
+    resource = rr_cli.read(resource_id)
+    root = jt(resource.name or resource._id)
+    root.id = resource_id
+    obj_list, assoc_list = rr_cli.find_objects(subject=resource_id,id_only=True)
+
+    if not obj_list:
+        root.leaf = True
+        return root
+
+    for obj,assoc in zip(obj_list,assoc_list):
+        if obj:
+
+            root.add_child(build(obj),assoc.p)
+
+    return root
