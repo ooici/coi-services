01eee9245c982282e14ed6eb2593c7670f653f1b
Michael Meisinger <mmeisinger@ucsd.edu>
2012-04-13 14:09:42 -0700

    Datastore load now loads in bulk

--- ion/processes/bootstrap/datastore_loader.py
+++ ion/processes/bootstrap/datastore_loader.py
@@ -87,22 +87,31 @@ class DatastoreAdmin(ImmediateProcess):
         if not DatastoreManager.exists(ds_name):
             log.warn("Datastore does not exist: %s" % ds_name)
         ds = DatastoreManager.get_datastore_instance(ds_name)
+        objects = []
         for fn in os.listdir(path):
             fp = os.path.join(path, fn)
             try:
-                cls._read_and_create_obj(ds, fp)
+                with open(fp, 'r') as f:
+                    yaml_text = f.read()
+                obj = yaml.load(yaml_text)
+                if "_rev" in obj:
+                    del obj["_rev"]
+                objects.append(obj)
             except Exception as ex:
                 if ignore_errors:
                     log.warn("load error id=%s err=%s" % (fn, str(ex)))
                 else:
                     raise ex
 
-    @classmethod
-    def _read_and_create_obj(cls, ds, fp):
-        with open(fp, 'r') as f:
-            yaml_text = f.read()
-        obj = yaml.load(yaml_text)
-        ds._preload_create_doc(obj)
+        if objects:
+            try:
+                res = ds.create_doc_mult(objects, allow_ids=True)
+                log.info("DatastoreLoader: Loaded %s objects into %s" % (len(res), ds_name))
+            except Exception as ex:
+                if ignore_errors:
+                    log.warn("load error id=%s err=%s" % (fn, str(ex)))
+                else:
+                    raise ex
 
     @classmethod
     def dump_datastore(cls, path=None, ds_name=None, clear_dir=True):
