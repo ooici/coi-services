5834c2f63eee7addaec7987a0f05458aa2ddf92d
Edward Hunter <edhunter@ucsd.edu>
2012-04-20 10:23:41 -0700

    Tweaked bars to import. Need to align.

--- ion/services/mi/drivers/uw_bars/driver.py
+++ ion/services/mi/drivers/uw_bars/driver.py
@@ -15,7 +15,7 @@ from ion.services.mi.drivers.uw_bars.protocol import BarsInstrumentProtocol
 from ion.services.mi.instrument_driver import InstrumentDriver
 #from ion.services.mi.instrument_driver import DriverState
 
-from ion.services.mi.common import InstErrorCode
+#from ion.services.mi.common import InstErrorCode
 from ion.services.mi.drivers.uw_bars.common import BarsChannel
 from ion.services.mi.drivers.uw_bars.common import BarsParameter
 from ion.services.mi.drivers.uw_bars.protocol import BarsProtocolState
