b09a477e9baa9cf3359a9f4b57f8d0e73b34e310
Patrick Armstrong <patricka@uvic.ca>
2012-05-15 14:40:10 -0700

    Add basic eeagent support to coi-services

---
+++ ion/agents/cei/execution_engine_agent.py
@@ -0,0 +1,131 @@
+import datetime
+import logging
+
+from pyon.agent.agent import ResourceAgent
+from pyon.public import IonObject, log
+from pyon.util.containers import get_safe
+
+from interface.objects import AgentCommand
+from ion.agents.cei.util import looping_call
+
+
+from eeagent.core import EEAgentCore
+from eeagent.beatit import make_beat_msg
+from eeagent.execute import get_exe_factory
+
+
+"""
+@package ion.agents.cei.execution_engine_agent
+@file ion/agents/cei/execution_engine_agent.py
+@author Patrick Armstrong
+@brief Pyon port of EEAgent
+ """
+
+class ExecutionEngineAgent(ResourceAgent):
+    """Agent to manage processes on a worker
+
+    """
+
+    def __init__(self):
+        log.debug("ExecutionEngineAgent init")
+        ResourceAgent.__init__(self)
+
+
+    def on_init(self):
+        log.debug("ExecutionEngineAgent Pyon on_init")
+        launch_type_name = get_safe(self.CFG, "eeagent.launch_type.name")
+
+        if not launch_type_name:
+            # TODO: Fail fast here?
+            log.error("No launch_type.name specified")
+
+        self._factory = get_exe_factory(launch_type_name, self.CFG, log=log)
+
+        # TODO: Allow other core class?
+        self.core = EEAgentCore(self.CFG, self._factory, log)
+
+        self.heartbeater = HeartBeater(self.CFG, self._factory, log=log)
+
+        interval = 0.5 #TODO get from CFG
+
+        looping_call(interval, self.heartbeater.poll)
+
+
+    def acmd_launch_process(self, u_pid, round, run_type, parameters):
+        self.core.launch_process(u_pid, round, run_type, parameters)
+
+    def acmd_terminate_process(self, u_pid, round):
+        self.core.terminate_process(u_pid, round)
+
+    def acmd_restart_process(self, u_pid, round):
+        self.core.restart_process(u_pid, round)
+
+    def acmd_cleanup(self, u_pid, round):
+        self.core.cleanup(u_pid, round)
+
+    def acmd_dump_state(self):
+        return make_beat_msg(self.core._process_managers_map)
+
+class HeartBeater(object):
+    def __init__(self, CFG, factory, log=logging):
+
+        self._log = log
+        self._log.log(logging.DEBUG, "Starting the heartbeat thread")
+        self._CFG = CFG
+        self._res = None
+        self._interval = int(CFG.eeagent.heartbeat)
+        self._res = None
+        self._done = False
+        self._factory = factory
+        self._next_beat(datetime.datetime.now())
+
+        self._factory.set_state_change_callback(self._state_change_callback, None)
+
+    def _next_beat(self, now):
+        self._beat_time = now + datetime.timedelta(seconds=self._interval)
+
+    def _state_change_callback(self, user_arg):
+        # on state change set the beat time to now
+        self._beat_time = datetime.datetime.now()
+
+    def poll(self):
+
+        now = datetime.datetime.now()
+        if now > self._beat_time:
+            self._next_beat(now)
+            self.beat()
+
+    def beat(self):
+        try:
+            message =  make_beat_msg(self._factory)
+            self._log.debug("Send heartbeat: %s" % message)
+        except:
+            self._log.exception("beat failed")
+
+
+class ExecutionEngineAgentClient(object):
+
+    def __init__(self, agent_client):
+        self.client = agent_client
+
+    def launch_process(self, u_pid, round, run_type, parameters):
+
+        args = [u_pid, round, run_type, parameters]
+        cmd = AgentCommand(command='launch_process', args=args)
+        return self.client.execute_agent(cmd)
+
+    def terminate_process(self, u_pid, round):
+
+        args = [u_pid, round]
+        cmd = AgentCommand(command='terminate_process', args=args)
+        return self.client.execute_agent(cmd)
+
+    def restart_process(self, u_pid, round):
+
+        args = [u_pid, round]
+        cmd = AgentCommand(command='restart_process', args=args)
+        return self.client.execute_agent(cmd)
+
+    def dump_state(self):
+        cmd = AgentCommand(command='dump_state', args=[])
+        return self.client.execute_agent(cmd)
---
+++ ion/agents/cei/test/test_eeagent.py
@@ -0,0 +1,124 @@
+import gevent
+from mock import Mock
+from nose.plugins.attrib import attr
+from gevent import queue
+import os
+import shutil
+
+from pyon.net.endpoint import RPCClient
+from pyon.service.service import BaseService
+from pyon.util.containers import DotDict
+from pyon.util.unit_test import PyonTestCase
+from pyon.util.int_test import IonIntegrationTestCase
+from pyon.util.context import LocalContextMixin
+from pyon.core.exception import NotFound, BadRequest
+from pyon.public import log
+from pyon.event.event import EventSubscriber
+from pyon.agent.agent import ResourceAgentClient
+
+from interface.services.icontainer_agent import ContainerAgentClient
+from interface.objects import ProcessDefinition, ProcessSchedule, ProcessTarget, ProcessStateEnum
+
+from ion.agents.cei.execution_engine_agent import ExecutionEngineAgentClient
+class FakeProcess(LocalContextMixin):
+    """
+    A fake process used because the test case is not an ion process.
+    """
+    name = ''
+    id=''
+    process_type = ''
+
+@attr('INT', group='cei')
+class ExecutionEngineAgentIntTest(IonIntegrationTestCase):
+
+    def setUp(self):
+        self._start_container()
+        self.container.start_rel_from_url('res/deploy/r2cei.yml')
+
+        self.resource_id = "eeagent_1234"
+        self._eea_name = "eeagent"
+
+        self.supd_directory = "/tmp/pyon-eeagent-supd-test"
+        os.mkdir(self.supd_directory)
+
+        self.agent_config = {
+            'eeagent': {
+              'heartbeat': 2,
+              'slots': 100,
+              'name': 'pyon_eeagent',
+              'launch_type': {
+                'name': 'supd',
+                'pyon_directory': os.getcwd(),
+                'supd_directory': self.supd_directory,
+                'supdexe': 'bin/supervisord'
+              },
+            },
+            'agent': {'resource_id': self.resource_id},
+            'logging': {                                                           
+            'loggers': {                                                         
+              'eeagent': {                                                       
+                'level': 'DEBUG',                                                
+                'handlers': ['console']                                  
+              }                                                                  
+            },                                                                   
+            'root': {                                                            
+              'handlers': ['console']                                    
+            },                                                                   
+          }                         
+        }
+
+
+        # Start eeagent.
+        self._eea_pid = None
+        log.debug("TestInstrumentAgent.setup(): starting EDA.")
+        container_client = ContainerAgentClient(node=self.container.node,
+            name=self.container.name)
+        self._eea_pid = container_client.spawn_process(name=self._eea_name,
+            module="ion.agents.cei.execution_engine_agent",
+            cls="ExecutionEngineAgent", config=self.agent_config)
+        log.info('Agent pid=%s.', str(self._eea_pid))
+
+        # Start a resource agent client to talk with the instrument agent.
+        self._eea_pyon_client = ResourceAgentClient(self.resource_id, process=FakeProcess())
+        log.info('Got eea client %s.', str(self._eea_pyon_client))
+
+        self.eea_client = ExecutionEngineAgentClient(self._eea_pyon_client)
+
+    def tearDown(self):
+        shutil.rmtree(self.supd_directory)
+
+    def test_basics(self):
+        true_u_pid = "test0"
+        round = 0
+        run_type = "supd"
+        true_parameters = {'exec': 'true', 'argv': []}
+        self.eea_client.launch_process(true_u_pid, round, run_type, true_parameters)
+        state = self.eea_client.dump_state().result
+        proc = get_proc_for_upid(state, true_u_pid)
+
+        self.assertEqual(proc.get('state'), [800, 'EXITED'])
+
+        cat_u_pid = "test1"
+        round = 0
+        run_type = "supd"
+        cat_parameters = {'exec': 'cat', 'argv': []}
+        self.eea_client.launch_process(cat_u_pid, round, run_type, cat_parameters)
+        state = self.eea_client.dump_state().result
+        proc = get_proc_for_upid(state, cat_u_pid)
+        self.assertEqual(proc.get('state'), [500, 'RUNNING'])
+
+        
+        self.eea_client.terminate_process(cat_u_pid, round)
+        state = self.eea_client.dump_state().result
+        proc = get_proc_for_upid(state, cat_u_pid)
+        self.assertEqual(proc.get('state'), [700, 'TERMINATED'])
+
+
+
+def get_proc_for_upid(state, upid):
+
+    for proc_state in state.get('processes', []):
+        if proc_state.get('upid') == upid:
+            return proc_state
+    else:
+        return None
