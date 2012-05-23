fd3abd8de1a192e9a3059e6164560d0f1db583de
Christopher Mueller <cmueller@asascience.com>
2012-05-23 12:54:08 -0400

    Reordered classes

--- ion/agents/data/handlers/slocum_data_handler.py
+++ ion/agents/data/handlers/slocum_data_handler.py
@@ -22,8 +22,51 @@ PACKET_CONFIG = {
     'data_stream' : ('prototype.sci_data.stream_defs', 'ctd_stream_packet')
 }
 
-class SlocumParser(object):
+class SlocumDataHandler(BaseDataHandler):
+    @classmethod
+    def _init_acquisition_cycle(cls, config):
+        ext_dset_res = get_safe(config, 'external_dataset_res', None)
+        if ext_dset_res:
+            ds_url = ext_dset_res.dataset_description.parameters['dataset_path']
+            log.debug('Instantiate a SlocumParser for dataset: \'{0}\''.format(ds_url))
+            config['parser'] = SlocumParser(ds_url)
 
+
+    @classmethod
+    def _new_data_constraints(cls, config):
+        return {}
+
+    @classmethod
+    def _get_data(cls, config):
+        parser = get_safe(config, 'parser', None)
+        ext_dset_res = get_safe(config, 'external_dataset_res', None)
+        if ext_dset_res and parser:
+            #CBM: Not in use yet...
+#            t_vname = ext_dset_res.dataset_description.parameters['temporal_dimension']
+#            x_vname = ext_dset_res.dataset_description.parameters['zonal_dimension']
+#            y_vname = ext_dset_res.dataset_description.parameters['meridional_dimension']
+#            z_vname = ext_dset_res.dataset_description.parameters['vertical_dimension']
+#            var_lst = ext_dset_res.dataset_description.parameters['variables']
+
+            max_rec = get_safe(config, 'max_records', 1)
+            dprod_id = get_safe(config, 'data_producer_id', 'unknown data producer')
+            tx_yml = get_safe(config, 'taxonomy')
+            ttool = TaxyTool.load(tx_yml) #CBM: Assertion inside RDT.__setitem__ requires same instance of TaxyTool
+
+            cnt = cls._calc_iter_cnt(len(parser.sensor_map), max_rec)
+            for x in xrange(cnt):
+                rdt = RecordDictionaryTool(taxonomy=ttool)
+
+                for name in parser.sensor_map:
+                    d = parser.data_map[name][x*max_rec:(x+1)*max_rec]
+                    rdt[name]=d
+
+                g = build_granule(data_producer_id=dprod_id, taxonomy=ttool, record_dictionary=rdt)
+                yield g
+        else:
+            log.warn('No parser object found in config')
+
+class SlocumParser(object):
     # John K's documentation says there are 16 header lines, but I believe there are actually 17
     # The 17th indicating the 'dtype' of the data for that column
     header_size = 17
@@ -70,48 +113,3 @@ class SlocumParser(object):
             self.sensor_map[sensor_names[i]]=(units[i],dtypes[i])
             dat = np.genfromtxt(fname=StringIO(fstr),skip_header=self.header_size,usecols=i,dtype=dtypes[i],missing_values='NaN')#,usemask=True)
             self.data_map[sensor_names[i]]=dat
-
-
-class SlocumDataHandler(BaseDataHandler):
-    @classmethod
-    def _init_acquisition_cycle(cls, config):
-        ext_dset_res = get_safe(config, 'external_dataset_res', None)
-        if ext_dset_res:
-            ds_url = ext_dset_res.dataset_description.parameters['dataset_path']
-            log.debug('Instantiate a SlocumParser for dataset: \'{0}\''.format(ds_url))
-            config['parser'] = SlocumParser(ds_url)
-
-
-    @classmethod
-    def _new_data_constraints(cls, config):
-        return {}
-
-    @classmethod
-    def _get_data(cls, config):
-        parser = get_safe(config, 'parser', None)
-        ext_dset_res = get_safe(config, 'external_dataset_res', None)
-        if ext_dset_res and parser:
-            #CBM: Not in use yet...
-#            t_vname = ext_dset_res.dataset_description.parameters['temporal_dimension']
-#            x_vname = ext_dset_res.dataset_description.parameters['zonal_dimension']
-#            y_vname = ext_dset_res.dataset_description.parameters['meridional_dimension']
-#            z_vname = ext_dset_res.dataset_description.parameters['vertical_dimension']
-#            var_lst = ext_dset_res.dataset_description.parameters['variables']
-
-            max_rec = get_safe(config, 'max_records', 1)
-            dprod_id = get_safe(config, 'data_producer_id', 'unknown data producer')
-            tx_yml = get_safe(config, 'taxonomy')
-            ttool = TaxyTool.load(tx_yml) #CBM: Assertion inside RDT.__setitem__ requires same instance of TaxyTool
-
-            cnt = cls._calc_iter_cnt(len(parser.sensor_map), max_rec)
-            for x in xrange(cnt):
-                rdt = RecordDictionaryTool(taxonomy=ttool)
-
-                for name in parser.sensor_map:
-                    d = parser.data_map[name][x*max_rec:(x+1)*max_rec]
-                    rdt[name]=d
-
-                g = build_granule(data_producer_id=dprod_id, taxonomy=ttool, record_dictionary=rdt)
-                yield g
-        else:
-            log.warn('No parser object found in config')
