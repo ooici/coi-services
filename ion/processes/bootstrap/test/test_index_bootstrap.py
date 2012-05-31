11d8c8c3ece0d5762cd4268c6348944287f26100
Luke Campbell <luke.s.campbell@gmail.com>
2012-05-31 10:56:50 -0400

    Luke: Adds unit tests back in for coverage

--- ion/processes/bootstrap/test/test_index_bootstrap.py
+++ ion/processes/bootstrap/test/test_index_bootstrap.py
@@ -24,7 +24,80 @@ use_es = CFG.get_safe('system.elasticsearch',False)
 
 @attr('UNIT')
 class IndexBootStrapUnitTest(PyonTestCase):
-    pass
+
+    @patch('ion.processes.bootstrap.index_bootstrap.ep.ElasticSearch')
+    def test_clean_bootstrap(self, mock_es):
+        config = CFG
+        config.system.force_clean=False
+        config.system.elasticsearch=True
+        config.server.elasticsearch.host = ''
+        config.server.elasticsearch.port = ''
+        config.op = 'clean_bootstrap'
+        ibs = IndexBootStrap()
+        ibs.CFG = config
+        ibs.index_bootstrap = Mock()
+        ibs.on_start()
+
+
+        self.assertTrue(ibs.index_bootstrap.called)
+
+    @patch('ion.processes.bootstrap.index_bootstrap.ep.ElasticSearch')
+    def test_bad_op(self, mock_es):
+        config = CFG
+        config.system.force_clean=False
+        config.system.elasticsearch=True
+        config.server.elasticsearch.host = ''
+        config.server.elasticsearch.port = ''
+        config.op = 'not_real'
+        ibs = IndexBootStrap()
+        ibs.CFG = config
+        with self.assertRaises(BadRequest):
+            ibs.on_start()
+
+        
+    @patch('ion.processes.bootstrap.index_bootstrap.IndexManagementServiceClient')
+    @patch('ion.processes.bootstrap.index_bootstrap.ep.ElasticSearch')
+    def test_index_bootstrap(self, mock_es, ims_cli):
+        #---------------------------------------------
+        # Mocks
+        #---------------------------------------------
+        mock_es().index_create.return_value         = {'ok':True, 'status':200}
+        mock_es().raw.return_value                  = {'ok':True, 'status':200}
+        mock_es().river_couchdb_create.return_value = {'ok':True, 'status':200}
+
+        db = DotDict()
+        db.datastore_name = 'test'
+        db.server.test.create = Mock()
+        
+        container = DotDict()
+        container.datastore_manager.get_datastore = Mock()
+        container.datastore_manager.get_datastore.return_value = db
+        
+        config = CFG
+        config.system.force_clean=False
+        config.system.elasticsearch=True
+        config.server.elasticsearch.host = ''
+        config.server.elasticsearch.port = ''
+        config.op = 'index_bootstrap'
+        
+        #---------------------------------------------
+        # Execution
+        #---------------------------------------------
+        ibs = IndexBootStrap()
+        ibs.CFG = config
+        ibs.container = container
+        ibs.on_start()
+
+        index_count = len(STD_INDEXES) + 2 # Add 1 for the resources index, and 1 for river
+        self.assertTrue(mock_es().index_create.call_count == index_count, 'Improper number of indices created')
+
+        river_count = len(STD_INDEXES) + 1 # Add 1 for the resources index
+        self.assertTrue(mock_es().river_couchdb_create.call_count == river_count, 'Improper number of rivers created')
+
+        total_count = len(STD_INDEXES) + len(COUCHDB_INDEXES) + 1
+        self.assertTrue(ims_cli().create_index.call_count == total_count, 'Improper number of index resources created')
+
+
 @attr('INT',group='dm')
 class IndexBootStrapIntTest(IonIntegrationTestCase):
     def setUp(self):
