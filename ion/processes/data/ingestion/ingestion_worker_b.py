dfd107864b196d6ca4dce11c7184fba86fb1cfb7
Luke Campbell <luke.s.campbell@gmail.com>
2012-06-22 10:41:28 -0700

    Luke; work on ingestion

---
+++ ion/processes/data/ingestion/ingestion_worker_b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+@author Luke Campbell <LCampbell@ASAScience.com>
+@file ingestion_worker_b
+@date 06/22/12 11:48
+@description DESCRIPTION
+'''
+
+class IngestionWorkerB(TransformDataProcess):
+    def on_start(self):
+        pass
+    def persist(self):
+        pass
