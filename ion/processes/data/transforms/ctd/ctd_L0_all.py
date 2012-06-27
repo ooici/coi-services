2f3896f5a6837d41b5f8f5fec30ccf8654209eea
Maurice Manning <mmmanning@ucsd.edu>
2012-06-27 13:21:59 -0700

    update ctd transforms to use the new stream and granule

--- ion/processes/data/transforms/ctd/ctd_L0_all.py
+++ ion/processes/data/transforms/ctd/ctd_L0_all.py
@@ -8,12 +8,25 @@ from pyon.ion.transform import TransformFunction, TransformDataProcess
 from pyon.service.service import BaseService
 from pyon.core.exception import BadRequest
 from pyon.public import IonObject, RT, log
+from pyon.util.containers import get_safe
 
 from prototype.sci_data.stream_defs import SBE37_CDM_stream_definition, L0_pressure_stream_definition, L0_temperature_stream_definition, L0_conductivity_stream_definition
 
 from prototype.sci_data.stream_parser import PointSupplementStreamParser
-from prototype.sci_data.constructor_apis import PointSupplementConstructor
-from prototype.sci_data.stream_defs import ctd_stream_definition
+#from prototype.sci_data.constructor_apis import PointSupplementConstructor
+#from prototype.sci_data.stream_defs import ctd_stream_definition
+
+### For new granule and stream interface
+from pyon.ion.granule.record_dictionary import RecordDictionaryTool
+from pyon.ion.granule.taxonomy import TaxyTool
+from pyon.ion.granule.granule import build_granule
+
+#from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
+#pmsc = PubsubManagementServiceClient(node=cc.node)
+#c_stream_id = pmsc.create_stream(name='conductivity')
+#t_stream_id = pmsc.create_stream(name='temperature')
+#p_stream_id = pmsc.create_stream(name='pressure')
+#cc.spawn_process('l0_transform', 'ion.processes.data.transforms.ctd_L0_all','ctd_L0_all', config={'processes':{'publish_streams':{'conductivity':c_stream_id, 'temperature':t_stream_id, 'pressure': p_stream_id } } })
 
 
 class ctd_L0_all(TransformDataProcess):
@@ -24,22 +37,40 @@ class ctd_L0_all(TransformDataProcess):
     # Make the stream definitions of the transform class attributes
     incoming_stream_def = SBE37_CDM_stream_definition()
 
-    outgoing_stream_pressure = L0_pressure_stream_definition()
-    outgoing_stream_temperature = L0_temperature_stream_definition()
-    outgoing_stream_conductivity = L0_conductivity_stream_definition()
+    #outgoing_stream_pressure = L0_pressure_stream_definition()
+    #outgoing_stream_temperature = L0_temperature_stream_definition()
+    #outgoing_stream_conductivity = L0_conductivity_stream_definition()
 
+    ### Taxonomies are defined before hand out of band... somehow.
+    pres_tx = TaxyTool()
+    pres_tx.add_taxonomy_set('pres','long name for pres')
+    pres_tx.add_taxonomy_set('lat','long name for latitude')
+    pres_tx.add_taxonomy_set('lon','long name for longitude')
+    pres_tx.add_taxonomy_set('height','long name for height')
+    pres_tx.add_taxonomy_set('time','long name for time')
+    # This is an example of using groups it is not a normative statement about how to use groups
+    pres_tx.add_taxonomy_set('coordinates','This group contains coordinates...')
+    pres_tx.add_taxonomy_set('data','This group contains data...')
 
+    temp_tx = TaxyTool()
+    temp_tx.add_taxonomy_set('temp','long name for temp')
+    temp_tx.add_taxonomy_set('lat','long name for latitude')
+    temp_tx.add_taxonomy_set('lon','long name for longitude')
+    temp_tx.add_taxonomy_set('height','long name for height')
+    temp_tx.add_taxonomy_set('time','long name for time')
+    # This is an example of using groups it is not a normative statement about how to use groups
+    temp_tx.add_taxonomy_set('coordinates','This group contains coordinates...')
+    temp_tx.add_taxonomy_set('data','This group contains data...')
 
-    # Retrieve the id of the OUTPUT stream from the out Data Product for each of the three output streams
-#    stream_ids, _ = self.clients.resource_registry.find_objects(out_data_product_id, PRED.hasStream, None, True)
-#
-#    log.debug("DataProcessManagementService:create_data_process retrieve out data prod streams: %s", str(stream_ids))
-#    if not stream_ids:
-#        raise NotFound("No Stream created for output Data Product " + str(out_data_product_id))
-#    if len(stream_ids) != 1:
-#        raise BadRequest("Data Product should only have ONE stream at this time" + str(out_data_product_id))
-#    self.output_stream_dict[name] = stream_ids[0]
-
+    cond_tx = TaxyTool()
+    cond_tx.add_taxonomy_set('cond','long name for cond')
+    cond_tx.add_taxonomy_set('lat','long name for latitude')
+    cond_tx.add_taxonomy_set('lon','long name for longitude')
+    cond_tx.add_taxonomy_set('height','long name for height')
+    cond_tx.add_taxonomy_set('time','long name for time')
+    # This is an example of using groups it is not a normative statement about how to use groups
+    cond_tx.add_taxonomy_set('coordinates','This group contains coordinates...')
+    cond_tx.add_taxonomy_set('data','This group contains data...')
 
     def process(self, packet):
 
@@ -47,54 +78,60 @@ class ctd_L0_all(TransformDataProcess):
         """
 
         # Use the PointSupplementStreamParser to pull data from a granule
-        psd = PointSupplementStreamParser(stream_definition=self.incoming_stream_def, stream_granule=packet)
+        #psd = PointSupplementStreamParser(stream_definition=self.incoming_stream_def, stream_granule=packet)
+        rdt = RecordDictionaryTool.load_from_granule(packet)
+        rdt0 = rdt['coordinates']
+        rdt1 = rdt['data']
 
+        conductivity = get_safe(rdt1, 'cond') #psd.get_values('conductivity')
+        pressure = get_safe(rdt1, 'pres') #psd.get_values('pressure')
+        temperature = get_safe(rdt1, 'temp') #psd.get_values('temperature')
 
-        conductivity = psd.get_values('conductivity')
-        pressure = psd.get_values('pressure')
-        temperature = psd.get_values('temperature')
-
-        longitude = psd.get_values('longitude')
-        latitude = psd.get_values('latitude')
-        height = psd.get_values('height')
-        time = psd.get_values('time')
+        longitude = get_safe(rdt0, 'lon') # psd.get_values('longitude')
+        latitude = get_safe(rdt0, 'lat')  #psd.get_values('latitude')
+        time = get_safe(rdt0, 'time') # psd.get_values('time')
+        height = get_safe(rdt0, 'height') # psd.get_values('time')
 
         log.warn('Got conductivity: %s' % str(conductivity))
         log.warn('Got pressure: %s' % str(pressure))
         log.warn('Got temperature: %s' % str(temperature))
 
-    
+        g = self._build_granule_settings(self.cond_tx, 'cond', conductivity, time, latitude, longitude, height)
+        
+        self.conductivity.publish(g)
 
-        # do L0 scaling here.....
+        g = self._build_granule_settings(self.temp_tx, 'temp', temperature, time, latitude, longitude, height)
 
+        self.temperature.publish(g)
 
-        # Use the constructor to put data into a granule
+        g = self._build_granule_settings(self.pres_tx, 'pres', pressure, time, latitude, longitude, height)
 
-        psc_conductivity = PointSupplementConstructor(point_definition=self.outgoing_stream_conductivity, stream_id=self.streams['conductivity'])
-
-        psc_pressure = PointSupplementConstructor(point_definition=self.outgoing_stream_pressure, stream_id=self.streams['pressure'])
-
-        psc_temperature = PointSupplementConstructor(point_definition=self.outgoing_stream_temperature, stream_id=self.streams['temperature'])
-
-        ### The stream id is part of the metadata which much go in each stream granule - this is awkward to do at the
-        ### application level like this!
-
-        for i in xrange(len(conductivity)):
-            point_id = psc_conductivity.add_point(time=time[i],location=(longitude[i],latitude[i],height[i]))
-            psc_conductivity.add_scalar_point_coverage(point_id=point_id, coverage_id='conductivity', value=conductivity[i])
-        self.conductivity.publish(psc_conductivity.close_stream_granule())
-
-        for i in xrange(len(pressure)):
-            point_id = psc_pressure.add_point(time=time[i],location=(longitude[i],latitude[i],height[i]))
-            psc_pressure.add_scalar_point_coverage(point_id=point_id, coverage_id='pressure', value=pressure[i])
-        self.pressure.publish(psc_pressure.close_stream_granule())
-
-        for i in xrange(len(temperature)):
-            point_id = psc_temperature.add_point(time=time[i],location=(longitude[i],latitude[i],height[i]))
-            psc_temperature.add_scalar_point_coverage(point_id=point_id, coverage_id='temperature', value=temperature[i])
-        self.temperature.publish(psc_temperature.close_stream_granule())
+        self.pressure.publish(g)
 
         return
+
+    def _build_granule_settings(self, taxonomy=None, field_name='', value=None, time=None, latitude=None, longitude=None, height=None):
+
+        root_rdt = RecordDictionaryTool(taxonomy=taxonomy)
+
+        data_rdt = RecordDictionaryTool(taxonomy=taxonomy)
+
+        data_rdt[field_name] = value
+
+        coor_rdt = RecordDictionaryTool(taxonomy=taxonomy)
+
+        coor_rdt['time'] = time
+        coor_rdt['lat'] = latitude
+        coor_rdt['lon'] = longitude
+        coor_rdt['height'] = height
+
+        root_rdt['coordinates'] = coor_rdt
+        root_rdt['data'] = data_rdt
+
+        log.debug("ctd_L0_all:_build_granule_settings: logging published Record Dictionary:\n %s", str(root_rdt.pretty_print()))
+
+        return build_granule(data_producer_id='ctd_L0', taxonomy=taxonomy, record_dictionary=root_rdt)
+
 
 
 
