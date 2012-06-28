e5825d12a87c8b4dc8e27a5c09f6282bc331652b
Maurice Manning <mmmanning@ucsd.edu>
2012-06-28 09:26:08 -0700

    add new granule to publisher, use new granule in unit tests

--- ion/processes/data/ctd_stream_publisher.py
+++ ion/processes/data/ctd_stream_publisher.py
@@ -29,12 +29,29 @@ from pyon.public import log
 import time
 from uuid import uuid4
 import random
+import numpy
 
 from prototype.sci_data.stream_defs import ctd_stream_packet, SBE37_CDM_stream_definition, ctd_stream_definition
 from prototype.sci_data.constructor_apis import PointSupplementConstructor
 
 from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
+### For new granule and stream interface
+from pyon.ion.granule.record_dictionary import RecordDictionaryTool
+from pyon.ion.granule.taxonomy import TaxyTool
+from pyon.ion.granule.granule import build_granule
 
+
+### Taxonomies are defined before hand out of band... somehow.
+tx = TaxyTool()
+tx.add_taxonomy_set('temp','long name for temp')
+tx.add_taxonomy_set('cond','long name for cond')
+tx.add_taxonomy_set('lat','long name for latitude')
+tx.add_taxonomy_set('lon','long name for longitude')
+tx.add_taxonomy_set('pres','long name for pres')
+tx.add_taxonomy_set('time','long name for time')
+tx.add_taxonomy_set('height','long name for height')
+tx.add_taxonomy_set('coordinates','This group contains coordinates...')
+tx.add_taxonomy_set('data','This group contains data...')
 
 
 class SimpleCtdPublisher(StandaloneProcess):
@@ -125,6 +142,51 @@ class SimpleCtdPublisher(StandaloneProcess):
             c=c, t=t, p=p, lat=lat, lon=lon, time=tvar)
 
         return ctd_packet
+
+
+    def _get_new_ctd_packet(self, stream_id, length):
+
+        rdt = RecordDictionaryTool(taxonomy=tx)
+        rdt0 = RecordDictionaryTool(taxonomy=tx)
+        rdt1 = RecordDictionaryTool(taxonomy=tx)
+
+
+
+        #Explicitly make these numpy arrays...
+        c = numpy.array([random.uniform(0.0,75.0)  for i in xrange(length)])
+
+        t = numpy.array([random.uniform(-1.7, 21.0) for i in xrange(length)])
+
+        p = numpy.array([random.lognormvariate(1,2) for i in xrange(length)])
+
+        lat = numpy.array([random.uniform(-90.0, 90.0) for i in xrange(length)])
+
+        lon = numpy.array([random.uniform(0.0, 360.0) for i in xrange(length)])
+
+        h = numpy.array([random.uniform(0.0, 360.0) for i in xrange(length)])
+
+        tvar = numpy.array([self.last_time + i for i in xrange(1,length+1)])
+
+        self.last_time = max(tvar)
+
+        log.warn('Got time: %s' % str(tvar))
+        log.warn('Got t: %s' % str(t))
+
+        rdt0['time'] = tvar
+        rdt0['lat'] = lat
+        rdt0['lon'] = lon
+        rdt0['height'] = h
+        rdt1['temp'] = t
+        rdt1['cond'] = c
+        rdt1['pres'] = p
+
+        rdt['coordinates'] = rdt0
+        rdt['data'] = rdt1
+
+        g = build_granule(data_producer_id=stream_id, taxonomy=tx, record_dictionary=rdt)
+
+        return g
+
 
 class PointCtdPublisher(StandaloneProcess):
 
