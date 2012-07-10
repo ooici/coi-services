5ee758a715faf07f9dd4658bd06ff135b1d989bd
Ian Katz <ifreecarve@gmail.com>
2012-07-10 12:33:46 -0400

    retire instead of delete resources

--- ion/services/sa/observatory/observatory_impl.py
+++ ion/services/sa/observatory/observatory_impl.py
@@ -7,6 +7,8 @@
 
 
 #from pyon.core.exception import BadRequest, NotFound
+from ion.services.sa.instrument.resource_lcs_policy import SitePolicy
+from pyon.ion.resource import LCE
 from pyon.public import PRED, RT
 
 from ion.services.sa.resource_impl.resource_simple_impl import ResourceSimpleImpl
@@ -22,6 +24,15 @@ class ObservatoryImpl(ResourceSimpleImpl):
 
     def _primary_object_label(self):
         return "observatory"
+
+    def on_simpl_init(self):
+        self.policy = SitePolicy(self.clients)
+
+        self.add_lce_precondition(LCE.PLAN, self.use_policy(self.policy.lce_precondition_plan))
+        self.add_lce_precondition(LCE.DEVELOP, self.use_policy(self.policy.lce_precondition_develop))
+        self.add_lce_precondition(LCE.INTEGRATE, self.use_policy(self.policy.lce_precondition_integrate))
+        self.add_lce_precondition(LCE.DEPLOY, self.use_policy(self.policy.lce_precondition_deploy))
+        self.add_lce_precondition(LCE.RETIRE, self.use_policy(self.policy.lce_precondition_retire))
 
     def link_site(self, observatory_id='', site_id=''):
         return self._link_resources(observatory_id, PRED.hasSite, site_id)
