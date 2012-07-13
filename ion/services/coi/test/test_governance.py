71302e360dbf121af20256cebbb255a3357bf0bc
Dave Foster <dfoster@asascience.com>
2012-07-13 10:08:26 -0400

    Disable policies installed during governance tests in tearDown

--- ion/services/coi/test/test_governance.py
+++ ion/services/coi/test/test_governance.py
@@ -133,6 +133,14 @@ class TestGovernanceInt(IonIntegrationTestCase):
         sa_header_roles = get_role_message_headers(self.org_client.find_all_roles_by_user(self.system_actor._id))
         self.sa_user_header = {'ion-actor-id': self.system_actor._id, 'ion-actor-roles': sa_header_roles }
 
+    def tearDown(self):
+        policy_list_ids, _ = self.rr_client.find_resources(restype=RT.Policy, id_only=True)
+
+        for polid in policy_list_ids:
+            self.pol_client.disable_policy(polid, headers=self.sa_user_header)
+
+        gevent.sleep(2)  # Wait for events to be fired and policy updated
+
     @attr('LOCOINT')
     @unittest.skipIf(os.getenv('CEI_LAUNCH_TEST', False),'Not integrated for CEI')
     def test_basic_policy(self):
