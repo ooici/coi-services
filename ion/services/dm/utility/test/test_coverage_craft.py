051422346bc0d55bb7f2261b9bd23dce5afc48ca
Luke Campbell <luke.s.campbell@gmail.com>
2012-07-20 14:31:22 -0400

    Luke: test for dm

--- ion/services/dm/utility/test/test_coverage_craft.py
+++ ion/services/dm/utility/test/test_coverage_craft.py
@@ -7,6 +7,33 @@
 '''
 
 from pyon.util.unit_test import PyonTestCase
+from ion.services.dm.utility.granule_utils import CoverageCraft, RecordDictionaryTool, build_granule, TaxyTool 
+from nose.plugins.attrib import attr
 
+import numpy as np
+
+@attr('UNIT')
 class CoverageCraftUnitTest(PyonTestCase):
-    pass
+
+    def sample_granule(self):
+        rdt = RecordDictionaryTool(CoverageCraft.tx)
+        rdt['time'] = np.arange(20)
+        rdt['temp'] = np.array([5] * 20)
+        rdt['cond'] = np.array([10] * 20)
+        rdt['lat'] = np.array([0] * 20)
+        rdt['lon'] = np.array([0] * 20)
+        rdt['depth'] = np.array([0] * 20)
+        rdt['data'] = np.array([0x01] * 20)
+
+        return build_granule('sample', CoverageCraft.tx, rdt)
+
+    def test_to_coverage(self):
+        granule = self.sample_granule()
+        crafter = CoverageCraft(granule=granule)
+        crafter.add_granule()
+
+        coverage = crafter.coverage
+        time_vals = coverage.get_time_values()
+        comp = time_vals == np.arange(20)
+        self.assertTrue(comp.all())
+
