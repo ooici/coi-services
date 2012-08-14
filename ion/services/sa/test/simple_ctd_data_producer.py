01d635e2423eee0ced6ae9adf2a6e22b683ef9f7
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-08-14 13:55:49 -0400

    fixed some stuff in ctd data producer

--- ion/services/sa/test/simple_ctd_data_producer.py
+++ ion/services/sa/test/simple_ctd_data_producer.py
@@ -102,29 +102,34 @@ class SimpleCtdDataProducer(SimpleCtdPublisher):
         cond_ctxt.fill_value = 0e0
         pdict.add_context(cond_ctxt)
 
+        temp_ctxt = ParameterContext('temp', param_type=QuantityType(value_encoding=np.float32))
+        temp_ctxt.uom = 'degree_Celsius'
+        temp_ctxt.fill_value = 0e0
+        pdict.add_context(temp_ctxt)
+
         return pdict
 
     def _add_location_time_ctxt(self, pdict):
 
-        t_ctxt = ParameterContext('time', param_type=QuantityType(value_encoding=np.int64))
+        t_ctxt = ParameterContext('time', param_type=QuantityType(value_encoding=numpy.int64))
         t_ctxt.reference_frame = AxisTypeEnum.TIME
         t_ctxt.uom = 'seconds since 1970-01-01'
         t_ctxt.fill_value = 0x0
         pdict.add_context(t_ctxt)
 
-        lat_ctxt = ParameterContext('lat', param_type=QuantityType(value_encoding=np.float32))
+        lat_ctxt = ParameterContext('lat', param_type=QuantityType(value_encoding=numpy.float32))
         lat_ctxt.reference_frame = AxisTypeEnum.LAT
         lat_ctxt.uom = 'degree_north'
         lat_ctxt.fill_value = 0e0
         pdict.add_context(lat_ctxt)
 
-        lon_ctxt = ParameterContext('lon', param_type=QuantityType(value_encoding=np.float32))
+        lon_ctxt = ParameterContext('lon', param_type=QuantityType(value_encoding=numpy.float32))
         lon_ctxt.reference_frame = AxisTypeEnum.LON
         lon_ctxt.uom = 'degree_east'
         lon_ctxt.fill_value = 0e0
         pdict.add_context(lon_ctxt)
 
-        depth_ctxt = ParameterContext('depth', param_type=QuantityType(value_encoding=np.float32))
+        depth_ctxt = ParameterContext('depth', param_type=QuantityType(value_encoding=numpy.float32))
         depth_ctxt.reference_frame = AxisTypeEnum.HEIGHT
         depth_ctxt.uom = 'meters'
         depth_ctxt.fill_value = 0e0
