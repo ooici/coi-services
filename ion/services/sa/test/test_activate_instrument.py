2e67459f451fe702ddc8bda3e871b8d3ced98754
Ian Katz <ifreecarve@gmail.com>
2012-09-14 18:43:11 -0400

    class to support properly waiting for a process to start

--- ion/services/sa/test/test_activate_instrument.py
+++ ion/services/sa/test/test_activate_instrument.py
@@ -26,8 +26,10 @@ from prototype.sci_data.stream_defs import \
 
 from interface.objects import AgentCommand
 from interface.objects import ProcessDefinition
+from interface.objects import ProcessStateEnum
 
 from gevent.event import AsyncResult
+
 
 from pyon.public import CFG
 from pyon.public import RT, LCS, PRED
@@ -51,6 +53,44 @@ from nose.plugins.attrib import attr
 
 import unittest
 import time
+
+
+from pyon.event.event import EventSubscriber
+from gevent import event as gevent_event
+from pyon.event.event import EventSubscriber
+class ProcessStateGate(EventSubscriber):
+    """
+    Ensure that we get a particular state, now or in the future.
+    """
+    def __init__(self, process_dispatcher_svc_client=None, process_id='', desired_state=None, *args, **kwargs):
+        EventSubscriber.__init__(self, *args, callback=self.trigger_cb, **kwargs)
+
+        self.pd_client = process_dispatcher_svc_client
+        self.desired_state = desired_state
+        self.process_id = process_id
+
+        #sanity check
+        self.pd_client.read_process(self.process_id)
+
+    def trigger_cb(self, event, x):
+        if event == self.desired_state:
+            self.stop()
+            self.gate.set()
+
+    def await(self, timeout=None):
+        self.gate = gevent_event.Event()
+        self.start()
+
+        #check on the process as it exists right now
+        process_obj = self.pd_client.read_process(self.process_id)
+        if self.desired_state == process_obj.process_state:
+            self.stop()
+            return True
+
+        #if it's not where we want it, wait.
+        return self.gate.wait(timeout)
+
+
 
 
 class FakeProcess(LocalContextMixin):
@@ -82,7 +122,6 @@ class TestActivateInstrumentIntegration(IonIntegrationTestCase):
         self.processdispatchclient = ProcessDispatcherServiceClient(node=self.container.node)
         self.dataprocessclient = DataProcessManagementServiceClient(node=self.container.node)
         self.dataproductclient = DataProductManagementServiceClient(node=self.container.node)
-
         
         #setup listerner vars
         self._data_greenlets = []
@@ -355,15 +394,19 @@ class TestActivateInstrumentIntegration(IonIntegrationTestCase):
 
         log.debug("test_createTransformsThenActivateInstrument: create L0 all data_process return")
 
-
         self.imsclient.start_instrument_agent_instance(instrument_agent_instance_id=instAgentInstance_id)
 
-        inst_agent_instance_obj= self.imsclient.read_instrument_agent_instance(instAgentInstance_id)
+        #wait for start
+        instance_obj = self.imsclient.read_instrument_agent_instance(instAgentInstance_id)
+        gate = ProcessStateGate(self.processdispatchclient, instance_obj.agent_process_id, ProcessStateEnum.SPAWN)
+        self.assertTrue(gate.await(30), "The instrument agent instance did not spawn in 30 seconds")
+
+        inst_agent_instance_obj = self.imsclient.read_instrument_agent_instance(instAgentInstance_id)
         log.debug( 'Instrument agent instance obj: = %s', str(inst_agent_instance_obj))
 
         # Start a resource agent client to talk with the instrument agent.
-        #self._ia_client = ResourceAgentClient('123xyz', name=inst_agent_instance_obj.agent_process_id,  process=FakeProcess())
-        self._ia_client = ResourceAgentClient(instDevice_id,  process=FakeProcess())
+        self._ia_client = ResourceAgentClient('iaclient', name=inst_agent_instance_obj.agent_process_id,  process=FakeProcess())
+        #self._ia_client = ResourceAgentClient('iaclient', name=instDevice_id,  process=FakeProcess())
         log.debug("test_activateInstrumentSample: got ia client %s", str(self._ia_client))
 
         cmd = AgentCommand(command=ResourceAgentEvent.INITIALIZE)
