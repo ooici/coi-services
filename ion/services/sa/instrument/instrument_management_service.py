9703601aa9cb056d0bc9e38ffe82fd2db3f5b387
Ian Katz <ifreecarve@gmail.com>
2012-09-14 13:39:38 -0400

    allow uploading of driver to have a different remote user than the current user, supplied in config

--- ion/services/sa/instrument/instrument_management_service.py
+++ ion/services/sa/instrument/instrument_management_service.py
@@ -725,6 +725,10 @@ class InstrumentManagementService(BaseInstrumentManagementService):
         cfg_remotepath  = CFG.service.instrument_management.driver_release_directory #'/var/www/release'
         cfg_user        = pwd.getpwuid(os.getuid())[0]
 
+        #allow overriding of user with config variable
+        if CFG.service.instrument_management.driver_release_user:
+            cfg_user = CFG.service.instrument_management.driver_release_user
+
         log.debug("creating tempfile for egg output")
         f_handle, tempfilename = tempfile.mkstemp()
         log.debug("writing egg data to disk at '%s'" % tempfilename)
