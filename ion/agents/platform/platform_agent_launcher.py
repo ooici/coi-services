bea1fbb76985f1e05f9ea2b4c238ffe8384a1be6
Carlos Rueda <carueda@mbari.org>
2012-09-18 16:37:55 -0700

    use StateGate pattern by default; local tests continue to run and hopefully it will help with the issues in buildbot

--- ion/agents/platform/platform_agent_launcher.py
+++ ion/agents/platform/platform_agent_launcher.py
@@ -35,10 +35,14 @@ class Launcher(object):
     Helper for launching platform agent processes.
     """
 
-    def __init__(self):
+    def __init__(self, use_gate=True):
+        """
+        @param use_gate     True to use StateGate pattern.
+        """
         self._pd_client = ProcessDispatcherServiceClient()
         self._event_queue = None
         self._event_sub = None
+        self._use_gate = use_gate
 
     def launch(self, platform_id, agent_config, timeout_spawn=30):
         """
@@ -55,10 +59,45 @@ class Launcher(object):
                   platform_id, str(timeout_spawn))
 
         try:
-            return self._do_launch(platform_id, agent_config, timeout_spawn)
+            if self._use_gate:
+                return self._do_launch_gate(platform_id, agent_config, timeout_spawn)
+            else:
+                return self._do_launch(platform_id, agent_config, timeout_spawn)
         finally:
             self._event_queue = None
             self._event_sub = None
+
+    def _do_launch_gate(self, platform_id, agent_config, timeout_spawn):
+
+        log.debug("_do_launch_gate: platform_id=%s", platform_id)
+
+        pa_name = 'PlatformAgent_%s' % platform_id
+
+        pdef = ProcessDefinition(name=pa_name)
+        pdef.executable = {
+            'module': PA_MOD,
+            'class': PA_CLS
+        }
+        pdef_id = self._pd_client.create_process_definition(process_definition=pdef)
+
+        pid = self._pd_client.create_process(process_definition_id=pdef_id)
+
+        log.debug("calling schedule_process: pid=%s", str(pid))
+        self._pd_client.schedule_process(process_definition_id=pdef_id,
+                                         process_id=pid,
+                                         configuration=agent_config)
+
+        if timeout_spawn:
+            gate = ProcessStateGate(self._pd_client, pid, ProcessStateEnum.SPAWN)
+            try:
+                gate.await(timeout_spawn)
+            except:
+                msg = "The platform agent instance did not spawn in %s seconds" %\
+                      timeout_spawn
+                log.error(msg, exc_Info=True)
+                raise PlatformException(msg)
+
+        return pid
 
     def _do_launch(self, platform_id, agent_config, timeout_spawn):
 
@@ -150,3 +189,38 @@ class Launcher(object):
         Helper to terminate a process
         """
         self._pd_client.cancel_process(pid)
+
+
+# copied from test_activate_instrument.py
+from gevent import event as gevent_event
+class ProcessStateGate(EventSubscriber):
+    """
+    Ensure that we get a particular state, now or in the future.
+    """
+    def __init__(self, process_dispatcher_svc_client=None, process_id='', desired_state=None, *args, **kwargs):
+        EventSubscriber.__init__(self, *args, callback=self.trigger_cb, **kwargs)
+
+        self.pd_client = process_dispatcher_svc_client
+        self.desired_state = desired_state
+        self.process_id = process_id
+
+        #sanity check
+        self.pd_client.read_process(self.process_id)
+
+    def trigger_cb(self, event, x):
+        if event == self.desired_state:
+            self.stop()
+            self.gate.set()
+
+    def await(self, timeout=None):
+        self.gate = gevent_event.Event()
+        self.start()
+
+        #check on the process as it exists right now
+        process_obj = self.pd_client.read_process(self.process_id)
+        if self.desired_state == process_obj.process_state:
+            self.stop()
+            return True
+
+        #if it's not where we want it, wait.
+        return self.gate.wait(timeout)
