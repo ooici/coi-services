9ae61df1daef348f0526d3b07c1e1aacb1ea3922
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2012-10-02 16:03:41 -0700

    restore DataProductLink

--- ion/processes/bootstrap/ion_loader.py
+++ ion/processes/bootstrap/ion_loader.py
@@ -130,7 +130,7 @@ class IONLoader(ImmediateProcess):
                       #'IngestionConfiguration',
                       'DataProduct',
                       'DataProcess',
-                      #'DataProductLink',
+                      'DataProductLink',
                       'Attachment',
                       'WorkflowDefinition',
                       'Workflow',
