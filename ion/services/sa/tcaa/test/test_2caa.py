ddbd08dbe9ba3ee913ca72c3906f216df252fae5
Edward Hunter <edwardhunter@ucsd.edu>
2012-10-11 10:41:18 -0700

    2caa test setup.

--- ion/services/sa/tcaa/test/test_2caa.py
+++ ion/services/sa/tcaa/test/test_2caa.py
@@ -96,9 +96,111 @@ class Test2CAA(IonIntegrationTestCase):
     def setUp(self):
         """
         """
-        pass
+        
+        ###################################################################
+        # Internal parameters and container.
+        ###################################################################
+        
+        # Internal parameters.        
+        self._platform_resource_id = 'abc123'
+        self._resource_id = 'fake_id'
+        self._remote_port = 0
+        self._terrestrial_port = 0
+        self._te_client = None
+        self._re_client = None
+        self._remote_pid = None
+        self._terrestrial_pid = None
+
+        # Start container.
+        log.debug('Staring capability container.')
+        self._start_container()
+        
+        # Bring up services in a deploy file (no need to message).
+        log.info('Staring deploy services.')
+        self.container.start_rel_from_url('res/deploy/r2deploy.yml')
+
+        # Create a container client.
+        log.debug('Creating container client.')
+        container_client = ContainerAgentClient(node=self.container.node,
+            name=self.container.name)
+        
+        ###################################################################
+        # Terrestrial endpoint.
+        ###################################################################
+
+        # Create the remote name.
+        xs_name = 'remote1'
+        terrestrial_svc_name = 'terrestrial_endpoint'
+        terrestrial_listen_name = terrestrial_svc_name + xs_name
+
+        # Create terrestrial config.
+        terrestrial_endpoint_config = {
+            'other_host' : 'localhost',
+            'other_port' : self._remote_port,
+            'this_port' : 0,
+            'platform_resource_id' : self._platform_resource_id,
+            'process' : {
+                'listen_name' : terrestrial_listen_name
+            }
+        }
+        
+        # Spawn the terrestrial enpoint process.
+        log.debug('Spawning terrestrial endpoint process.')
+        self._terrestrial_pid = container_client.spawn_process(
+            name=terrestrial_listen_name,
+            module='ion.services.sa.tcaa.terrestrial_endpoint',
+            cls='TerrestrialEndpoint',
+            config=terrestrial_endpoint_config)
+        log.debug('Terrestrial endpoint pid=%s.', str(self._terrestrial_pid))
+
+        # Create a terrestrial client.
+        self.te_client = TerrestrialEndpointClient(
+            process=FakeProcess(),
+            to_name=terrestrial_listen_name)
+        log.debug('Got te client %s.', str(self.te_client))
+        self._terrestrial_port = self.te_client.get_port()
+        log.debug('Terrestrial port is: %i', self._terrestrial_port)
+    
+        ###################################################################
+        # Remote endpoint.
+        ###################################################################
+    
+        remote_svc_name = 'remote_endpoint'
+        remote_listen_name = remote_svc_name + xs_name
+        
+        # Create agent config.
+        remote_endpoint_config = {
+            'other_host' : 'localhost',
+            'other_port' : self._remote_port,
+            'this_port' : 0,
+            'platform_resource_id' : self._platform_resource_id,
+            'process' : {
+                'listen_name' : remote_listen_name
+            }
+        }
+        
+        # Spawn the remote enpoint process.
+        log.debug('Spawning remote endpoint process.')
+        self._remote_pid = container_client.spawn_process(
+            name=remote_listen_name,
+            module='ion.services.sa.tcaa.remote_endpoint',
+            cls='RemoteEndpoint',
+            config=remote_endpoint_config)
+        log.debug('Remote endpoint pid=%s.', str(self._remote_pid))
+
+        # Create an endpoint client.
+        self.re_client = RemoteEndpointClient(
+            process=FakeProcess(),
+            to_name=remote_listen_name)
+        log.debug('Got re client %s.', str(self.re_client))
+        
+        # Remember the remote port.
+        self._remote_port = self.re_client.get_port()
+        log.debug('The remote port is: %i.', self._remote_port)
+        
     
     def test_xxx(sefl):
         """
         """
-        pass
+        gevent.sleep(2)
+        
