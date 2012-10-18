916039fba3b1cb7059121882b3fc6f15d90c27b7
Ian Katz <ifreecarve@gmail.com>
2012-10-18 14:38:12 -0400

    more refactor of module uploader to allow different flavors of uploads

--- ion/util/module_uploader.py
+++ ion/util/module_uploader.py
@@ -36,16 +36,97 @@ class RegisterModulePreparerBase(object):
         self.modules["tempfile"]   = tempfile
         self.modules["os"]         = os
 
+        self.subprocess = subprocess
+        self.tempfile   = tempfile
+        self.os         = os
+
+
+    def get_uploader_class(self):
+        """
+        this will drive the factory method
+        """
+        return RegisterModuleUploader
+
+
+    def get_dest_url(self, dest_filename):
+        """
+        calculate the destination URL from the filename
+        """
+        www_rel = path_subtract(self.dest_path, self.dest_wwwroot)
+        return "http://%s%s/%s" % (self.dest_host, www_rel, dest_filename)
+
+
+    def prepare(self, dest_contents_b64, dest_filename=None):
+        """
+        validate a file and process it to prepare for uploading
+
+        return RegisterModuleUploader object (or None), message
+        """
+        return self.uploader_object_factory(dest_contents_b64, dest_filename), ""
+
+
+    def uploader_object_factory(self, dest_contents_b64, dest_filename):
+
+        cls = self.get_uploader_class()
+        ret = cls(dest_user     = self.dest_user,
+                  dest_host     = self.dest_host,
+                  dest_path     = self.dest_path,
+                  dest_file     = dest_filename,
+                  dest_contents = dest_contents_b64,
+                  dest_url      = self.get_dest_url(dest_filename),
+                  modules       = self.modules)
+        return ret
+
+
+
+class RegisterModulePreparerPy(RegisterModulePreparerBase):
+    """
+    class to register a python file by putting it in a web-accessible location
+    """
+
+    def prepare(self, py_b64, dest_filename=None):
+        """
+        perform syntax check and return uploader object
+        """
+        log.debug("creating tempfile with contents")
+        f_handle, tempfilename = self.tempfile.mkstemp()
+        log.debug("writing contents to disk at '%s'", tempfilename)
+        self.os.write(f_handle, base64.decodestring(py_b64))
+
+        log.info("syntax checking file")
+        py_proc = self.subprocess.Popen(["python", "-m", "py_compile", tempfilename],
+                                        stdout=self.subprocess.PIPE,
+                                        stderr=self.subprocess.PIPE)
+
+        py_out, py_err = py_proc.communicate()
+
+        # clean up
+        log.debug("removing tempfile at '%s'", tempfilename)
+
+        if 0 != py_proc.returncode:
+            return False, ("Syntax check failed.  (STDOUT: %s) (STDERR: %s)"
+                           % (py_out, py_err))
+
+        ret = self.uploader_object_factory(py_b64, dest_filename or tempfilename)
+        return ret, ""
+
+
 
 
 
 class RegisterModulePreparerEgg(RegisterModulePreparerBase):
     """
     class to register an egg file by putting it in a web-accessible location
-
     """
 
-    def prepare(self, egg_b64):
+    def get_uploader_class(self):
+        """
+        this will drive the factory method
+        """
+        return RegisterModuleUploaderEgg
+
+
+    def prepare(self, egg_b64, dest_filename=None):
         """
         validate the egg and process it to prepare for uploading
 
@@ -75,24 +156,17 @@ class RegisterModulePreparerEgg(RegisterModulePreparerBase):
                 return None, "Egg's PKG-INFO did not include a field called '%s'" % f
 
         #determine egg name
-        egg_filename = "%s-%s-py2.7.egg" % (pkg_info_data["Name"].replace("-", "_"), pkg_info_data["Version"])
-        log.info("Egg filename is '%s'", egg_filename)
+        dest_filename = "%s-%s-py2.7.egg" % (pkg_info_data["Name"].replace("-", "_"), pkg_info_data["Version"])
+        log.info("Egg filename is '%s'", dest_filename)
 
-        www_rel = path_subtract(self.dest_path, self.dest_wwwroot)
 
-        egg_url = "http://%s%s/%s" % (self.dest_host, www_rel, egg_filename)
+        egg_url = self.get_dest_url(dest_filename)
         log.info("Egg url will be '%s'", egg_url)
 
         egg_url_filename = "%s v%s.url" % (pkg_info_data["Name"].replace("-", "_"), pkg_info_data["Version"])
 
 
-        ret = RegisterModuleUploaderEgg(dest_user     = self.dest_user,
-                                        dest_host     = self.dest_host,
-                                        dest_path     = self.dest_path,
-                                        dest_file     = egg_filename,
-                                        dest_contents = egg_b64,
-                                        dest_url      = egg_url,
-                                        modules       = self.modules)
+        ret = self.uploader_object_factory(egg_b64, dest_filename)
 
         ret.set_egg_urlfile_name(egg_url_filename)
 
