7eb938b0025219916d989f21774ec63e1a817cb7
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-11-05 14:44:42 -0500

    bumped ion defs and some work

--- ion/processes/data/transforms/event_alert_transform.py
+++ ion/processes/data/transforms/event_alert_transform.py
@@ -6,8 +6,10 @@
 @author Swarbhanu Chatterjee
 '''
 from pyon.util.log import log
+from pyon.util.arg_check import validate_is_instance, validate_true
 from pyon.event.event import EventPublisher, EventSubscriber
 from ion.core.process.transform import TransformEventListener, TransformStreamListener, TransformEventPublisher
+from interface.objects import DeviceStatusType
 import gevent
 from gevent import queue
 
@@ -123,22 +125,33 @@ class DemoStreamAlertTransform(TransformStreamListener, TransformEventListener, 
     def on_start(self):
         super(DemoStreamAlertTransform,self).on_start()
         self.value = self.CFG.get_safe('process.value', 0)
+        self.instrument_variable = self.CFG.get_safe('process.variable', 0)
+        self.valid_values = self.CFG.get_safe('process.valid_values', [-200,200])
+        validate_is_instance(self.valid_values, list)
 
         self.granules = []
 
+    def _stringify_list(self, my_list = None):
+        validate_true(len(my_list) == 2, "List has more than just the 2 variables to specify lower and upper limits")
+        return "%s %s" % (my_list[0], my_list[1])
+
     def recv_packet(self, msg, stream_route, stream_id):
         '''
-        The callback method.
-        If the events satisfy the criteria, publish an alert event.
+        The callback method. For situations like bad or no data, publish an alert event.
+
+        @param msg granule
+        @param stream_route StreamRoute object
+        @param stream_id str
         '''
         log.debug('StreamAlertTransform got an incoming packet!')
+        good_values = self._check_values_in_granule(granule=msg, field=self.instrument_variable)
 
-        value = self._extract_parameters_from_stream(msg, "VALUE")
-
-        if msg.find("PUBLISH") > -1 and (value < self.value):
-            self.publish(subtype='OUT_OF_RANGE')
+        if not good_values:
+            # Data is out-of-range
+            self.publish(subtype=self.instrument_variable, state= DeviceStatusType.OUT_OF_RANGE)
         else:
-            self.publish(subtype='IN_RANGE')
+            # Data is in-range
+            self.publish(subtype=self.instrument_variable, state=DeviceStatusType.OK)
 
 
     def process_event(self, msg, headers):
@@ -148,35 +161,35 @@ class DemoStreamAlertTransform(TransformStreamListener, TransformEventListener, 
 
         if self.granules.empty():
             log.debug("No granule arrived since the last timer event. Publishing an alarm!!!")
-            self.publish(subtype= 'NO_DATA')
+            self.publish(subtype=self.instrument_variable, state=DeviceStatusType.NO_DATA)
         else:
             log.debug("Granules have arrived since the last timer event.")
             self.granules.clear()
 
 
-    def publish(self, subtype = None):
+    def publish(self, subtype = None, state = None, value = None):
         '''
         Publish an alert event
         '''
         self.publisher.publish_event(origin="DemoStreamAlertTransform",
             event_type = 'DeviceStatusEvent',
             sub_type = subtype,
+            state = state,
+            value = value,
+            valid_values = self.valid_values,
             description= "Event to deliver the status of instrument.")
 
 
-    def _extract_parameters_from_stream(self, msg, field ):
+    def _check_values_in_granule(self, granule = None, field = None):
 
-        tokens = msg.split(" ")
+        rdt = RecordDictionaryTool.load_from_granule(granule)
 
-        try:
-            for token in tokens:
-                token = token.strip()
-                if token == '=':
-                    i = tokens.index(token)
-                    if tokens[i-1] == field:
-                        return int(tokens[i+1].strip())
-        except IndexError:
-            log.warning("Could not extract value from the message. Please check its format.")
+        value_array[:] = rdt[field]
+        
+        if value_array.min() < self.valid_values[0] or value_array.max() > self.valid_values[1]:
+            return False
+        else:
+            return True
 
-        return self.value
+
 
