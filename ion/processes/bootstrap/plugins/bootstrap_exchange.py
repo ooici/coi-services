fa4205f551f91da3bb293f26fc675c5f4322607b
Dave Foster <dfoster@asascience.com>
2012-11-06 11:18:56 -0500

    Exchange bootstrap associates brokers with exchange spaces

--- ion/processes/bootstrap/plugins/bootstrap_exchange.py
+++ ion/processes/bootstrap/plugins/bootstrap_exchange.py
@@ -11,7 +11,7 @@ from pyon.public import get_sys_name, RT, PRED
 from pyon.ion.exchange import ExchangeSpace, ExchangePoint, ExchangeName
 from interface.services.coi.iexchange_management_service import ExchangeManagementServiceProcessClient
 import pprint
-from interface.objects import ExchangeName as ResExchangeName
+from interface.objects import ExchangeBroker as ResExchangeBroker
 from interface.objects import ExchangeSpace as ResExchangeSpace
 from interface.objects import ExchangePoint as ResExchangePoint
 
@@ -25,6 +25,9 @@ class BootstrapExchange(BootstrapPlugin):
         Bootstraps initial objects in the system from configuration (pyon.yml) via
         EMS calls.
         """
+
+        # get default org_id
+        # @TODO: single org assumed for now
         org_ids = process.container.resource_registry.find_resources(RT.Org, id_only=True)
         if not (len(org_ids) and len(org_ids[0]) == 1):
             raise StandardError("Could not determine org_id")
@@ -33,13 +36,16 @@ class BootstrapExchange(BootstrapPlugin):
 
         ems_client = ExchangeManagementServiceProcessClient(process=process)
 
-        for xsname, xpdict in config.get_safe('exchange_spaces', {}).iteritems():
+        #
+        # Create XSs and XPs
+        #
+        for xsname, xsdict in config.get_safe('exchange_spaces', {}).iteritems():
             xso = ResExchangeSpace(name=xsname)
             xso_id = ems_client.create_exchange_space(xso, org_id)
 
             log.info("ExchangeSpace %s, id %s", xsname, xso_id)
 
-            for xpname, xpopts in xpdict.get('exchange_points', {}).iteritems():
+            for xpname, xpopts in xsdict.get('exchange_points', {}).iteritems():
 
                 # @TODO: some translation for types CFG currentl has it as "topic_tree" and we've been using "ttree"
                 ttype = xpopts.get('type', 'topic_tree')
@@ -50,6 +56,23 @@ class BootstrapExchange(BootstrapPlugin):
                 xpo_id = ems_client.create_exchange_point(xpo, xso_id)
 
                 log.info("\tExchangePoint %s, id %s", xpname, xpo_id)
+
+            #
+            # Create and associate brokers with XSs
+            #
+            for brokername in xsdict.get('brokers', []):
+                xbo = ResExchangeBroker(name=brokername)
+                xbo_id = ems_client.create_exchange_broker(xbo)
+
+                log.info("\tExchangeBroker %s, id %s", brokername, xbo_id)
+
+                # directly associate broker with XS
+                # @TODO: should EMS provide this?
+                # first find out if the assoc exists already
+                assocs = process.container.resource_registry.find_associations(xso_id, PRED.hasExchangeBroker, id_only=True)
+                if len(assocs) > 0:
+                    continue
+                process.container.resource_registry.create_association(xso_id, PRED.hasExchangeBroker, xbo_id)
 
     def on_restart(self, process, config, **kwargs):
         """
