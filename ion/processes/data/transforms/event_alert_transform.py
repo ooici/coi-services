e0514aed25ab60e7ad267dc7b865039f0749d031
Maurice Manning <mmmanning@ucsd.edu>
2012-11-13 22:56:41 -0800

    set time_stamp for CommsEvent

--- ion/processes/data/transforms/event_alert_transform.py
+++ ion/processes/data/transforms/event_alert_transform.py
@@ -271,7 +271,7 @@ class DemoStreamAlertTransform(TransformStreamListener, TransformEventListener, 
                     origin_type='PlatformDevice',
                     sub_type = self.instrument_variable_name,
                     ts_created=get_ion_ts(),
-                    time_stamp =int(time.time()),
+                    time_stamp =int(time.time() + 2208988800),  # granules use NTP not unix
                     state=DeviceCommsType.DATA_DELIVERY_INTERRUPTION,
                     lapse_interval_seconds=self.timer_interval,
                     description = "Event to deliver the communications status of the instrument.")
