b0644e734fbaf75e8947632c0c74c90552b7f286
Stephen Henrie <shenrie@ucsd.edu>
2012-11-27 10:57:55 -0800

    Fix example stream publisher

--- examples/stream/publish.py
+++ examples/stream/publish.py
@@ -5,9 +5,13 @@ def start_ctd_publisher(container):
 
 
     pubsubclient = PubsubManagementServiceClient(node=container.node)
-    stream_id, route = pubsubclient.create_stream('ctd_publisher', exchange_point='science_data')
 
-    pid = container.spawn_process('ctdpublisher', 'ion.processes.data.sinusoidal_stream_publisher','SinusoidalCtdPublisher',{'process':{'stream_id':stream_id}})
+    ctd_stream_def_id = pubsubclient.create_stream_definition(name='SBE37_CDM')
+
+    stream_id, route = pubsubclient.create_stream('ctd_publisher', exchange_point='science_data', stream_definition_id=ctd_stream_def_id)
+
+    pid = container.spawn_process('ctdpublisher', 'ion.processes.data.sinusoidal_stream_publisher','SinusoidalCtdPublisher',
+        {'process':{'stream_id':stream_id}})
 
     print 'stream_id=' + stream_id
     print 'pid=' + pid
