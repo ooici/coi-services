4f02bdb261017256d7dd7e63c930342632092e9b
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2013-02-06 10:49:18 -0500

    Added and wrote splitterand L1 pressure transforms

---
+++ ion/processes/data/transforms/ctd/presf_L0_splitter.py
@@ -0,0 +1,76 @@
+
+'''
+@author Swarbhanu Chatterjee
+@file ion/processes/data/transforms/ctd/presf_L0_splitter.py
+@description The transform takes the presf_tide_measurement stream as input and outputs the DPD7_out_L0_pressure stream
+(will create this streamdef shortly). It simple maps the input pressure param to an pressure output param
+(as well as supporting params: time, port_timestamp, driver_timestamp, internal_timestamp, preferred_timestamp,)
+other data parameters are dropped
+'''
+
+from ion.core.process.transform import TransformDataProcess
+from pyon.core.exception import BadRequest
+### For new granule and stream interface
+from ion.services.dm.utility.granule.record_dictionary import RecordDictionaryTool
+from ion.core.function.transform_function import SimpleGranuleTransformFunction
+from interface.services.dm.ipubsub_management_service import PubsubManagementServiceProcessClient
+
+# For usage: please refer to the integration tests in
+# ion/processes/data/transforms/ctd/test/test_ctd_transforms.py
+
+class PresfL0Splitter(TransformDataProcess):
+    ''' A basic transform that receives input through a subscription,
+    parses the input from a CTD, extracts the pressure value and scales it according to
+    the defined algorithm. If the transform
+    has an output_stream it will publish the output on the output stream.
+
+    '''
+    output_bindings = ['absolute_pressure']
+
+    def on_start(self):
+        super(PresfL0Splitter, self).on_start()
+
+        if not self.CFG.process.publish_streams.has_key('absolute_pressure'):
+            raise BadRequest("For the PresfL0Splitter, please send the stream_id using "
+                             "a special keyword (ex: absolute_pressure)")
+
+        self.pres_stream = self.CFG.process.publish_streams.absolute_pressure
+
+        # Read the parameter dict from the stream def of the stream
+        pubsub = PubsubManagementServiceProcessClient(process=self)
+        self.stream_definition = pubsub.read_stream_definition(stream_id=self.pres_stream)
+
+    def recv_packet(self, packet, stream_route, stream_id):
+        """Processes incoming data!!!!
+        """
+
+        if packet == {}:
+            return
+        granule = PresfL0SplitterAlgorithm.execute(packet, params=self.stream_definition._id)
+        self.absolute_pressure.publish(msg=granule)
+
+
+class PresfL0SplitterAlgorithm(SimpleGranuleTransformFunction):
+
+    @staticmethod
+    @SimpleGranuleTransformFunction.validate_inputs
+    def execute(input=None, context=None, config=None, params=None, state=None):
+
+        rdt = RecordDictionaryTool.load_from_granule(input)
+        out_rdt = RecordDictionaryTool(stream_definition_id=params)
+
+        absolute_pressure = rdt['absolute_pressure']
+        pres_value = absolute_pressure
+
+        for key, value in rdt.iteritems():
+
+            cond = key=='time' or key=='port_timestamp' or key=='driver_timestamp'\
+                   or  key=='internal_timestamp' or key=='preferred_timestamp' or key=='timestamp'\
+                   or key=='lat' or key=='lon' or key=='absolute_pressure'
+
+            if cond and key in out_rdt:
+                out_rdt[key] = value[:]
+
+        out_rdt['absolute_pressure'] = pres_value
+
+        return out_rdt.to_granule()
