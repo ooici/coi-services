c971380a4bb275dfd267749f0d80839dbf150c3c
Michael Barry <mbarry@asascience.com>
2013-02-06 10:24:08 -0500

    modified coverage handler to use different types

--- ion/util/pydap/handlers/coverage/coverage_handler.py
+++ ion/util/pydap/handlers/coverage/coverage_handler.py
@@ -7,11 +7,11 @@ from email.utils import formatdate
 from stat import ST_MTIME
 
 from coverage_model.coverage import SimplexCoverage
-from coverage_model import QuantityType
-from pydap.model import DatasetType,BaseType, GridType, StructureType
+from coverage_model.parameter_types import QuantityType,ConstantRangeType,ArrayType, ConstantType, RecordType, CategoryType, BooleanType
+from pydap.model import DatasetType,BaseType, SequenceType
 from pydap.handlers.lib import BaseHandler
 import time
-import numpy as np
+import sys
 
 class Handler(BaseHandler):
 
@@ -20,127 +20,178 @@ class Handler(BaseHandler):
 
     def __init__(self, filepath):
         self.filepath = filepath
+    
+    def is_str(self, data):
+        for d in data:
+            if not isinstance(d, str):
+                return False
+        return True
+    def is_list(self, data):
+        for d in data:
+            if not isinstance(d, (list,tuple)):
+                return False
+        return True
 
     def parse_constraints(self, environ):
         base = os.path.split(self.filepath)
-        coverage = SimplexCoverage.load(base[0], base[1],mode='r')
-        #coverage = SimplexCoverage.pickle_load(self.filepath)
+        self.cov = SimplexCoverage.load(base[0], base[1],mode='r')
 
         last_modified = formatdate(time.mktime(time.localtime(os.stat(self.filepath)[ST_MTIME])))
         environ['pydap.headers'].append(('Last-modified', last_modified))
 
-        atts = {}
-        atts['title'] = coverage.name
-        dataset = DatasetType(coverage.name) #, attributes=atts)
-        fields, queries = environ['pydap.ce']
-        queries = filter(bool, queries)  # fix for older version of pydap
-
-        all_vars = coverage.list_parameters()
-        t = []
-
-        for param in all_vars:
-            if not isinstance(coverage.get_parameter_context(param).param_type, QuantityType):
-                t.append(param)
-        [all_vars.remove(i) for i in t]
-
-        time_context = coverage.get_parameter_context(coverage.temporal_parameter_name)
+        dataset = DatasetType(self.cov.name)
+        
+        #find time fill index
+        time_context = self.cov.get_parameter_context(self.cov.temporal_parameter_name)
         time_fill_value = time_context.fill_value
-        time_data = coverage.get_parameter_values(coverage.temporal_parameter_name)
+        time_data = self.cov.get_parameter_values(self.cov.temporal_parameter_name)
         fill_index = -1
         try:
             fill_index = numpy.where(time_data == time_fill_value)[0][0]
         except IndexError:
             pass
-
-        # If no fields have been explicitly requested, of if the sequence
-        # has been requested directly, return all variables.
-
-        if not fields:
-            fields = [[(name, ())] for name in all_vars]
-
+        fields, queries = environ['pydap.ce']
+        queries = filter(bool, queries)  # fix for older version of pydap
+        #got through data parameters
+        all_vars = self.cov.list_parameters()
+        fields = [[(name, ())] for name in all_vars]
+        seq = SequenceType("data")
         for var in fields:
-            target = dataset
             while var:
                 name, slice_ = var.pop(0)
-                covname = urllib.unquote(name)
-                param = coverage.get_parameter(covname)
+                name = urllib.unquote(name)
 
-                #print 'Incoming tuple ', slice_
+                slice_ = self.update_slice_object(slice_, fill_index)
+                if slice_ is None:
+                    continue
 
-                slice_ = slice_[0] if slice_ else slice(None)
-                
-                #print 'formed slice ', slice_
-
-
-                #need to truncate slice here in case time has fill values
-                if slice_.start is None and slice_.stop is None and fill_index >=0:
-                    slice_ = slice(0, fill_index, 1)
-
-                if slice_.start and slice_.stop is None:
-
-                    if fill_index > slice_.start:
-                        continue
-
-                    if fill_index > slice_.stop:
-                        slice_.stop = fill_index
-
-                if slice_.start is not None and slice_.start == slice_.stop:
-                    slice_ = slice(slice_.start, slice_.stop+1, slice_.step)
-                    #print 'crap'
-
-                #print slice_
-
-
-                if param.is_coordinate or target is not dataset:
+                print "name",name
+                #print "is_coord",param.is_coordinate
+                pc = self.cov.get_parameter_context(name)
+                #just dealing with time series data for now make a sequence
+                try:
+                    attrs = {}
                     try:
-                        target[name] = get_var(coverage,name,slice_)
+                        attrs['units'] = pc.uom
                     except:
-                        log.exception('Problem reading coverage %s', coverage.name)
-                        continue
+                        pass
+                    attrs['long_name'] = pc.long_name
+                    data = self.cov.get_parameter_values(name, tdoa=slice_)
+                    if isinstance(pc.param_type, QuantityType):
+                        ndata = numpy.array(data) 
+                        seq[name] = BaseType(name=name, data=ndata, type=ndata.dtype.char, attributes=attrs)
+                    if isinstance(pc.param_type, ConstantType):
+                        ndata = numpy.array(data)
+                        seq[name] = BaseType(name=name, data=ndata, type=ndata.dtype.char, attributes=attrs)
+                    if isinstance(pc.param_type, ConstantRangeType):
+                        try:
+                            col1,col2 = zip(*data)
+                        except:
+                            col1 = []
+                            col2 = []
+                        name_col1 = "_".join([name,"low"])
+                        name_col2 = "_".join([name,"high"])
+                        col1 = numpy.array(col1)
+                        seq[name_col1] = BaseType(name=name_col1, data=col1, type=col1.dtype.char, attributes=attrs)
+                        col2 = numpy.array(col2)
+                        seq[name_col2] = BaseType(name=name_col2, data=col2, type=col2.dtype.char, attributes=attrs)
+                    if isinstance(pc.param_type,BooleanType):
+                        boolean_values = self.transform_boolean(data)
+                        boolean_values = numpy.array(boolean_values)
+                        seq[name] = BaseType(name=name, data=boolean_values, type=boolean_values.dtype.char, attributes=attrs)
+                    if isinstance(pc.param_type,CategoryType):
+                        cat_data = self.transform_categories(pc.param_type.categories, data)
+                        cat_data = numpy.array(cat_data)
+                        seq[name] = BaseType(name=name, data=cat_data, type=cat_data.dtype.char, attributes=attrs)
+                    if isinstance(pc.param_type,ArrayType):
+                        data = numpy.array(data)
+                        ttype = numpy.dtype.char
+                        #numpy defaults string array to type 'O' which is object which [ydap does nto support
+                        #hack to fix it
+                        #TODO: handle recursively
+                        if self.is_str(data) == True:
+                            ttype = 's'
+                            seq[name] = BaseType(name=name, data=data, type=ttype, attributes=attrs)
+                        elif self.is_list(data):
+                            for x,lst in enumerate(data):
+                                nname = "_".join([name, str(x)])
+                                idata = numpy.array(lst)
+                                ttype = numpy.dtype.char
+                                if self.is_str(idata) == True:
+                                    ttype = 's'
+                                seq[nname] = BaseType(name=nname, data=idata, type=ttype, attributes=attrs)
+                        else:
+                            log.exception("only handle list and string objects")
+                    if isinstance(pc.param_type,RecordType):
+                        print "rc data", data
+                        keys = []
+                        values = []
+                        for ddict in data:
+                            for k,v in ddict.iteritems():
+                                keys.append(k)
+                                values.append(v)
+                        keys = numpy.array(keys)
+                        ttype = numpy.dtype.char
+                        if self.is_str(keys) == True:
+                            ttype = 's'
+                        seq[name+"_keys"] = BaseType(name=name+"_keys", data=keys, type=ttype, attributes=attrs)
+                        values = numpy.array(values)
+                        if self.is_str(values) == True:
+                            ttype = 's'
+                        seq[name+"_values"] = BaseType(name=name+"_values", data=values, type=ttype, attributes=attrs)
+                        
 
-                elif var:
-                    target.setdefault(name, StructureType(name=name, attributes={'units':coverage.get_parameter_context(name).uom}))
-                    target = target[name]
-                else:  # return grid
-                    grid = target[name] = GridType(name=name)
-                    try:
-                        grid[name] = get_var(coverage,name, slice_)
-                    except:
-                        log.exception('Problem reading coverage %s', coverage.name)
-                        continue
-
-                    dim = coverage.temporal_parameter_name 
-                    try:
-                        grid[dim] = get_var(coverage,dim,slice_)
-                    except:
-                        log.exception('Problem reading coverage %s', coverage.name)
-                        continue
+                except Exception, e:
+                    print "exception", e
+                    import traceback
+                    print traceback.format_exc()
+                    log.exception('Problem reading cov %s %s', self.cov.name, e)
+                    continue
+        
+        dataset['data'] = seq 
         dataset._set_id()
-        dataset.close = coverage.close
-        #print dataset.__dict__
+        dataset.close = self.cov.close
+        #print "dataset",dataset
         return dataset
 
-def get_var(coverage,name,slice_):
-    data = coverage.get_parameter_values(name,tdoa=slice_)
-    if not isinstance(data, np.ndarray):
-        data = np.array([data])
-    attrs = {'units': coverage.get_parameter_context(name).uom}
-    dims = (coverage.temporal_parameter_name,)
+    def update_slice_object(self, slice_, fill_index):
+
+        slice_ = slice_[0] if slice_ else slice(None)
+        #print 'formed slice ', slice_
+
+        #need to truncate slice here in case time has fill values
+        if slice_.start is None and slice_.stop is None and fill_index >=0:
+            slice_ = slice(0, fill_index, 1)
+
+        if slice_.start and slice_.stop is None:
+
+            if fill_index > slice_.start:
+                return None
+
+            if fill_index > slice_.stop:
+                slice_.stop = fill_index
+
+        if slice_.start is not None and slice_.start == slice_.stop:
+            slice_ = slice(slice_.start, slice_.stop+1, slice_.step)
+        
+        return slice_
     
+    def transform_categories(self, categories, cat_data):
+        result = []
+        for d in cat_data:
+            for k,v in categories.iteritems():
+                if d == v:
+                    result.append(k)
+        return result
+
+        
+    def transform_boolean(self, boolean_values):
+        result = []
+        for b in boolean_values:
+            if b == True:
+                result.append(1)
+            else:
+                result.append(0)
+        return result
     
-    log.debug( 'name=%s', name)
-    log.debug( 'data=%s', data)
-    log.debug( 'shape=%s' , data.shape)
-    log.debug( 'type=%s', data.dtype.char)
-    log.debug( 'dims=%s', dims)
-    log.debug( 'attrs=%s', attrs)
-    return BaseType(name=name, data=data, shape=data.shape, type=data.dtype.char, dimensions=dims, attributes=attrs)
-
-
-
-if __name__ == '__main__':
-    import sys
-    from paste.httpserver import serve
-
-    application = Handler(sys.argv[1])
-    serve(application, port=8001)
+        
