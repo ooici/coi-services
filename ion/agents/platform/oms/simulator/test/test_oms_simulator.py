1e90db83d69b7c2e519f0cf899225871cfda94fc
Carlos Rueda <carueda@mbari.org>
2013-02-15 16:00:54 -0800

    allow to also test against our simulator running externally

--- ion/agents/platform/oms/simulator/test/test_oms_simulator.py
+++ ion/agents/platform/oms/simulator/test/test_oms_simulator.py
@@ -4,7 +4,8 @@
 @package ion.agents.platform.oms.simulator.test.test_oms_simulator
 @file    ion/agents/platform/oms/simulator/test/test_oms_simulator.py
 @author  Carlos Rueda
-@brief   Test cases for the simulator
+@brief   Test cases for the simulator, by default in embedded form,
+         but the OMS environment variable can be used to indicate other.
 """
 
 __author__ = 'Carlos Rueda'
@@ -17,6 +18,7 @@ Logger.set_logger(log)
 
 from pyon.util.unit_test import IonUnitTestCase
 
+from ion.agents.platform.oms.oms_client_factory import OmsClientFactory
 from ion.agents.platform.oms.simulator.oms_simulator import OmsSimulator
 from ion.agents.platform.oms.test.oms_test_mixin import OmsTestMixin
 
@@ -34,10 +36,11 @@ class Test(IonUnitTestCase, OmsTestMixin):
     def setUpClass(cls):
         OmsTestMixin.setUpClass()
         OmsTestMixin.start_http_server()
-        cls.oms = OmsSimulator()
+        cls.oms = OmsClientFactory.create_instance()
 
     @classmethod
     def tearDownClass(cls):
-        cls.oms._deactivate_simulator()
+        if isinstance(cls.oms, OmsSimulator):
+            cls.oms._deactivate_simulator()
         event_notifications = OmsTestMixin.stop_http_server()
         log.info("event_notifications = %s" % str(event_notifications))
