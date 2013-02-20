dc4b697dccf8556aa7bc9e347540ffde4edd9776
Michael Barry <mbarry@asascience.com>
2013-02-20 13:44:53 -0500

    first pass generic transform working

--- ion/processes/data/transforms/transform_prime.py
+++ ion/processes/data/transforms/transform_prime.py
@@ -30,7 +30,6 @@ class TransformPrime(TransformDataProcess):
         self.pubsub_management = PubsubManagementServiceProcessClient(process=self)
     
     def recv_packet(self, msg, stream_route, stream_id):
-        
         #rdt = RecordDictionaryTool(stream_definition_id=stream_def_out.id)
         
         #publisher = getattr(self, self.CFG.process.stream_id)
@@ -39,8 +38,6 @@ class TransformPrime(TransformDataProcess):
         pass
 
     def execute_transform(self, msg, stream_id):
-        import sys
-        
         stream_def_in = self.pubsub_management.read_stream_definition(stream_id=stream_id)
         incoming_pdict_dump = stream_def_in.parameter_dictionary
         
@@ -50,19 +47,30 @@ class TransformPrime(TransformDataProcess):
         incoming_pdict = ParameterDictionary.load(incoming_pdict_dump)
         outgoing_pdict = ParameterDictionary.load(outgoing_pdict_dump)
         
-
         merged_pdict = dict([(k,v) for k,v in incoming_pdict.iteritems()] + [(k,v) for k,v in outgoing_pdict.iteritems()])
         rdt_in = RecordDictionaryTool.load_from_granule(msg)
         rdt_out = RecordDictionaryTool(stream_definition_id=stream_def_out._id)
-        print >> sys.stderr, "available_fields", rdt_in._available_fields
-        print >> sys.stderr, "available_fields", rdt_out._available_fields
         for key,pctup in merged_pdict.iteritems():
             n,pc = pctup
-            print >> sys.stderr, key,"param_type", pc.param_type
             #if function then a transform is applied to calculate values
             if isinstance(pc.param_type, ParameterFunctionType):
                 #apply transform
                 pv = get_value_class(pc.param_type, rdt_in.domain)
+                #recursive function applies values
+                def pval_callback(name, slice_):
+                    result = None
+                    #search for input data...first level input
+                    if name in rdt_in._available_fields:
+                        result = rdt_in[name]
+                    else:
+                        #not first level data so continue to evaluate
+                        n,pc = merged_pdict[name]
+                        pv = get_value_class(pc.param_type, rdt_in.domain)
+                        pv._pval_callback = pval_callback
+                        result = pv[:]
+                    return result
+                #set the evaluation callback so it can find values in the input stream
+                pv._pval_callback = pval_callback
                 #transform should be on the outgoing stream
                 if key in rdt_out._available_fields:
                     rdt_out._rd[key] = pv[:]
@@ -70,7 +78,7 @@ class TransformPrime(TransformDataProcess):
                 #field exists in both the in and the out stream so pass it along to the output stream
                 if key in rdt_in._available_fields and key in rdt_out._available_fields:
                     #pass through
-                    rdt_out._rd[key] = rdt_in._rd[key]
+                    rdt_out._rd[key] = rdt_in._rd[key][:]
         return rdt_out 
 
 
