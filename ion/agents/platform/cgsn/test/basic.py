20df12dd7faa8c84596ed1fc78b59da1e90f1f9f
Carlos Rueda <carueda@mbari.org>
2013-02-27 16:52:25 -0800

    use appropriate IP for my laptop 134.89.13.60  (not simply localhost), and also use a socket for sending and a (bound) socket for receiving the response.  Tested OK with the CG services endpoint at WHOI.

--- ion/agents/platform/cgsn/test/basic.py
+++ ion/agents/platform/cgsn/test/basic.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 """
-@package 
-@file 
+@brief  Basic test of messaging between my laptop (within MBARI network) and
+        the 7370 box at WHOI set up my Michael Eder. With the CG services
+        enpoint running, execute this test:
+            bin/python ion/agents/platform/cgsn/test/basic.py
 @author Carlos Rueda
-@brief
 """
 
 __author__ = 'Carlos Rueda'
@@ -15,16 +16,27 @@ from socket import *
 
 
 def client():
-    server_address = ('localhost', 2221)
+    # this is the CG service endpoint at WHOI
     server_address = ('128.128.24.43', 2221)
-    client_socket = socket(AF_INET, SOCK_DGRAM)
+    server_socket = socket(AF_INET, SOCK_DGRAM)
 
+    # this is my laptop within the MBARI network
+    client_address = ('134.89.13.60', 10011)
+    client_socket = socket(AF_INET, SOCK_DGRAM)
+    client_socket.bind(client_address)
+
+    #
+    # simple test is to send a command to the CG services endpoint ...
+    #
     cmd = "DCL_STATUS"
     data = "31,19,112,%i,%s" % (len(cmd), cmd)
     print "Sending %r" % data
-    nobytes = client_socket.sendto(data, server_address)
+    nobytes = server_socket.sendto(data, server_address)
     print "Sent %i bytes" % nobytes
 
+    #
+    # ... and get the corresponding response:
+    #
     recv_data = client_socket.recv(1024)
     print "\t", "Received: ", recv_data
 
