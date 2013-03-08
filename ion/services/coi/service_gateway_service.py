025d6c51c26b8931ab1fe2160aaed6539efabcb6
Luke Campbell <luke.s.campbell@gmail.com>
2013-03-08 16:14:05 -0500

    Adds new submodule ion-functions

--- ion/services/coi/service_gateway_service.py
+++ ion/services/coi/service_gateway_service.py
@@ -702,6 +702,7 @@ def get_version_info():
     pkg_list = ["coi-services",
                 "pyon",
                 "coverage-model",
+                "ion-functions",
                 "eeagent",
                 "epu",
                 "utilities",
