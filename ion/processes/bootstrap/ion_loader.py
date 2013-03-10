94dab8ce933c26f4c0fdda08593e4d9aed73c111
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2013-03-10 15:34:48 -0700

    update tested doc revision

--- ion/processes/bootstrap/ion_loader.py
+++ ion/processes/bootstrap/ion_loader.py
@@ -93,10 +93,10 @@ CANDIDATE_UI_ASSETS = 'https://userexperience.oceanobservatories.org/database-ex
 MASTER_DOC = "https://docs.google.com/spreadsheet/pub?key=0AttCeOvLP6XMdG82NHZfSEJJOGdQTkgzb05aRjkzMEE&output=xls"
 
 ### the URL below should point to a COPY of the master google spreadsheet that works with this version of the loader
-TESTED_DOC = "https://docs.google.com/spreadsheet/pub?key=0AgGScp7mjYjydFZocTR4S0xZZURkN01pVnJhcmV4NVE&output=xls"
+TESTED_DOC = "https://docs.google.com/spreadsheet/pub?key=0AgkUKqO5m-ZidDFSc1RSaVFqVVR4OS1iS0dzejZkRmc&output=xls"
 #
 ### while working on changes to the google doc, use this to run test_loader.py against the master spreadsheet
-TESTED_DOC=MASTER_DOC
+#TESTED_DOC=MASTER_DOC
 
 # The preload spreadsheets (tabs) in the order they should be loaded
 DEFAULT_CATEGORIES = [
