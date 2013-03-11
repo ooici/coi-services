dcabcb567f5a4dc14485245f6fb4d8262fa7cf54
Luke Campbell <luke.s.campbell@gmail.com>
2013-03-11 13:33:29 -0400

    Fixes param map parser

--- ion/services/dm/utility/types.py
+++ ion/services/dm/utility/types.py
@@ -144,8 +144,10 @@ def get_pfunc(pfid):
 
 def evaluate_pmap(pfid, pmap):
     for k,v in pmap.iteritems():
-        if isinstance(v, dict) or 'PFID' in k:
-            pmap[k] = evaluate_pmap(k, v)
+        if isinstance(v, dict):
+            pfid_,pmap_ = v.popitem()
+            print 'Getting function for ', pfid_
+            pmap[k] = evaluate_pmap(pfid_, pmap_)
         if isinstance(v, basestring) and 'PD' in v:
             print 'looking up ', v
             pmap[k] = get_param_name(v)
