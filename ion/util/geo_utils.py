1f00fc4a05947305d05412f293b07f6663e32ad8
seman <sseman@gmail.com>
2013-03-25 15:39:18 -0700

    Updated GeoUtils to calculate midpoint

--- ion/util/geo_utils.py
+++ ion/util/geo_utils.py
@@ -9,35 +9,51 @@ class GeoUtils(object):
     DISTANCE_LONGEST = "longest"
 
     @classmethod
-    def calc_geospatial_point_center(cls, geospatial_bounds=None, distance="shortest"):
-        if not geospatial_bounds or (distance != GeoUtils.DISTANCE_SHORTEST and distance != GeoUtils.DISTANCE_LONGEST):
+    def calc_geospatial_point_center(cls, geospatial_bounds=None, distance=None):
+        if not geospatial_bounds:
             raise BadRequest ("Geospatial bounds data is not set correctly")
         if (geospatial_bounds.geospatial_latitude_limit_north < -90 or geospatial_bounds.geospatial_latitude_limit_north > 90 or
             geospatial_bounds.geospatial_latitude_limit_south < -90 or geospatial_bounds.geospatial_latitude_limit_south > 90 or
             geospatial_bounds.geospatial_longitude_limit_east < -180 or geospatial_bounds.geospatial_longitude_limit_east > 180 or
             geospatial_bounds.geospatial_longitude_limit_west < -180 or geospatial_bounds.geospatial_longitude_limit_west > 180):
             raise BadRequest ("Geospatial bounds data out of range")
-
         geo_index_obj = IonObject(OT.GeospatialIndex)
+
+        west_lon = geospatial_bounds.geospatial_longitude_limit_west
+        east_lon = geospatial_bounds.geospatial_longitude_limit_east
+        north_lat = geospatial_bounds.geospatial_latitude_limit_north
+        south_lat = geospatial_bounds.geospatial_latitude_limit_south
+        if not distance:
+            if (west_lon >= 0 and east_lon >= 0) or (west_lon <= 0 and east_lon <= 0):  # Same hemisphere
+                if west_lon < east_lon:  # West is "to the left" of East
+                    distance = GeoUtils.DISTANCE_SHORTEST
+                else:  # West is "to the right" of East
+                    distance = GeoUtils.DISTANCE_LONGEST
+            elif west_lon >= 0 and east_lon <= 0:
+                if west_lon + abs(east_lon) < 180:
+                    distance = GeoUtils.DISTANCE_LONGEST
+                else:
+                    distance = GeoUtils.DISTANCE_SHORTEST
+            elif west_lon <= 0 and east_lon >= 0:
+                if abs(west_lon) + east_lon > 180:
+                    distance = GeoUtils.DISTANCE_LONGEST
+                else:
+                    distance = GeoUtils.DISTANCE_SHORTEST
+            else:
+                distance = GeoUtils.DISTANCE_SHORTEST
         if distance == GeoUtils.DISTANCE_SHORTEST:
-            geo_index_obj.lat, geo_index_obj.lon = GeoUtils.midpoint_shortest(geospatial_bounds.geospatial_latitude_limit_north,
-                                                         geospatial_bounds.geospatial_longitude_limit_west,
-                                                         geospatial_bounds.geospatial_latitude_limit_south,
-                                                         geospatial_bounds.geospatial_longitude_limit_east)
+            geo_index_obj.lat, geo_index_obj.lon = GeoUtils.midpoint_shortest( north_lat=north_lat, west_lon=west_lon, south_lat=south_lat, east_lon=east_lon)
         elif distance == GeoUtils.DISTANCE_LONGEST:
-            geo_index_obj.lat,  geo_index_obj.lon = GeoUtils.midpoint_longest(geospatial_bounds.geospatial_latitude_limit_north,
-                                                          geospatial_bounds.geospatial_longitude_limit_west,
-                                                          geospatial_bounds.geospatial_latitude_limit_south,
-                                                          geospatial_bounds.geospatial_longitude_limit_east)
+            geo_index_obj.lat,  geo_index_obj.lon = GeoUtils.midpoint_longest( north_lat=north_lat, west_lon=west_lon, south_lat=south_lat, east_lon=east_lon)
         else:
             raise BadRequest("Distance type not specified")
         return geo_index_obj
 
     @staticmethod
-    def midpoint_shortest(lat1, lon1, lat2, lon2):
+    def midpoint_shortest(north_lat, west_lon, south_lat, east_lon):
         g = Geod(ellps='clrk66')
-        af, ab, dist = g.inv(lon1, lat1, lon2, lat2)
-        rlon, rlat, az = g.fwd(lon1, lat1, af, dist/2)
+        af, ab, dist = g.inv(west_lon, north_lat, east_lon, south_lat)
+        rlon, rlat, az = g.fwd(west_lon, north_lat, af, dist/2)
         # decimal places   degrees      distance
         #        0         1            111   km
         #        1         0.1          11.1  km
@@ -53,10 +69,10 @@ class GeoUtils(object):
         return rlat, rlon
 
     @staticmethod
-    def midpoint_longest(lat1, lon1, lat2, lon2):
+    def midpoint_longest(north_lat, west_lon, south_lat, east_lon):
         g = Geod(ellps='clrk66')
-        af, ab, dist = g.inv(lon1, lat1, lon2, lat2)
-        rlon, rlat, az = g.fwd(lon1, lat1, af, dist/2)
+        af, ab, dist = g.inv(west_lon, north_lat, east_lon, south_lat)
+        rlon, rlat, az = g.fwd(west_lon, north_lat, af, dist/2)
         rlon += 180 if rlon < 0 else -180
         rlon = "%.6f" % rlon
         rlat = "%.6f" % rlat
