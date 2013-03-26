3ec67ca4b4506291676ebe7c213d98efff76bb70
Luke Campbell <luke.s.campbell@gmail.com>
2013-03-26 16:23:41 -0400

    Adds Global Range Test Parser

--- ion/util/test/test_stored_values.py
+++ ion/util/test/test_stored_values.py
@@ -5,7 +5,8 @@
 '''
 
 from pyon.util.int_test import IonIntegrationTestCase
-from ion.util.stored_values import StoredValueManager, simple_csv_lookup_parser
+from ion.util.stored_values import StoredValueManager
+from ion.util.parsers.global_range_test import grt_parser
 from nose.plugins.attrib import attr
 
 @attr('INT',group='dm')
@@ -29,18 +30,20 @@ class TestStoredValueManager(IonIntegrationTestCase):
         self.assertTrue('key' in doc and doc['key']=='value')
         self.assertTrue('key2' in doc and doc['key2']=='value2')
 
-    def test_csv_parser(self):
+    def test_global_range_test_parser(self):
         svm = StoredValueManager(self.container)
-        simple_csv_lookup_parser(self.container, 'constants', csv_document_example)
-        self.addCleanup(svm.delete_stored_value, 'constants')
-        doc = svm.read_value('constants')
-        self.assertIn('universe', doc)
-        self.assertEquals(doc['gamma'],0.57721566 )
+        for key,doc in grt_parser(grt_sample_doc):
+            svm.stored_value_cas(key,doc)
+            self.addCleanup(svm.delete_stored_value,key)
 
+        doc = svm.read_value('sbe37-abc123_TEMPWAT_UHHH')
+        self.assertEquals(doc['grt_min_value'], 0.)
+        self.assertEquals(doc['array'],'array 1')
+        doc = svm.read_value('sbe37-abc123_PRESWAT_flagged')
+        self.assertEquals(doc['grt_max_value'], 689.47)
 
-csv_document_example='''coefficient name,value
-pi,3.14159265
-e,2.71828
-gamma,0.57721566
-universe,42'''
+grt_sample_doc ='''Array,Instrument Class,Reference Designator,Data Product In,Units,Data Product Flagged,Min Value (lim(1)),Max Value (lim(2))
+array 1,SBE-37,sbe37-abc123,TEMPWAT,deg_C,UHHH,0,20
+array 1,SBE-37,sbe37-abc123,CONDWAT,S m-1,????,0,30
+array 1,SBE-37,sbe37-abc123,PRESWAT,dbar,flagged,0,689.47'''
 
