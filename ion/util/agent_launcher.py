504f995ff370a59f3cb50bddfff1d3cc81d63094
Ian Katz <ifreecarve@gmail.com>
2013-04-01 15:05:40 -0400

    better error, better location

--- ion/util/agent_launcher.py
+++ ion/util/agent_launcher.py
@@ -32,7 +32,6 @@ class AgentLauncher(object):
                                                                       configuration=agent_config)
 
         log.info("AgentLauncher got process id='%s' from process_dispatcher.schedule_process()", process_id)
-        assert process_id
         self.process_id = process_id
         return process_id
 
