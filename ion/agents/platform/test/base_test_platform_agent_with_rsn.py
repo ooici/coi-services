31773e821378e985aca02cb00b4dba121809d06b
Carlos Rueda <carueda@mbari.org>
2013-04-03 14:32:20 -0700

    some refactoring in platform integration test to facilitate further tests, in particular there is a new test_platform_instrument intended to exercise some more detailed tests against the instrument launched by the platform.

--- ion/agents/platform/test/base_test_platform_agent_with_rsn.py
+++ ion/agents/platform/test/base_test_platform_agent_with_rsn.py
@@ -302,8 +302,8 @@ class BaseIntTestPlatform(IonIntegrationTestCase, HelperTestMixin):
         self.addCleanup(self._stop_event_subscribers)
         self._start_event_subscriber(sub_type="platform_event")
 
-        # the keys of instruments that have been set up
-        self._setup_instruments = set()
+        # instruments that have been set up: instr_key: i_obj
+        self._setup_instruments = {}
 
     #################################################################
     # data subscribers handling
@@ -812,6 +812,8 @@ class BaseIntTestPlatform(IonIntegrationTestCase, HelperTestMixin):
         i_obj.instrument_agent_instance_id = instrument_agent_instance_id
         i_obj.org_obj = org_obj
 
+        log.debug("KK CREATED I_obj: %s", i_obj)
+
         return i_obj
 
     def _create_instrument(self, instr_key):
@@ -835,10 +837,18 @@ class BaseIntTestPlatform(IonIntegrationTestCase, HelperTestMixin):
         log.debug("making the structure for an instrument agent")
         i_obj = self._make_instrument_agent_structure(instr_key, org_obj)
 
-        self._setup_instruments.add(instr_key)
+        self._setup_instruments[instr_key] = i_obj
 
         log.debug("_create_instrument: created instrument %r", instr_key)
 
+        return i_obj
+
+    def _get_instrument(self, instr_key):
+        """
+        Gets the i_obj constructed by _create_instrument(instr_key).
+        """
+        self.assertIn(instr_key, self._setup_instruments)
+        i_obj = self._setup_instruments[instr_key]
         return i_obj
 
     #################################################################
@@ -912,6 +922,132 @@ class BaseIntTestPlatform(IonIntegrationTestCase, HelperTestMixin):
             self._assign_child_to_parent(p_obj, parent_obj)
 
         return p_obj
+
+    def _set_up_single_platform_with_some_instruments(self, instr_keys):
+        """
+        Sets up single platform with some instruments
+
+        @param instr_keys  Keys of the instruments to be assigned.
+                           Must be keys in instruments_dict in
+                           base_test_platform_agent_with_rsn
+
+        @return p_root for subsequent termination
+        """
+
+        for instr_key in instr_keys:
+            self.assertIn(instr_key, instruments_dict)
+
+        p_root = self._create_single_platform()
+
+        # create and assign instruments:
+        for instr_key in instr_keys:
+            i_obj = self._create_instrument(instr_key)
+            self._assign_instrument_to_platform(i_obj, p_root)
+
+        return p_root
+
+    def _set_up_platform_hierarchy_with_some_instruments(self, instr_keys):
+        """
+        Sets up a multiple-level platform hierarchy with instruments associated
+        to some of the platforms.
+
+        The platform hierarchy corresponds to the sub-network in the
+        simulated topology rooted at 'Node1B', which at time of writing
+        looks like this:
+
+        Node1B
+            Node1C
+                Node1D
+                    MJ01C
+                        LJ01D
+                LV01C
+                    PC01B
+                        SC01B
+                            SF01B
+                    LJ01C
+            LV01B
+                LJ01B
+                MJ01B
+
+        In DEBUG logging level for the platform agent, files like the following
+        are generated under logs/:
+           platform_CFG_received_Node1B.txt
+           platform_CFG_received_MJ01C.txt
+           platform_CFG_received_LJ01D.txt
+
+        @param instr_keys  Keys of the instruments to be assigned.
+                           Must be keys in instruments_dict in
+                           base_test_platform_agent_with_rsn
+
+        @return p_root for subsequent termination
+        """
+
+        for instr_key in instr_keys:
+            self.assertIn(instr_key, instruments_dict)
+
+        #####################################
+        # create platform hierarchy
+        #####################################
+        log.info("will create platform hierarchy ...")
+        start_time = time.time()
+
+        root_platform_id = 'Node1B'
+        p_objs = {}
+        p_root = self._create_hierarchy(root_platform_id, p_objs)
+
+        log.info("platform hierarchy built. Took %.3f secs. "
+                  "Root platform=%r, number of platforms=%d: %s",
+                  time.time() - start_time,
+                  root_platform_id, len(p_objs), p_objs.keys())
+
+        self.assertIn(root_platform_id, p_objs)
+        self.assertEquals(13, len(p_objs))
+
+        #####################################
+        # create the indicated instruments
+        #####################################
+        log.info("will create %d instruments: %s", len(instr_keys), instr_keys)
+        start_time = time.time()
+
+        i_objs = []
+        for instr_key in instr_keys:
+            i_obj = self._create_instrument(instr_key)
+            i_objs.append(i_obj)
+            log.debug("instrument created = %r (%s)",
+                      i_obj.instrument_agent_instance_id, instr_key)
+
+        log.info("%d instruments created. Took %.3f secs.", len(instr_keys), time.time() - start_time)
+
+        #####################################
+        # assign the instruments
+        #####################################
+        log.info("will assign instruments ...")
+        start_time = time.time()
+
+        plats_to_assign_instrs = [
+            'LJ01D', 'SF01B', 'LJ01B', 'MJ01B',     # leaves
+            'MJ01C', 'Node1D', 'LV01B', 'Node1C'    # intermediate
+        ]
+
+        # assign one available instrument to a platform;
+        # the assignments are arbitrary.
+        num_assigns = min(len(instr_keys), len(plats_to_assign_instrs))
+
+        for ii in range(num_assigns):
+            platform_id = plats_to_assign_instrs[ii]
+            self.assertIn(platform_id, p_objs)
+            p_obj = p_objs[platform_id]
+            i_obj = i_objs[ii]
+            self._assign_instrument_to_platform(i_obj, p_obj)
+            log.debug("instrument %r (%s) assigned to platform %r",
+                      i_obj.instrument_agent_instance_id,
+                      instr_keys[ii],
+                      platform_id)
+
+        log.info("%d instruments assigned. Took %.3f secs.",
+                 num_assigns, time.time() - start_time)
+
+        return p_root
 
     #################################################################
     # start / stop platform
--- ion/services/sa/observatory/test/test_platform_launch.py
+++ ion/services/sa/observatory/test/test_platform_launch.py
@@ -27,7 +27,6 @@ from pyon.public import log
 import time
 
 from ion.agents.platform.test.base_test_platform_agent_with_rsn import BaseIntTestPlatform
-from ion.agents.platform.test.base_test_platform_agent_with_rsn import instruments_dict
 
 from mock import patch
 from pyon.public import CFG
@@ -37,7 +36,10 @@ from pyon.public import CFG
 class TestPlatformLaunch(BaseIntTestPlatform):
 
     def _run_commands(self):
-
+        """
+        A common sequence of commands for the root platform in some of the
+        tests below.
+        """
         self._ping_agent()
         self._initialize()
         self._go_active()
@@ -71,98 +73,14 @@ class TestPlatformLaunch(BaseIntTestPlatform):
         # basic test of launching a single platform with an instrument
         #
 
-        p_root = self._create_single_platform()
-        i_obj = self._create_instrument('SBE37_SIM_01')
-        self._assign_instrument_to_platform(i_obj, p_root)
+        p_root = self._set_up_single_platform_with_some_instruments(['SBE37_SIM_01'])
 
-        #####################################
-        # start the root platform:
-        #####################################
-        log.info("will start the root platform ...")
-        start_time = time.time()
         self._start_platform(p_root)
-        log.info("root platform started. Took %.3f secs.", time.time() - start_time)
-
         self._run_commands()
         self._stop_platform(p_root)
 
     def test_platform_hierarchy_with_some_instruments(self):
         #
-        # test of launching a multiple-level platform hierarchy with
-        # instruments associated to some of the platforms.
-        #
-        # The platform hierarchy corresponds to the sub-network in the
-        # simulated topology rooted at 'Node1B', which at time of writing
-        # looks like this:
-        #
-        # Node1B
-        #     Node1C
-        #         Node1D
-        #             MJ01C
-        #                 LJ01D
-        #         LV01C
-        #             PC01B
-        #                 SC01B
-        #                     SF01B
-        #             LJ01C
-        #     LV01B
-        #         LJ01B
-        #         MJ01B
-        #
-        # In DEBUG logging level for the platform agent, files like the following
-        # are generated under logs/:
-        #    platform_CFG_received_Node1B.txt
-        #    platform_CFG_received_MJ01C.txt
-        #    platform_CFG_received_LJ01D.txt
-
-        #####################################
-        # create platform hierarchy
-        #####################################
-        log.info("will create platform hierarchy ...")
-        start_time = time.time()
-
-        root_platform_id = 'Node1B'
-        p_objs = {}
-        p_root = self._create_hierarchy(root_platform_id, p_objs)
-
-        log.info("platform hierarchy built. Took %.3f secs. "
-                  "Root platform=%r, number of platforms=%d: %s",
-                  time.time() - start_time,
-                  root_platform_id, len(p_objs), p_objs.keys())
-
-        self.assertIn(root_platform_id, p_objs)
-        self.assertEquals(13, len(p_objs))
-
-        #####################################
-        # create some instruments
-        #####################################
-        instr_keys = sorted(instruments_dict.keys())
-        log.info("will create %d instruments: %s", len(instr_keys), instr_keys)
-        start_time = time.time()
-
-        i_objs = []
-        for instr_key in instr_keys:
-            i_obj = self._create_instrument(instr_key)
-            i_objs.append(i_obj)
-            log.debug("instrument created = %r (%s)",
-                      i_obj.instrument_agent_instance_id, instr_key)
-
-        log.info("%d instruments created. Took %.3f secs.", len(instr_keys), time.time() - start_time)
-
-        #####################################
-        # assign the instruments
-        #####################################
-        log.info("will assign instruments ...")
-        start_time = time.time()
-
-        plats_to_assign_instrs = [
-            'LJ01D', 'SF01B', 'LJ01B', 'MJ01B',  # leaves
-            'MJ01C', 'Node1D', 'LV01B',          # intermediate
-        ]
-
-        # assign one available instrument to a platform
-        num_assigns = min(len(instr_keys), len(plats_to_assign_instrs))
-
         # TODO for some reason assigning more than 2 or 3 instruments triggers
         # exceptions in the port_agent toward the finalization of the test:
         #
@@ -183,52 +101,14 @@ class TestPlatformLaunch(BaseIntTestPlatform):
         # generated from instrument_agent ... perhaps the finalization of the
         # instr agent is conflicting somehow with attempts to re-connect (?)
         #
-        # Ah! it seems there're similar exceptions in coi_pycc with other tests
-        # eg.,
-        # test_connect_failed (ion.agents.instrument.test.test_instrument_agent.TestInstrumentAgent)
-        #
-        # So, while this is investigated setting number of assignments to 2
+        # So, while this is investigated, setting number of assignments to 2
         # as it seems to be consistently stable:
-        num_assigns = 2
+        #instr_keys = sorted(instruments_dict.keys())
 
-        for ii in range(num_assigns):
-            platform_id = plats_to_assign_instrs[ii]
-            self.assertIn(platform_id, p_objs)
-            p_obj = p_objs[platform_id]
-            i_obj = i_objs[ii]
-            self._assign_instrument_to_platform(i_obj, p_obj)
-            log.debug("instrument %r (%s) assigned to platform %r",
-                      i_obj.instrument_agent_instance_id,
-                      instr_keys[ii],
-                      platform_id)
+        instr_keys = ["SBE37_SIM_01", "SBE37_SIM_02", ]
 
-        log.info("%d instruments assigned. Took %.3f secs.",
-                 num_assigns, time.time() - start_time)
-
-        #####################################
-        # start the root platform:
-        #####################################
-        log.info("will start the root platform ...")
-        start_time = time.time()
+        p_root = self._set_up_platform_hierarchy_with_some_instruments(instr_keys)
 
         self._start_platform(p_root)
-
-        log.info("root platform started. Took %.3f secs.", time.time() - start_time)
-
-        #####################################
-        # run the commands:
-        #####################################
-        log.info("will run commands ...")
-        start_time = time.time()
         self._run_commands()
-
-        log.info("commands run. Took %.3f secs.", time.time() - start_time)
-
-        #####################################
-        # stop the root platform
-        #####################################
-        log.info("will stop the root platform ...")
-        start_time = time.time()
         self._stop_platform(p_root)
-
-        log.info("root platform stopped. Took %.3f secs.", time.time() - start_time)
