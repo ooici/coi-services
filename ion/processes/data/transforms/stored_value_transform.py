48dd2e19350f4e69d689f6c25bee97a0a9dfeb46
Luke Campbell <luke.s.campbell@gmail.com>
2013-04-03 09:11:33 -0400

    Fixes and updates stored_value_transform with test

--- ion/processes/data/transforms/stored_value_transform.py
+++ ion/processes/data/transforms/stored_value_transform.py
@@ -12,20 +12,31 @@ import numpy as np
 
 class StoredValueTransform(TransformStreamListener):
     def on_start(self):
+        TransformStreamListener.on_start(self)
         self.document_key = self.CFG.get_safe('process.document_key')
         self.stored_value_manager = StoredValueManager(self.container)
 
     def recv_packet(self, msg, route, stream_id):
+        print 'Got RDT'
         rdt = RecordDictionaryTool.load_from_granule(msg)
 
         document = {}
 
         for k,v in rdt.iteritems():
             value_array = np.atleast_1d(v[:])
+            print 'Some Values: ', repr(value_array)
             if 'f' in value_array.dtype.str:
-                document[k] = [float(i) for i in value_array]
+                print 'Storing'
+                if value_array.shape == (1,):
+                    document[k] = float(value_array[0])
+                else:
+                    document[k] = [float(i) for i in value_array]
             elif 'i' in value_array.dtype.str:
-                document[k] = [int(i) for i in value_array]
+                print 'Storing'
+                if value_array.shape == (1,):
+                    document[k] = int(value_array[0])
+                else:
+                    document[k] = [int(i) for i in value_array]
 
         self.stored_value_manager.stored_value_cas(self.document_key, document)
 
--- ion/services/sa/process/test/test_int_data_process_management_service.py
+++ ion/services/sa/process/test/test_int_data_process_management_service.py
@@ -1189,6 +1189,34 @@ class TestDataProcessManagementPrime(IonIntegrationTestCase):
         data_process_id = self.data_process_management.create_data_process2(data_process_definition_id=data_process_definition_id, in_data_product_ids=[input_data_product_id], out_data_product_ids=[],configuration=config)
         self.addCleanup(self.data_process_management.delete_data_process2,data_process_id)
 
+        self.data_process_management.activate_data_process2(data_process_id)
+        self.addCleanup(self.data_process_management.deactivate_data_process2, data_process_id)
+
+        stream_def_ids, _ = self.resource_registry.find_objects(input_data_product_id, PRED.hasStreamDefinition, id_only=True)
+        stream_def_id = stream_def_ids[0]
+        rdt = RecordDictionaryTool(stream_definition_id=stream_def_id)
+        rdt['time'] = [0]
+        rdt['lat'] = [40.38]
+        rdt['lon'] = [-72.21]
+
+        self.publish_to_data_product(input_data_product_id, rdt)
+
+        def verifier():
+            svm = StoredValueManager(self.container)
+            try:
+                doc = svm.read_value('example_document')
+            except NotFound:
+                return False
+            np.testing.assert_almost_equal(doc['lat'], 40.38, 4)
+            np.testing.assert_almost_equal(doc['lon'], -72.21, 4)
+            return True
+        success = poll(verifier)
+        self.assertTrue(success)
+        svm = StoredValueManager(self.container)
+        svm.delete_stored_value('example_document')
+            
+
+
     @attr('LOCOINT')
     @unittest.skipIf(os.getenv('CEI_LAUNCH_TEST', False), 'Skip test while in CEI LAUNCH mode')
     def test_global_range_test(self):
