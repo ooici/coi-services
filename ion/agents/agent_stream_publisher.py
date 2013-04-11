920551b5beb41269f5c1fd032a4becccfae9b419
Edward Hunter <edwardhunter@ucsd.edu>
2013-04-11 15:51:00 -0700

    Opt in classes for generic agent mechanisms.

---
+++ ion/agents/agent_stream_publisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+@package ion.agents.agent_stream_publisher
+@file ion/agents/agent_stream_publisher.py
+@author Edward Hunter
+@brief Class for managing stream publishing details for opt-in use by agents
+that stream data.
+"""
+
+__author__ = 'Edward Hunter'
+__license__ = 'Apache 2.0'
+
+# Pyon imports
+from pyon.public import IonObject, log, RT, PRED, LCS, OT, CFG
+
+
+
+
+
