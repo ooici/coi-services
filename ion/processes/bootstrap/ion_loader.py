e20a73bcf160c782db894bfc25d43a906e20bdfa
Maurice Manning <mmmanning@ucsd.edu>
2013-04-29 16:24:34 -0700

    update the tested_doc

--- ion/processes/bootstrap/ion_loader.py
+++ ion/processes/bootstrap/ion_loader.py
@@ -97,7 +97,7 @@ CANDIDATE_UI_ASSETS = 'https://userexperience.oceanobservatories.org/database-ex
 MASTER_DOC = "https://docs.google.com/spreadsheet/pub?key=0AttCeOvLP6XMdG82NHZfSEJJOGdQTkgzb05aRjkzMEE&output=xls"
 
 ### the URL below should point to a COPY of the master google spreadsheet that works with this version of the loader
-TESTED_DOC = "https://docs.google.com/spreadsheet/pub?key=0AgjFgozf2vG6dC05RnZHMEx3V0RYNEp3YTgwUllMU2c&output=xls"
+TESTED_DOC = "https://docs.google.com/spreadsheet/pub?key=0AgjFgozf2vG6dEVqWnBsdHJ3MW5aMFpzTnA5V014Mmc&output=xls"
 #
 ### while working on changes to the google doc, use this to run test_loader.py against the master spreadsheet
 #TESTED_DOC=MASTER_DOC
