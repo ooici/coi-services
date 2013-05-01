267e7a5f1fcbc2a56d1c384c861ebe22e6df1c39
Edward Hunter <edwardhunter@ucsd.edu>
2013-05-01 13:16:06 -0700

    Fix autoreconnect test logic.

--- ion/agents/instrument/test/test_instrument_agent.py
+++ ion/agents/instrument/test/test_instrument_agent.py
@@ -45,6 +45,7 @@ from pyon.core.object import IonObjectDeserializer
 
 # Pyon exceptions.
 from pyon.core.exception import BadRequest, Conflict, Timeout, ResourceError
+from pyon.core.exception import IonException
 
 # Agent imports.
 from pyon.util.context import LocalContextMixin
@@ -2101,7 +2102,6 @@ class TestInstrumentAgent(IonIntegrationTestCase):
         state = self._ia_client.get_agent_state()
         self.assertEqual(state, ResourceAgentState.UNINITIALIZED)
 
-    @unittest.skip('Fails in buildbot.')
     def test_autoreconnect(self):
         """
         test_autoreconnect
@@ -2139,7 +2139,7 @@ class TestInstrumentAgent(IonIntegrationTestCase):
                 try:
                     gevent.sleep(.5)
                     test._ia_client.execute_resource(cmd)
-                except:
+                except IonException:
                     break
                 
             while True:
@@ -2147,10 +2147,10 @@ class TestInstrumentAgent(IonIntegrationTestCase):
                     gevent.sleep(.5)
                     test._ia_client.execute_resource(cmd)
                     break
-                except:
+                except IonException:
                     pass
-
-        timeout = gevent.Timeout(120)
+                
+        timeout = gevent.Timeout(240)
         timeout.start()
         try:
 
@@ -2167,10 +2167,13 @@ class TestInstrumentAgent(IonIntegrationTestCase):
             
             # Wait for the device to connect and start sampling again.
             gl.join()
+            gl = None
             timeout.cancel()
             
-        except Exception as ex:
-            gl.kill()
+        except (Exception, gevent.Timeout) as ex:
+            if gl:
+                gl.kill()
+                gl = None
             self.fail('Could not reconnect to device: '+str(ex))
 
     def test_connect_failed(self):
