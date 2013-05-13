cc4d555dbb7474122e4508a5e235e71520c346c4
Jamie Chen <jachen@ucsd.edu>
2013-05-13 15:52:30 -0700

    Release Version 2.0.35

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import sys
 if sys.platform == 'darwin':
     os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
 
-version = '2.0.35-dev'
+version = '2.0.35'
 
 setup(  name = 'coi-services',
         version = version,
