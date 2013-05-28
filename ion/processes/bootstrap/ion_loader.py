cbe5358e4cd8d011f321dcf1fccb70af736eea7f
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2013-05-28 14:29:08 -0700

    broaden asset warning in preload

--- ion/processes/bootstrap/ion_loader.py
+++ ion/processes/bootstrap/ion_loader.py
@@ -2675,9 +2675,9 @@ Reason: %s
             path = "%s/%s" % (self.attachment_path, filename)
             with open(path, "rb") as f:
                 att_obj.content = f.read()
-        except IOError, ioe:
+        except:
             # warn instead of fail here
-            log.warn("Failed to open attachment file %s: %s", filename, ioe)
+            log.warn("Failed to open attachment file: %s", filename, exc_info=True)
 
         att_id = self.container.resource_registry.create_attachment(res_id, att_obj)
         self._register_id(row[COL_ID], att_id, att_obj)
