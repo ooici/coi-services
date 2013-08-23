f39d244420bd963daf50700c92777dd18f6fbb65
Luke Campbell <luke.s.campbell@gmail.com>
2013-08-23 14:24:30 -0400

    Fixes potential indent error

--- ion/services/dm/presentation/discovery_service.py
+++ ion/services/dm/presentation/discovery_service.py
@@ -943,8 +943,8 @@ class DiscoveryService(BaseDiscoveryService):
         if id_only:
             return resource_ids
 
-            if not isinstance(resource_ids, list):
-                resource_ids = list(resource_ids)
+        if not isinstance(resource_ids, list):
+            resource_ids = list(resource_ids)
         resources = self.clients.resource_registry.read_mult(resource_ids)
 
         return resources
@@ -958,8 +958,8 @@ class DiscoveryService(BaseDiscoveryService):
         if id_only:
             return resource_ids
 
-            if not isinstance(resource_ids, list):
-                resource_ids = list(resource_ids)
+        if not isinstance(resource_ids, list):
+            resource_ids = list(resource_ids)
         resources = self.clients.resource_registry.read_mult(resource_ids)
 
         return resources
