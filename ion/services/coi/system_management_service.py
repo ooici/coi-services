4677b64dcb1ff75569dc4848a2f707f702ee8540
Michael Meisinger <mmeisinger@ucsd.edu>
2013-09-05 15:59:12 -0700

    Initial implementation of triggered container stats

--- ion/services/coi/system_management_service.py
+++ ion/services/coi/system_management_service.py
@@ -1,9 +1,13 @@
-from interface.services.coi.isystem_management_service import BaseSystemManagementService
+#!/usr/bin/env python
+
+__author__ = 'Michael Meisinger, Jonathan Newbrough'
 
 from pyon.event.event import EventPublisher
+from pyon.public import OT, IonObject
+
 from interface.objects import AllContainers
-from pyon.core.bootstrap import IonObject
-from pyon.public import OT
+from interface.services.coi.isystem_management_service import BaseSystemManagementService
+
 
 ALL_CONTAINERS_INSTANCE = AllContainers()
 
@@ -15,11 +19,14 @@ class SystemManagementService(BaseSystemManagementService):
     def on_start(self,*a,**b):
         super(SystemManagementService,self).on_start(*a,**b)
         self.sender = EventPublisher()
+
     def on_quit(self,*a,**b):
         self.sender.close()
+
     def perform_action(self, predicate, action):
         userid = None # get from context
         self.sender.publish_event(event_type=OT.ContainerManagementRequest, origin=userid, predicate=predicate, action=action)
+
     def set_log_level(self, logger='', level='', recursive=False):
         self.perform_action(ALL_CONTAINERS_INSTANCE, IonObject(OT.ChangeLogLevel, logger=logger, level=level, recursive=recursive))
 
@@ -37,3 +44,16 @@ class SystemManagementService(BaseSystemManagementService):
         @throws BadRequest    None
         """
         self.perform_action(ALL_CONTAINERS_INSTANCE, IonObject(OT.TriggerGarbageCollection))
+
+    def trigger_container_snapshot(self, include_snapshots=None, exclude_snapshots=None, take_at_time='',
+                                   clear_all=False, persist_snapshot=True):
+
+        self.perform_action(ALL_CONTAINERS_INSTANCE, IonObject(OT.TriggerContainerSnapshot,
+                                                               include_snapshots=include_snapshots,
+                                                               exclude_snapshots=exclude_snapshots,
+                                                               take_at_time=take_at_time,
+                                                               clear_all=clear_all,
+                                                               persist_snapshot=persist_snapshot))
+
+    def prepare_system_shutdown(self, mode=''):
+        self.perform_action(ALL_CONTAINERS_INSTANCE, IonObject(OT.PrepareSystemShutdown, mode=mode))
