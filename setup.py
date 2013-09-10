c21a14d8c3efbaaf7b02b4c0c3e469fc9f3bd039
Jamie Chen <jachen@ucsd.edu>
2013-09-10 08:41:09 -0700

    Bump Version to 2.0.66-dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import sys
 if sys.platform == 'darwin':
     os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
 
-version = '2.0.65'
+version = '2.0.66-dev'
 
 setup(  name = 'coi-services',
         version = version,
