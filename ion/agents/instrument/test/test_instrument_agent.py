4616d03df6bca27a4b1cd432b0540515aee70800
Edward Hunter <edwardhunter@ucsd.edu>
2013-09-10 14:49:36 -0700

    minor comment.

--- ion/agents/instrument/test/test_instrument_agent.py
+++ ion/agents/instrument/test/test_instrument_agent.py
@@ -78,6 +78,7 @@ from ooi.timer import Timer
 """
 --with-queueblame   report leftover queues
 --with-pycc         run in seperate container
+--with-greenletleak
 bin/nosetests -s -v --nologcapture ion/agents/instrument/test/test_gateway_to_instrument_agent.py:TestInstrumentAgentViaGateway
 bin/nosetests -s -v --nologcapture ion/agents/instrument/test/test_instrument_agent.py:TestInstrumentAgent
 bin/nosetests -s -v --nologcapture ion/agents/instrument/test/test_instrument_agent.py:TestInstrumentAgent.test_initialize
