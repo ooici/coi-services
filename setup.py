04957202b2cfbe27b2366a3317f35561cf0be600
Jamie Chen <jachen@ucsd.edu>
2013-09-27 13:55:46 -0700

    New trunk version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import sys
 if sys.platform == 'darwin':
     os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
 
-version = '2.0.69-dev'
+version = '2.1.0-dev'
 
 setup(  name = 'coi-services',
         version = version,
