6842a38ac658fb645199001470ab3ad38cbe9266
Bill French <wfrench@ucsd.edu>
2014-01-31 12:33:44 -0800

    fixed lost connection retry logic

--- ion/agents/data/test/test_dsa_moas_ctdgv.py
+++ ion/agents/data/test/test_dsa_moas_ctdgv.py
@@ -11,11 +11,13 @@ __author__ = 'Bill French'
 __license__ = 'Apache 2.0'
 
 import gevent
+import os
 from pyon.public import log
 from nose.plugins.attrib import attr
 
 from ion.agents.data.test.dataset_test import DatasetAgentTestCase
 from ion.services.dm.test.dm_test_case import breakpoint
+from pyon.agent.agent import ResourceAgentState
 import unittest
 
 ###############################################################################
@@ -24,7 +26,7 @@ import unittest
 
 
 @attr('INT', group='sa')
-class HypmCTDTest(DatasetAgentTestCase):
+class GliderCTDTest(DatasetAgentTestCase):
     """
     Verify dataset agent can harvest data fails, parse the date, publish,
     ingest and retrieve stored data.
@@ -39,7 +41,7 @@ class HypmCTDTest(DatasetAgentTestCase):
             #mi_repo = '/Users/wfrench/Workspace/code/wfrench/marine-integrations'
         )
 
-        super(HypmCTDTest, self).setUp()
+        super(GliderCTDTest, self).setUp()
 
     def test_parse(self):
         """
@@ -75,4 +77,26 @@ class HypmCTDTest(DatasetAgentTestCase):
     def test_capabilities(self):
         self.assert_agent_capabilities()
 
+    def test_lost_connection(self):
+        """
+        Test a parser exception and verify that the lost connection logic works
+        """
+        self.assert_initialize()
 
+        path = self.create_sample_data("moas_ctdgv/file_1.mrg", "unit_363_2013_245_6_6.mrg")
+        os.chmod(path, 0000)
+
+        self.assert_state_change(ResourceAgentState.LOST_CONNECTION)
+
+        # Sleep long enough to let the first reconnect happen and fail again.
+        gevent.sleep(65)
+
+        # Resolve the issue
+        os.chmod(path, 0755)
+
+        # We should transition back to streaming and stay there.
+        self.assert_state_change(ResourceAgentState.STREAMING, timeout=180)
+        self.create_sample_data("moas_ctdgv/file_2.mrg", "unit_363_2013_245_10_6.mrg")
+
+        granules = self.get_samples(self.test_config.stream_name, 4, timeout=30)
+        self.assert_data_values(granules, 'moas_ctdgv/merged.result.yml')
