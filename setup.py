ff0576af5c257be773ab148e2dc7d283bc08f94f
Jamie Chen <jachen@ucsd.edu>
2014-02-12 16:36:53 -0800

    Release Version 2.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import sys
 if sys.platform == 'darwin':
     os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
 
-version = '2.1.3-dev'
+version = '2.1.3'
 
 setup(  name = 'coi-services',
         version = version,
