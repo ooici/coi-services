20f18c69c96c01c9e56e56b7bc85a9ea4362e7d4
Luke Campbell <luke.s.campbell@gmail.com>
2014-02-20 10:17:21 -0500

    Adds proper greenlet handling to replay stuff

--- ion/processes/data/replay/replay_process.py
+++ ion/processes/data/replay/replay_process.py
@@ -89,6 +89,7 @@ class ReplayProcess(BaseReplayProcess):
         self.stream_id       = self.CFG.get_safe('process.publish_streams.output', '')
         self.stream_def      = pubsub.read_stream_definition(stream_id=self.stream_id)
         self.stream_def_id   = self.stream_def._id
+        self.replay_thread   = None
 
         self.publishing.clear()
         self.play.set()
@@ -254,7 +255,7 @@ class ReplayProcess(BaseReplayProcess):
         '''
         if self.publishing.is_set():
             return False
-        gevent.spawn(self.replay)
+        self.replay_thread = self._process.thread_manager.spawn(self.replay)
         return True
 
     def replay(self):
