dc000565177baf71aad7b24fcbd7d2ed8897971b
Michael Meisinger <mmeisinger@ucsd.edu>
2014-05-30 16:25:25 -0700

    CIDEVSTC-238: Identity management service extension for security tokens

--- ion/services/coi/identity_management_service.py
+++ ion/services/coi/identity_management_service.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 
-__author__ = 'Thomas R. Lennan, Stephen Henrie'
+__author__ = 'Thomas R. Lennan, Stephen Henrie, Michael Meisinger'
 
-
-from pyon.core.exception import Conflict, Inconsistent, NotFound, BadRequest
-from pyon.core.security.authentication import Authentication
-from pyon.ion.resource import ExtendedResourceContainer
-from pyon.core.governance.negotiation import Negotiation
-from pyon.public import PRED, RT, IonObject, OT
-from pyon.util.log import log
-from interface.objects import  NegotiationStatusEnum
-from uuid import uuid4
+import calendar
+import copy
 from datetime import datetime, timedelta
 import time
-import copy
-import calendar
+from uuid import uuid4
+
+from pyon.core.security.authentication import Authentication
+from pyon.core.exception import Unauthorized
+from pyon.ion.resource import ExtendedResourceContainer
+from pyon.public import log, PRED, RT, IonObject, OT, Conflict, Inconsistent, NotFound, BadRequest, get_ion_ts_millis
+
+from interface.objects import ProposalOriginatorEnum, NegotiationStatusEnum, NegotiationTypeEnum, \
+    SecurityToken, TokenTypeEnum
 
 from interface.services.coi.iidentity_management_service import BaseIdentityManagementService
 from interface.services.coi.iorg_management_service import OrgManagementServiceProcessClient
-from interface.objects import ProposalStatusEnum, ProposalOriginatorEnum, NegotiationStatusEnum, NegotiationTypeEnum
+
+MAX_TOKEN_VALIDITY = 365*24*60*60
+
 
 class IdentityManagementService(BaseIdentityManagementService):
     """
-    A resource registry that stores identities of users and resources, including bindings of internal identities to external identities. Also stores metadata such as a user profile.a	A resource registry that stores identities of users and resources, including bindings of internal identities to external identities. Also stores metadata such as a user profile.a
+    Stores identities of users and resources, including bindings of internal
+    identities to external identities. Also stores metadata such as a user profile.
     """
 
     def on_init(self):
@@ -370,34 +373,37 @@ class IdentityManagementService(BaseIdentityManagementService):
             raise NotFound("complete_account_merge: Association between UserCredentials and ActorIdentity not found. ActorIdentity ID %s, UserCredentials ID: %s " % actor_identity_id % user_credential_id)
         self.clients.resource_registry.delete_association(association_id[0])
 
-    def __generate_token(self):
+    # -------------------------------------------------------------------------
+    # Merge account support
+
+    def _generate_token(self):
         return str(uuid4()) + "_" + str(uuid4())
 
-    def __get_current_user_id(self):
+    def _get_current_user_id(self):
         ctx = self.get_context()
         return ctx.get('ion-actor-id', None) if ctx else None
 
-    def __update_user_info_token(self, token=""):
+    def _update_user_info_token(self, token=""):
         if not token:
-            raise BadRequest("__update_user_info_token: token must be set")
-        ion_actor_id = self.__get_current_user_id()
+            raise BadRequest("_update_user_info_token: token must be set")
+        ion_actor_id = self._get_current_user_id()
         if ion_actor_id:
             current_user_info = self.find_user_info_by_id(ion_actor_id)
             current_user_info.tokens.append(token)
             self.update_user_info(current_user_info)
         else:
-            raise BadRequest("__update_user_info_token: Current UserInfo not found")
+            raise BadRequest("_update_user_info_token: Current UserInfo not found")
 
-    def __validate_token_string(self, token_string, user_info):
+    def _validate_token_string(self, token_string, user_info):
         # Find the token from the  UserInfo
         token_obj = [token for token in user_info.tokens if token.token_string == token_string]
         if not token_obj or not token_obj[0].merge_email or not token_obj[0].expires:
-            raise NotFound("__validate_token: Token data not found")
+            raise NotFound("_validate_token: Token data not found")
         token_obj = token_obj[0]
         # Validate the expiration time and token status
         current_time = calendar.timegm((datetime.utcnow()).timetuple())
         if current_time > token_obj.expires or "OPEN" != token_obj.status:
-            raise BadRequest("__validate_token: access token expired or token status is invalid")
+            raise BadRequest("_validate_token: access token expired or token status is invalid")
         return token_obj
 
     def initiate_account_merge(self, merge_user_email=''):
@@ -416,12 +422,12 @@ class IdentityManagementService(BaseIdentityManagementService):
         if not merge_user_info:
             raise NotFound("initiate_account_merge: email address not found")
         # Validate current user and the merger user are two different accounts
-        if self.find_user_info_by_id(self.__get_current_user_id())._id == merge_user_info._id:
+        if self.find_user_info_by_id(self._get_current_user_id())._id == merge_user_info._id:
             raise BadRequest("initiate_account_merge: current and merge accounts are the same")
-        token_str = self.__generate_token()
+        token_str = self._generate_token()
         expire_time = calendar.timegm((datetime.utcnow() + timedelta(days=2)).timetuple())  # Set token expire time
-        token = IonObject("TokenInformation", {"token_string": token_str, "expires": expire_time, "status": "OPEN", "merge_email": merge_user_email})
-        self.__update_user_info_token(token)
+        token = SecurityToken(token_string=token_str, expires=expire_time, status="OPEN", merge_email=merge_user_email)
+        self._update_user_info_token(token)
 
         return token_str
 
@@ -436,11 +442,11 @@ class IdentityManagementService(BaseIdentityManagementService):
             raise BadRequest("complete_account_merge: token_str must be set")
 
         # Get current UserInfo
-        current_user_id = self.__get_current_user_id()
+        current_user_id = self._get_current_user_id()
         current_user_info = self.find_user_info_by_id(current_user_id)
 
         # Find all the necessary data of the merge account
-        token_obj = self.__validate_token_string(token_string, current_user_info)
+        token_obj = self._validate_token_string(token_string, current_user_info)
         merge_user_info_obj = self.find_user_info_by_email(token_obj.merge_email)  # Find UserInfo of the merge account
         merge_user_info_id = merge_user_info_obj._id
         subjects, associations = self.clients.resource_registry.find_subjects(RT.ActorIdentity, PRED.hasInfo, merge_user_info_id)  # Find ActorIdentity of the merge account
@@ -470,3 +476,137 @@ class IdentityManagementService(BaseIdentityManagementService):
         self.update_user_info(current_user_info)
         log.debug("complete_account_merge: account merge completed from %s to %s" %(merge_actor_identity_id, current_user_id))
         return True
+
+
+    # -------------------------------------------------------------------------
+    # Manage authentication tokens (R2 M185)
+
+    def _generate_auth_token(self, actor_id=None, expires=""):
+        token_string = uuid4().hex
+        token = SecurityToken(token_type=TokenTypeEnum.ACTOR_AUTH, token_string=token_string,
+                              actor_id=actor_id, expires=expires, status="OPEN")
+        return token
+
+    def create_authentication_token(self, actor_id='', start_time='', validity=0):
+        """Create an authentification token for provided actor id with a given start time and validity.
+        start_time defaults to current time if empty and uses a system timestamp.
+        validity is in seconds and must be set.
+
+        @param actor_id    str
+        @param start_time    str
+        @param validity    int
+        @retval token_string    str
+        @throws BadRequest    Illegal parameter type of value
+        @throws NotFound    Object not found
+        """
+        if not actor_id:
+            raise BadRequest("Must provide argument: actor_id")
+        actor_obj = self.clients.resource_registry.read(actor_id)
+        if actor_obj.type_ != RT.ActorIdentity:
+            raise BadRequest("Illegal type for argument actor_id")
+        if type(validity) not in (int, long):
+            raise BadRequest("Illegal type for argument validity")
+        if validity <= 0 or validity > MAX_TOKEN_VALIDITY:
+            raise BadRequest("Illegal value for argument validity")
+        cur_time = get_ion_ts_millis()
+        if not start_time:
+            start_time = cur_time
+        start_time = int(start_time)
+        if start_time > cur_time:
+            raise BadRequest("Illegal value for start_time: Future values not allowed")
+        if (start_time + 1000*validity) < cur_time:
+            raise BadRequest("Illegal value for start_time: Already expired")
+        expires = str(start_time + 1000*validity)
+
+        token = self._generate_auth_token(actor_id, expires=expires)
+        token_id = "token_%s" % token.token_string
+
+        self.container.object_store.create(token, token_id)
+
+        return token.token_string
+
+    def read_authentication_token(self, token_string=''):
+        """Returns the token object for given actor authentication token string.
+
+        @param token_string    str
+        @retval token    SecurityToken
+        @throws BadRequest    Illegal parameter type of value
+        @throws NotFound    Token string not found
+        """
+        token_id = "token_%s" % token_string
+        token = self.container.object_store.read(token_id)
+        if not isinstance(token, SecurityToken):
+            raise Inconsistent("Token illegal type")
+        return token
+
+    def update_authentication_token(self, token=None):
+        """Updates the given token.
+
+        @param token    SecurityToken
+        @throws BadRequest    Illegal parameter type of value
+        @throws NotFound    Token not found
+        """
+        if not isinstance(token, SecurityToken):
+            raise BadRequest("Illegal argument type: token")
+        if token.token_type != TokenTypeEnum.ACTOR_AUTH:
+            raise BadRequest("Argument token: Illegal type")
+        cur_time = get_ion_ts_millis()
+        token_exp = int(token.expires)
+        if token_exp > cur_time + 1000*MAX_TOKEN_VALIDITY:
+            raise BadRequest("Argument token: Maximum expiry extended")
+
+        self.container.object_store.update(token)
+
+    def invalidate_authentication_token(self, token_string=''):
+        """Invalidates an authentication token, but leaves it in place for auditing purposes.
+
+        @param token_string    str
+        @throws BadRequest    Illegal parameter type of value
+        @throws NotFound    Token string not found
+        """
+        token_id = "token_%s" % token_string
+        token = self.container.object_store.read(token_id)
+        if not isinstance(token, SecurityToken):
+            raise Inconsistent("Token illegal type")
+        if token.token_type != TokenTypeEnum.ACTOR_AUTH:
+            raise BadRequest("Illegal token type")
+        token.status = "INVALID"
+        self.container.object_store.update(token)
+        log.info("Invalidated security auth token: %s", token.token_string)
+
+    def check_authentication_token(self, token_string=''):
+        """Checks given token and returns a dict with actor id if valid.
+
+        @param token_string    str
+        @retval token_info    dict
+        @throws BadRequest    Illegal parameter type of value
+        @throws NotFound    Token string not found
+        @throws Unauthorized    Token not valid anymore or otherwise
+        """
+        token_id = "token_%s" % token_string
+        token = self.container.object_store.read(token_id)
+        if not isinstance(token, SecurityToken):
+            raise Inconsistent("Token illegal type")
+        if token.token_type != TokenTypeEnum.ACTOR_AUTH:
+            raise BadRequest("Illegal token type")
+        if token.token_string != token_string:
+            raise Inconsistent("Found token's token_string does not match")
+        cur_time = get_ion_ts_millis()
+        if token.status != "OPEN":
+            raise Unauthorized("Token status invalid")
+        if cur_time >= int(token.expires):
+            raise Unauthorized("Token expired")
+
+        token_info = dict(actor_id=token.actor_id,
+                    expiry=token.expires,
+                    token=token,
+                    token_id=token_id)
+
+        log.info("Authentication token %s resolved to actor %s, expiry %s", token_string, token.actor_id, token.expires)
+
+        return token_info
+
+    def _get_actor_authentication_tokens(self, actor_id):
+        actor_tokens = []
+        raise NotImplemented("TODO")
+        return actor_tokens
