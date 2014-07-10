c46a93c55fc4499077d4f8f3d7d18fafe1f9fa37
Carlos Rueda <carueda@mbari.org>
2014-07-09 19:23:39 -0700

    remove logic added per OOIION-1551, which is now obsolete.
    Related old commit: https://github.com/ooici/coi-services/commit/763a90d0edfe

--- ion/agents/platform/platform_agent_stream_publisher.py
+++ ion/agents/platform/platform_agent_stream_publisher.py
@@ -5,42 +5,30 @@
 @file    ion/agents/platform/platform_agent_stream_publisher.py
 @author  Carlos Rueda
 @brief   Stream publishing support for platform agents.
-         NOTE immediate goal is to extract the functionality from the platform
-         agent code itself for modularization purposes and
-         facilitate alignment with AgentStreamPublisher. For the moment there
-         is some replication here of part of the functions there.
 """
 
 __author__ = 'Carlos Rueda'
 
 
-
 from pyon.public import log
 from pyon.ion.stream import StreamPublisher
-
-from interface.objects import StreamRoute
-
-from ion.agents.platform.platform_driver_event import AttributeValueDriverEvent
-
-from ion.services.dm.utility.granule.record_dictionary import RecordDictionaryTool
-import numpy
-
-from coverage_model.parameter import ParameterDictionary
-
 from pyon.core.bootstrap import get_obj_registry
 from pyon.core.object import IonObjectDeserializer
 
-import uuid
+from interface.objects import StreamRoute
 
+from ion.services.dm.utility.granule.record_dictionary import RecordDictionaryTool
+
+from coverage_model.parameter import ParameterDictionary
+
+import numpy
+import uuid
 import logging
 
 
 class PlatformAgentStreamPublisher(object):
     """
     Stream publishing support for platform agents.
-    NOTE immediate goal of this module is to extract the functionality
-    from the platform agent code itself. A next goal is to align
-    (eventually extending) AgentStreamPublisher.
     """
 
     def __init__(self, agent):
@@ -163,49 +151,14 @@ class PlatformAgentStreamPublisher(object):
 
         for param_name, param_value in driver_event.vals_dict.iteritems():
 
-            in_rdt = False
             param_name = param_name.lower()
-            if param_name in rdt:
-                in_rdt = True
-            else:
+            if not param_name in rdt:
                 if param_name not in self._unconfigured_params:
                     # an unrecognized attribute for this platform:
                     self._unconfigured_params.add(param_name)
                     log.warn('%r: got attribute value event for unconfigured parameter %r in stream %r'
                              ' rdt.keys=%s',
                              self._platform_id, param_name, stream_name, list(rdt.iterkeys()))
-
-            if not in_rdt:
-                # OOIION-1551 The following is temporary to facilitate
-                # transitioning to the use of the new attr-id format in
-                # resources like the preload. So, for the moment, the following
-                # checks whether the name part is in the RDT to then use that
-                # instead of simply logging a warning and skipping this
-                # parameter.
-                # Note: we still log the warning above for the original
-                # param_name to facilitate general inspection.
-                separator = param_name.rfind('|')
-                if separator >= 0:
-                    # try the attr-name part:
-                    attr_name = param_name[:separator]
-                    if attr_name in rdt:
-                        # so, we found the attr-name part in the RDT; let's
-                        # use that and note that we will be ignoring the
-                        # attr-instance part in this case. Again, this is
-                        # temporary to facilitate review/transition.
-                        log.warn('%r: OOIION-1551: will use %r, which was found '
-                                 'in RDT. The original %r was not.',
-                                 self._platform_id, attr_name, param_name)
-                        in_rdt = True
-                        param_name = attr_name
-                    else:
-                        if attr_name not in self._unconfigured_params:
-                            self._unconfigured_params.add(attr_name)
-                            log.warn('%r: got attribute value event for unconfigured parameter %r in stream %r'
-                                     ' rdt.keys=%s',
-                                     self._platform_id, attr_name, stream_name, list(rdt.iterkeys()))
-
-            if not in_rdt:
                 continue
 
             # separate values and timestamps:
